9c310d815cdb1c5cc4e3f6890ad8f4588a6c0862
Felipe Flores <felipefloresvendas@gmail.com>
2021-06-29 17:11:33 -0300

    fix: lint, feat: added lint:styles for corrections

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ApiByAreas, ApiByCategory, ApiByIngredients, ApiRecipeDetail, ingredientImg } from './services/theMealDBAPI';
 
 async function App() {
-  // console.log(await ApiByCategory());
-  // console.log(await ApiByAreas());
-  // console.log(await ApiByIngredients());
-  console.log(await ApiRecipeDetail(52777));
-
   return (
     <div className="meals">
       <span className="logo">TRYBE</span>
