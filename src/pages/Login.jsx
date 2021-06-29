e50efd0cee0ae10044aee54e332aec8ffa086bce
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-06-29 20:00:27 -0300

    feat: requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,31 @@ import React from 'react';
 function Login() {
   return (
     <div>
-      Tela de login
+      <label htmlFor="email">
+        Email
+        <input
+          type="text"
+          data-testid="email-input"
+          id="email"
+          // onChange={  }
+        />
+      </label>
+      <label htmlFor="password">
+        Senha
+        <input
+          type="password"
+          data-testid="password-input"
+          id="password"
+          // onChange={  }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        // onClick={ }
+      >
+        Entrar
+      </button>
     </div>
   );
 }
