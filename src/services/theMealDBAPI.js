8302966e1b8059bfd55804c92ff16317caeb9e39
Felipe Flores <felipefloresvendas@gmail.com>
2021-06-29 16:55:21 -0300

    feat: implements all mealApi methods, added 'services' folder

---
+++ src/services/theMealDBAPI.js
@@ -0,0 +1,53 @@
+// categorias: https://www.themealdb.com/api/json/v1/1/list.php?c=list
+// areas: https://www.themealdb.com/api/json/v1/1/list.php?a=list
+// ingredientes: https://www.themealdb.com/api/json/v1/1/list.php?i=list
+
+export const ApiByCategory = async () => {
+  try {
+    const fetchApi = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+    const getResponse = await fetchApi.json();
+    return getResponse;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const ApiByAreas = async () => {
+  try {
+    const fetchApi = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
+    const getResponse = await fetchApi.json();
+    return getResponse;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const ApiByIngredients = async () => {
+  try {
+    const fetchApi = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+    const getResponse = await fetchApi.json();
+    return getResponse;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const ingredientImg = async (ingredient) => {
+  try {
+    const getResponse = await fetch(`https://www.themealdb.com/images/ingredients/${ingredient}.png`);
+    // const getResponse = await fetchApi.json();
+    return getResponse;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const ApiRecipeDetail = async (id) => {
+  try {
+    const fetchApi = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const getResponse = await fetchApi.json();
+    return getResponse;
+  } catch (error) {
+    return error;
+  }
+};
