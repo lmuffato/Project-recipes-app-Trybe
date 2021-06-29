23cbca1aabd0b9392b114668b491757b43a2646d
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-06-29 19:04:43 -0300

    feat: inputs validation | requirement 5

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const minPassLength = 7;
+  function handleClick() {
+    localStorage.setItem('email', email);
+  }
+  const patternEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return (
     <>
       <h1>Login</h1>
       <form>
         <input
           type="email"
+          value={ email }
           placeholder="E-mail"
           data-testid="email-input"
+          pattern={ patternEmail }
+          onChange={ (ev) => setEmail(ev.target.value) }
         />
         <input
           type="password"
+          value={ password }
           placeholder="Senha"
           data-testid="password-input"
+          onChange={ (ev) => setPassword(ev.target.value) }
         />
         <button
           type="button"
           data-testid="login-submit-btn"
+          onClick={ handleClick }
+          disabled={ !((patternEmail.test(email)) && (password.length >= minPassLength)) }
         >
           Entrar
         </button>
