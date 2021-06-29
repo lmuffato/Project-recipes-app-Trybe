ec22850fa2b1094255c972b9eaf6e6c0ce88c8f3
hheloisa <hhackenhaar@gmail.com>
2021-06-29 19:17:52 -0300

    Fix: lint

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
-import UserContext from '../contexts/UserContext';
 import { Link } from 'react-router-dom';
+import UserContext from '../contexts/UserContext';
 
 function LoginPage() {
   const contextUser = useContext(UserContext);
