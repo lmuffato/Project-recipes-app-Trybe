53e4f9ffb563eac6f2222b738ac6dbc874cd2cba
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-06-29 17:16:42 -0300

    Feat: estruturacao inicial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import Provider from './store/Provider';
+import Routes from './components/Routes';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Provider>
+        <Routes />
+      </Provider>
+    </BrowserRouter>
   );
 }
 
