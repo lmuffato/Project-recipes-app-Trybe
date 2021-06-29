81ca8cd1ae475e48097f5974cce507f4f96f5730
Gui-lira <gui.lira.martins@gmail.com>
2021-06-29 16:52:25 -0300

    adding context

---
+++ src/provider/RecipesProvider.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RecipesContext from '../context/RecipesContext';
+
+export default function RecipesProvider({ children }) {
+  context = {};
+  return (
+    <RecipesContext.Provider value={ context }>
+      { children }
+    </RecipesContext.Provider>
+  );
+}
+
+RecipesProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
