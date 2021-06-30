bdf4a1b5b00c7e96c565260485d8ca579b9591f7
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-30 11:00:02 -0300

    feat: req10/1

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/react';
-import Foods from '../pages/Foods';
 import renderWithRouter from '../helper/renderWithRouter';
+import Foods from '../pages/Foods';
+import Login from '../pages/Login';
+
+const testids = {
+  profileTopButton: 'profile-top-btn',
+  pageTitle: 'page-title',
+  searchTopButton: 'search-top-btn',
+};
 
 describe('Requirement 9', () => {
   it('renders profile-top-btn', () => {
     const { getByTestId } = renderWithRouter(<Foods />);
-    const profileTop = getByTestId('profile-top-btn');
+    const profileTop = getByTestId(testids.profileTopButton);
     expect(profileTop).toBeInTheDocument();
   });
 
   it('renders page-title', () => {
     const { getByTestId } = renderWithRouter(<Foods />);
-    const pageTitle = getByTestId('page-title');
+    const pageTitle = getByTestId(testids.pageTitle);
     expect(pageTitle).toBeInTheDocument();
   });
 
   it('renders search-top-btn', () => {
     const { getByTestId } = renderWithRouter(<Foods />);
-    const searchTop = getByTestId('search-top-btn');
+    const searchTop = getByTestId(testids.searchTopButton);
     expect(searchTop).toBeInTheDocument();
+  });
+});
+
+describe('Requirement 10', () => {
+  it('doesnt render Header component on the Login screen', () => {
+    const { queryByTestId } = renderWithRouter(<Login />);
+
+    expect(queryByTestId(testids.profileTopButton)).toBeNull();
+    expect(queryByTestId(testids.pageTitle)).toBeNull();
+    expect(queryByTestId(testids.searchTopButton)).toBeNull();
+  });
+
+  it('renders the correct icons on the FoodArea screen', () => {
+
   });
 });
 
@@ -35,18 +56,18 @@ describe('Requirement 11', () => {
     const { pathname } = history.location;
     expect(pathname).toBe('/perfil');
   });
+});
 
-  describe('Requirement 12', () => {
-    it('shows and hides the seach input', () => {
-      const { queryByRole, getByRole } = renderWithRouter(<Foods />);
+describe('Requirement 12', () => {
+  it('shows and hides the seach input', () => {
+    const { queryByRole, getByRole } = renderWithRouter(<Foods />);
 
-      const searchButton = getByRole('img', {
-        name: /search/i,
-      });
-
-      expect(queryByRole('textbox')).toBeNull();
-      fireEvent.click(searchButton);
-      expect(queryByRole('textbox')).toBeInTheDocument();
+    const searchButton = getByRole('img', {
+      name: /search/i,
     });
+
+    expect(queryByRole('textbox')).toBeNull();
+    fireEvent.click(searchButton);
+    expect(queryByRole('textbox')).toBeInTheDocument();
   });
 });
