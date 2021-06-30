3e5e770341e76d5efef6c8751de5b819f7683089
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-06-30 12:59:13 -0400

    fix: :zap: requisitos 9/10/11/12

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import SearchIcon from '../images/searchIcon.svg';
 import ProfileImage from '../images/profileIcon.svg';
 
 function Header({ title, search = false }) {
+  const [displaySearch, setDisplaySearch] = useState(false);
+  const history = useHistory();
   return (
     <header>
-      <button type="button">
+      <button
+        type="button"
+        onClick={ () => history.push() }
+      >
         <img
           data-testid="profile-top-btn"
           src={ ProfileImage }
@@ -17,7 +23,10 @@ function Header({ title, search = false }) {
       <h1 data-testid="page-title">{title}</h1>
 
       {search && (
-        <button type="button">
+        <button
+          type="button"
+          onClick={ () => setDisplaySearch(!displaySearch) }
+        >
           <img
             src={ SearchIcon }
             alt="SearchBtn"
