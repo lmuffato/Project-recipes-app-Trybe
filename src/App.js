23f18afde40715bf5ee1ae4d0c7cbae1a5e8272f
Stanrley Barbosa - Surface <3011stan@gmail.com>
2021-06-30 00:07:49 -0300

    Requisitos do 82 ao 87

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from 'react-router-dom';
+import Provider from './context/Provider';
+import ProfileScreen from './pages/ProfileScreen/ProfileScreen';
+import InitPage from './pages/InitPage/InitPage';
+import RecipesDone from './pages/RecipesDone/RecipesDone';
+import RecipesFav from './pages/RecipesFav/RecipesFav';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Provider>
+      <Switch>
+        <Route exact path="/" component={ InitPage } />
+        <Route path="/perfil" component={ ProfileScreen } />
+        <Route path="/receitas-feitas" component={ RecipesDone } />
+        <Route path="/receitas-favoritas" component={ RecipesFav } />
+      </Switch>
+    </Provider>
   );
 }
 
---
+++ src/pages/InitPage/InitPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const InitPage = () => (
+  <div>
+    <h1>Página Inicial Teste</h1>
+    <Link to="/perfil">Perfil</Link>
+  </div>
+);
+
+export default InitPage;
