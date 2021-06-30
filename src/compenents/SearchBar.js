8d1b9a2d4bf61bf8054712ebb4bda9fbad02dc6a
rafael <rafittu@outlook.com>
2021-06-29 23:09:12 -0300

    feat: req-14

--- src/compenents/SearchBar.js
+++ src/compenents/SearchBar.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchBar() {
+  const [recipes, setRecipes] = useState({});
+  const [name, setName] = useState('');
+  const [request, setRequest] = useState('');
+  const [letter, setLetter] = useState('');
+
+  const handleChange = (type, word) => {
+    setRequest(type);
+    setLetter(word);
+  };
+
+  const getData = async () => {
+    if (letter === 'f' && name.length > 1) {
+      alert('Sua busca deve conter somente 1 (um) caracter');
+    }
+    const endpoint = `https://www.themealdb.com/api/json/v1/1/${request}.php?${letter}=${name}`;
+    await fetch(endpoint).then((data) => data.json())
+      .then((results) => setRecipes(results));
+  };
+  console.log(recipes.meals);
+
   return (
     <div>
       <label htmlFor="search-input">
         <input
           type="text"
           data-testid="search-input"
+          onChange={ (e) => setName(e.target.value) }
         />
       </label>
 
@@ -15,6 +36,7 @@ function SearchBar() {
         <input
           type="radio"
           name="filterType"
+          onChange={ () => handleChange('filter', 'i') }
           data-testid="ingredient-search-radio"
         />
       </label>
@@ -24,6 +46,7 @@ function SearchBar() {
         <input
           type="radio"
           name="filterType"
+          onChange={ () => handleChange('search', 's') }
           data-testid="name-search-radio"
         />
       </label>
@@ -33,11 +56,13 @@ function SearchBar() {
         <input
           type="radio"
           name="filterType"
+          onChange={ () => handleChange('search', 'f') }
           data-testid="first-letter-search-radio"
         />
       </label>
       <button
         type="button"
+        onClick={ () => getData() }
         data-testid="exec-search-btn"
       >
         Buscar
