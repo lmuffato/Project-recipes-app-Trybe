6a0823bcf84f32adbb60f3aac1225f52e43ec456
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-30 20:09:18 -0300

    fix: foods.length

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,67 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { string, bool } from 'prop-types';
-import { useHistory } from 'react-router-dom';
 import profileSvg from '../images/profileIcon.svg';
 import searchSvg from '../images/searchIcon.svg';
-
-import useRecipe from '../hooks/useRecipe';
-
-import { fetchIngredient, fetchName, fetchFirstLetter } from '../services/data';
+import useSearch from '../hooks/useSearch';
 
 export default function Header({ title, searchIcon = false }) {
-  const { setRecipe, recipe } = useRecipe();
-
-  const [searchResult, setSearchResult] = useState('');
-  const [selectedSearch, setSelectedSearch] = useState('');
-  const [showSearch, setShowSearch] = useState(false);
-
-  const history = useHistory();
-  const { pathname } = history.location;
-  const recipeKeyName = pathname === '/comidas' ? 'meals' : 'drinks';
-  const foods = recipe[recipeKeyName];
-
-  const getRecipe = () => {
-    const site = pathname === '/comidas' ? 'meal' : 'cocktail';
-
-    switch (selectedSearch) {
-      case 'ingredient':
-        return fetchIngredient(site, searchResult);
-      case 'name':
-        return fetchName(site, searchResult);
-      case 'firstLetter':
-        if (searchResult.length === 1) {
-          return fetchFirstLetter(site, searchResult);
-        }
-        alert('Sua busca deve conter somente 1 (um) caracter');
-        return { meals: [], drinks: [] };
-      default:
-        return { meals: [], drinks: [] };
-    }
-  };
-
-  const getSearch = async () => {
-    const recipeResponse = await getRecipe();
-    console.log(recipeResponse);
-    setRecipe(recipeResponse);
-  };
-
-  const redirectToMealOrDrink = () => {
-    const idFood = pathname === '/comidas' ? 'idMeal' : 'idDrink';
-
-    if (foods.length === 1 && foods) {
-      history.push(`${pathname}/${foods[0][idFood]}`);
-    }
-  };
-
-  if (foods) redirectToMealOrDrink();
+  const {
+    setSearchResult,
+    setSelectedSearch,
+    showSearch,
+    setShowSearch,
+    foods,
+    getSearch,
+    history,
+    redirectToMealOrDrink,
+  } = useSearch();
 
   if (!foods) {
     alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
   }
 
+  if (foods) redirectToMealOrDrink();
+
   return (
     <header>
-      <button type="button" onClick={() => history.push('/perfil')}>
+      <button type="button" onClick={ () => history.push('/perfil') }>
         <img
           data-testid="profile-top-btn"
           src={ profileSvg }
@@ -72,8 +35,8 @@ export default function Header({ title, searchIcon = false }) {
       <h1 data-testid="page-title">{title}</h1>
 
       {searchIcon && (
-        <button type="button" onClick={() => setShowSearch(!showSearch)}>
-          <img src={searchSvg} alt="Search" data-testid="search-top-btn" />
+        <button type="button" onClick={ () => setShowSearch(!showSearch) }>
+          <img src={ searchSvg } alt="Search" data-testid="search-top-btn" />
         </button>
       )}
 
@@ -83,7 +46,7 @@ export default function Header({ title, searchIcon = false }) {
             <input
               data-testid="search-input"
               placeholder="Buscar Receita"
-              onChange={({ target }) => setSearchResult(target.value)}
+              onChange={ ({ target }) => setSearchResult(target.value) }
             />
 
             <label htmlFor="ingredient-search-radio">
@@ -94,7 +57,7 @@ export default function Header({ title, searchIcon = false }) {
                 name="search-radio"
                 id="ingredient-search-radio"
                 value="ingredient"
-                onChange={({ target }) => setSelectedSearch(target.value)}
+                onChange={ ({ target }) => setSelectedSearch(target.value) }
               />
             </label>
 
@@ -106,7 +69,7 @@ export default function Header({ title, searchIcon = false }) {
                 name="search-radio"
                 id="name-search-radio"
                 value="name"
-                onChange={({ target }) => setSelectedSearch(target.value)}
+                onChange={ ({ target }) => setSelectedSearch(target.value) }
               />
             </label>
 
@@ -118,14 +81,14 @@ export default function Header({ title, searchIcon = false }) {
                 name="search-radio"
                 id="first-letter-search-radio"
                 value="firstLetter"
-                onChange={({ target }) => setSelectedSearch(target.value)}
+                onChange={ ({ target }) => setSelectedSearch(target.value) }
               />
             </label>
 
             <button
               data-testid="exec-search-btn"
               type="button"
-              onClick={getSearch}
+              onClick={ getSearch }
             >
               Buscar
             </button>
