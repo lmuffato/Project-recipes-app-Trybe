8ee57d079c9445e55bf821dd724ddb0dd5238699
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-06-30 16:42:39 -0300

    feat: requisito 8

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,9 +1,11 @@
 import React, { createContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export const UserContext = createContext({});
 
 function UserContextProvider({ children }) {
+  const history = useHistory();
   const [emailData, setEmail] = useState('');
   const [passwordData, setPassword] = useState('');
   const [disabledData, setDisabled] = useState(true);
@@ -19,11 +21,11 @@ function UserContextProvider({ children }) {
   }, [emailData, passwordData]);
 
   const handleClick = () => {
-    console.log(emailData);
     const user = { email: emailData };
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
     localStorage.setItem('user', JSON.stringify(user));
+    history.push('/comidas');
   };
 
   const contextValue = {
