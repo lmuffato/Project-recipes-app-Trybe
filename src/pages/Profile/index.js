73ee9f2334fa1e0ab21e5f804a72d25f229fc782
Mateus Alencar <mtauws@gmail.com>
2021-06-30 15:28:13 -0300

    chore: req 82, 83, 84, 85, 86, 87

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
 
-const Profile = () => (
-  <div>
-    <h1>Profile</h1>
-    <Footer />
-  </div>
+const Profile = () => {
+  const userEmail = JSON.parse(localStorage.getItem('user')) || '';
 
-);
+  const logout = () => {
+    localStorage.clear();
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <span data-testid="profile-email">{ userEmail.email }</span>
+        <div className="buttons">
+          <button type="button" data-testid="profile-done-btn">
+            <Link to="/receitas-feitas">
+              Receitas Feitas
+            </Link>
+          </button>
+          <button type="button" data-testid="profile-favorite-btn">
+            <Link to="/receitas-favoritas">
+              Receitas Favoritas
+            </Link>
+          </button>
+          <button type="button" data-testid="profile-logout-btn" onClick={ logout }>
+            <Link to="/">
+              Sair
+            </Link>
+          </button>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Profile;
--- src/provider/RecipesProvider.jsx
+++ src/provider/RecipesProvider.jsx
@@ -3,24 +3,24 @@ import PropTypes from 'prop-types';
 import RecipesContext from '../context/RecipesContext';
 
 export default function RecipesProvider({ children }) {
-  const handleApi = async ({ textInput, radioInput, place }) => {
-    if (radioInput === ingredient) {
-      const fetched = await fetch(`https://www.the${place}db.com/api/json/v1/1/filter.php?i=${textInput}`);
-      const json = await fetched.json();
-      console.log(json);
-    } else if (radioInput === 'name') {
-      const fetched = await fetch(`https://www.the${place}db.com/api/json/v1/1/search.php?s=${textInput}`);
-      const json = await fetched.json();
-      console.log(json);
-    } else {
-      const fetched = await fetch(`https://www.the${place}db.com/api/json/v1/1/search.php?f=${textInput}`);
-      const json = await fetched.json();
-      console.log(json);
-    }
-  };
+  // const handleApi = async ({ textInput, radioInput, place }) => {
+  //   if (radioInput === ingredient) {
+  //     const fetched = await fetch(`https://www.the${place}db.com/api/json/v1/1/filter.php?i=${textInput}`);
+  //     const json = await fetched.json();
+  //     console.log(json);
+  //   } else if (radioInput === 'name') {
+  //     const fetched = await fetch(`https://www.the${place}db.com/api/json/v1/1/search.php?s=${textInput}`);
+  //     const json = await fetched.json();
+  //     console.log(json);
+  //   } else {
+  //     const fetched = await fetch(`https://www.the${place}db.com/api/json/v1/1/search.php?f=${textInput}`);
+  //     const json = await fetched.json();
+  //     console.log(json);
+  //   }
+  // };
 
   const context = {
-    handleApi,
+    // handleApi,
   };
 
   return (
