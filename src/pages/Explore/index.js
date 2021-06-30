1af12224b5ebb644c9182a58306049b63d256f51
Mateus Alencar <mtauws@gmail.com>
2021-06-30 18:35:42 -0300

    chore: req 67, 68, 69

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
+
+import './style.css';
 
 const Explore = () => (
   <div>
-    <h1>Explore</h1>
+    <div className="buttons">
+      <Link to="/explorar/comidas">
+        <button data-testid="explore-food" type="button">Explorar Comidas</button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-drinks" block>Explorar Bebidas</button>
+      </Link>
+    </div>
     <Footer />
   </div>
 );
