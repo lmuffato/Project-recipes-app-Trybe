2efa6db906e0f05756ae5d634b51693fca21de2b
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-29 21:51:08 -0400

    implementando req 10

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import MainFood from './Pages/MainFood';
+import MainDrink from './Pages/MainDrink';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route path="/comidas" component={ MainFood } />
+      <Route path="/bebidas" component={ MainDrink } />
     </Switch>
   );
 }
---
+++ src/Pages/MainDrink.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function MainDrink() {
+  return (
+    <Header />
+  );
+}
+
+export default MainDrink;
