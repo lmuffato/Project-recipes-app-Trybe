cb6b98543288e2db11e2d888b7e5a93062162ace
pauloeliezerg <pauloeliezerg@gmail.com>
2021-06-30 18:08:07 -0300

    footer tests implemented

---
+++ src/pages/DrinkDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function DrinkDetails() {
+  return (
+    <>
+      <Header title="Detalhes da bebida" />
+      <h1>DrinkDetails</h1>
+    </>
+  );
+}
---
+++ src/pages/FoodProgress.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function FoodProgress() {
+  return (
+    <>
+      <Header title="Receita de Comida em Andamento" />
+      <h1>FoodProgress</h1>
+    </>
+  );
+}
