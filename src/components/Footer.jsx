24e5adb9f89c45cb8e6d282271d2b2435fa1701b
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-30 18:23:38 -0400

    req 19 pronto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import './Footer.css';
 
 function Footer() {
   return (
-    <footer data-testid="footer">
+    <footer data-testid="footer" className="footer">
       <img src={ drinkIcon } data-testid="drinks-bottom-btn" alt="drink icon" />
       <img src={ exploreIcon } data-testid="explore-bottom-btn" alt="explore icon" />
       <img src={ mealIcon } data-testid="food-bottom-btn" alt="food icon" />
