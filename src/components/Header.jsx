114e65c089a9aec8c760fe2c2b277ef60ae4b0fc
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-29 23:44:59 -0300

    feat: req10

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { string, bool } from 'prop-types';
 
-export default function Header({ title }) {
+import profileSvg from '../images/profileIcon.svg';
+import searchSvg from '../images/searchIcon.svg';
+
+export default function Header({ title, searchIcon = false }) {
   return (
-    <button
-      data-testid="profile-top-btn"
-      type="button"
-    >
-      Avatar
-    </button>
+    <header>
+      <button type="button">
+        <img
+          data-testid="profile-top-btn"
+          src={ profileSvg }
+          alt="Profile Avatar"
+        />
+      </button>
+
+      <h1 data-testid="page-title">{title}</h1>
+
+      {searchIcon && (
+        <button type="button">
+          <img src={ searchSvg } alt="Search" data-testid="search-top-btn" />
+        </button>
+      )}
+    </header>
   );
 }
+
+Header.propTypes = {
+  title: string,
+  searchIcon: bool,
+}.isRequired;
--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import Header from '../components/Header';
+import Foods from '../pages/Foods';
 import renderWithRouter from '../helper/renderWithRouter';
 
 describe('Requirement 9', () => {
-  const { getByTestId } = renderWithRouter(<Header />);
-
   it('renders profile-top-btn', () => {
+    const { getByTestId } = renderWithRouter(<Foods />);
     const profileTop = getByTestId('profile-top-btn');
     expect(profileTop).toBeInTheDocument();
   });
 
   it('renders page-title', () => {
+    const { getByTestId } = renderWithRouter(<Foods />);
     const pageTitle = getByTestId('page-title');
     expect(pageTitle).toBeInTheDocument();
   });
 
   it('renders search-top-btn', () => {
+    const { getByTestId } = renderWithRouter(<Foods />);
     const searchTop = getByTestId('search-top-btn');
     expect(searchTop).toBeInTheDocument();
   });
