6830706b580356e1206bfd464feb0aa78e1caf49
MarcioFujita <hanker78@gmail.com>
2021-06-30 16:44:54 -0300

    Requisitos 13 - 15

--- src/services/data.js
+++ src/services/data.js
@@ -1,20 +1,23 @@
-export const fetchIngredient = async (ingredient) => {
-  const URL = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`;
+const genericFetch = async (URL) => {
   const response = await fetch(URL);
   const json = await response.json();
   return json;
 };
 
-export const fetchName = async (name) => {
-  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
-  const response = await fetch(URL);
-  const json = await response.json();
-  return json;
+export const fetchIngredient = async (site, ingredient) => {
+  const URL = `https://www.${site}db.com/api/json/v1/1/filter.php?i=${ingredient}`;
+  const result = await genericFetch(URL);
+  return result;
 };
 
-export const fetchFirstLetter = async (firstLetter) => {
-  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${firstLetter}`;
-  const response = await fetch(URL);
-  const json = await response.json();
-  return json;
+export const fetchName = async (site, name) => {
+  const URL = `https://www.${site}db.com/api/json/v1/1/search.php?s=${name}`;
+  const result = await genericFetch(URL);
+  return result;
+};
+
+export const fetchFirstLetter = async (site, firstLetter) => {
+  const URL = `https://www.${site}db.com/api/json/v1/1/search.php?s=${firstLetter}`;
+  const result = await genericFetch(URL);
+  return result;
 };
