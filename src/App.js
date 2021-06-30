fa5641961fd660ae462d728bd71b12fe5c589504
Stanrley Barbosa - Surface <3011stan@gmail.com>
2021-06-30 15:10:55 -0300

    feat: :rocket: [Grupo 12] Requisitos 82 ao 87

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import React from 'react';
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Switch, Route } from 'react-router-dom';
+import Routes from './Routes';
 import Provider from './context/Provider';
-import ProfileScreen from './pages/ProfileScreen/ProfileScreen';
-import InitPage from './pages/InitPage/InitPage';
-import RecipesDone from './pages/RecipesDone/RecipesDone';
-import RecipesFav from './pages/RecipesFav/RecipesFav';
 
 function App() {
   return (
     <Provider>
-      <Switch>
-        <Route exact path="/" component={ InitPage } />
-        <Route path="/perfil" component={ ProfileScreen } />
-        <Route path="/receitas-feitas" component={ RecipesDone } />
-        <Route path="/receitas-favoritas" component={ RecipesFav } />
-      </Switch>
+      <Routes />
     </Provider>
   );
 }
--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,20 @@
-// import React from 'react';
+import React from 'react';
 // import { Route, Switch } from 'react-dom';
+import { Switch, Route } from 'react-router-dom';
+import ProfileScreen from './pages/ProfileScreen/ProfileScreen';
+import InitPage from './pages/InitPage/InitPage';
+import RecipesDone from './pages/RecipesDone/RecipesDone';
+import RecipesFav from './pages/RecipesFav/RecipesFav';
 
-// function Routes() {
-//   return (
-//     <Route>
-//       <Switch>
-//         <Login />
-//       </Switch>
-//     </Route>
-//   );
-// }
+function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ InitPage } />
+      <Route path="/perfil" component={ ProfileScreen } />
+      <Route path="/receitas-feitas" component={ RecipesDone } />
+      <Route path="/receitas-favoritas" component={ RecipesFav } />
+    </Switch>
+  );
+}
 
-// export default Routes;
+export default Routes;
