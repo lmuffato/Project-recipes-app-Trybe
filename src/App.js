0a08b20a5b86e3bccc575665aad4161941edbeff
Hernanne <hernannedomiciano@gmail.com>
2021-06-29 21:08:47 -0300

    feat: Login screen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Provider from './context/Provider';
+import Routes from './Routes';
 
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+      <Provider>
+        <Routes />
+      </Provider>
     </div>
   );
 }
