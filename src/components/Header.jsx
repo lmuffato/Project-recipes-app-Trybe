a8661923c4b9d3c990bba3c0c4dd17ba8182defc
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-29 23:53:34 -0300

    feat: req11/12

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { string, bool } from 'prop-types';
-
+import { useHistory } from 'react-router-dom';
 import profileSvg from '../images/profileIcon.svg';
 import searchSvg from '../images/searchIcon.svg';
 
 export default function Header({ title, searchIcon = false }) {
+  const [showSearch, setShowSearch] = useState(false);
+  const history = useHistory();
+
   return (
     <header>
-      <button type="button">
+      <button type="button" onClick={ () => history.push('/perfil') }>
         <img
           data-testid="profile-top-btn"
           src={ profileSvg }
@@ -18,10 +21,16 @@ export default function Header({ title, searchIcon = false }) {
       <h1 data-testid="page-title">{title}</h1>
 
       {searchIcon && (
-        <button type="button">
+        <button type="button" onClick={ () => setShowSearch(!showSearch) }>
           <img src={ searchSvg } alt="Search" data-testid="search-top-btn" />
         </button>
       )}
+
+      <div>
+        {showSearch && (
+          <input data-testid="search-input" placeholder="Buscar Receita" />
+        )}
+      </div>
     </header>
   );
 }
