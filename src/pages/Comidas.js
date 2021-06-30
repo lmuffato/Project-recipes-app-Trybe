f9e57f10346e4d160c50033130560e5cd2bbd010
Marcelo Maurício <mmauricio.design@gmail.com>
2021-06-30 13:45:46 -0300

    feat: create footer with links - req 19

--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Footer from '../components/Footer';
+
 function Comidas() {
   return (
     <div>
       <h1>Comidas</h1>
+      <Footer />
     </div>
   );
 }
