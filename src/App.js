c737c90e45b4dcfadb863be6816512a8444d32fe
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-30 14:30:03 -0400

    req 10 pronto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import MainFood from './Pages/MainFood';
