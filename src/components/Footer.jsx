3ab3436a445d443cff44ef8d93cd0962fa12af93
pauloeliezerg <pauloeliezerg@gmail.com>
2021-06-30 15:50:32 -0300

    footer-req20/22/23/24

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import './Footer.css';
 
 export default function Footer() {
+  const history = useHistory();
   return (
-    <footer data-testid="footer">
-      <button type="button" data-testid="drinks-bottom-btn">
-        <img src={ drinkIcon } alt="Drink Icon" />
+    <footer className="footer" data-testid="footer">
+      <button type="button" onClick={ () => history.push('/bebidas') }>
+        <img src={ drinkIcon } alt="Drink Icon" data-testid="drinks-bottom-btn" />
       </button>
-      <button type="button" data-testid="explore-bottom-btn">
-        <img src={ exploreIcon } alt="Explore Icon" />
+      <button type="button" onClick={ () => history.push('/explorar') }>
+        <img src={ exploreIcon } alt="Explore Icon" data-testid="explore-bottom-btn" />
       </button>
-      <button type="button" data-testid="food-bottom-btn">
-        <img src={ mealIcon } alt="Food Icon" />
+      <button type="button" onClick={ () => history.push('/comidas') }>
+        <img src={ mealIcon } alt="Food Icon" data-testid="food-bottom-btn" />
       </button>
     </footer>
   );
--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,2 +1,28 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
+
+import Drinks from '../pages/Drinks';
+import DrinkExplore from '../pages/DrinkExplore';
+import DrinkIngredientExplore from '../pages/DrinkIngredientExplore';
+import Explore from '../pages/Explore';
+import FavoriteRecipes from '../pages/FavoriteRecipes';
+import FoodArea from '../pages/FoodArea';
+import Foods from '../pages/Foods';
+import FoodExplore from '../pages/FoodExplore';
+import FoodIngredientExplore from '../pages/FoodIngredientExplore';
+import Login from '../pages/Login';
+import Profile from '../pages/Profile';
+import RecipesDone from '../pages/RecipesDone';
+
+import renderWithRouter from '../helper/renderWithRouter';
+
+const testids = {
+  footer: 'footer',
+  drinksBottonBtn: 'drinks-bottom-btn',
+  exploreBottonBtn: 'explore-bottom-btn',
+  foodBottonBtn: 'food-bottom-btn',
+};
+
+describe('Requirement 19', ()=>{
+
+})
