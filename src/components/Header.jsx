372f27c545c8121e60a998f847f135a3f6fdaf79
Jodiel Briesemeister <jodielll29@gmail.com>
2021-06-30 16:04:40 -0300

    feat: req12,13

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
@@ -6,28 +6,91 @@ import searchIcon from '../images/searchIcon.svg';
 import './Header.css';
 
 function Header({ props: { search, title } }) {
+  const [searchBar, setSearchBar] = useState(false);
   // const history = useHistory();
   // const { pathname } = history.location;
   // const allName = pathname.split('/');
   // const firstName = `${allName[1].charAt(0).toLocaleUpperCase()}${allName[1].slice(1)}`;
   // const lastName = `${allName[2].charAt(0).toLocaleUpperCase()}${allName[2].slice(1)}`;
+  const showSearchBar = () => {
+    if (searchBar) setSearchBar(false);
+    else setSearchBar(true);
+  };
+
+  const showSearchButton = () => {
+    if (search) {
+      return (
+        <button type="button" onClick={ showSearchBar }>
+          <img
+            src={ searchIcon }
+            data-testid="search-top-btn"
+            alt="search icon"
+          />
+        </button>
+      );
+    }
+  };
+
+  const renderSearchBar = () => {
+    if (searchBar) {
+      return (
+        <form>
+          <input
+            type="text"
+            placeholder="Buscar Receita"
+            data-testid="search-input"
+          />
+          <label htmlFor="ingredient">
+            <input
+              type="radio"
+              id="ingredient"
+              name="searchRadio"
+              data-testid="ingredient-search-radio"
+            />
+            Ingrediente
+          </label>
+          <label htmlFor="name">
+            <input
+              type="radio"
+              id="name"
+              name="searchRadio"
+              data-testid="name-search-radio"
+            />
+            Nome
+          </label>
+          <label htmlFor="firstLetter">
+            <input
+              type="radio"
+              id="firstLetter"
+              name="searchRadio"
+              data-testid="first-letter-search-radio"
+            />
+            Primeira Letra
+          </label>
+          <button
+            type="button"
+            data-testid="exec-search-btn"
+          >
+            Buscar
+          </button>
+        </form>
+      );
+    }
+  };
 
   return (
-    <header>
-      <img src={ profileIcon } data-testid="profile-top-btn" alt="profile icon" />
-      <h1
-        data-testid="page-title"
-      >
-        {title}
-      </h1>
-      {
-        search && <img
-          src={ searchIcon }
-          data-testid="search-top-btn"
-          alt="search icon"
-        />
-      }
-    </header>
+    <div>
+      <header>
+        <img src={ profileIcon } data-testid="profile-top-btn" alt="profile icon" />
+        <h1
+          data-testid="page-title"
+        >
+          {title}
+        </h1>
+        { showSearchButton() }
+      </header>
+      { renderSearchBar() }
+    </div>
   );
 }
 
