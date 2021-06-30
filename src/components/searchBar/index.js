9fc7e66504107068ebd6722a3be09bbcc81e1f5f
Alexandre Oliveira <alexandremolive@gmail.com>
2021-06-30 18:30:25 -0300

    feat-req-14-done

--- src/components/searchBar/index.js
+++ src/components/searchBar/index.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  fetchByIngredientApi,
+  fetchByNameApi,
+  fetchByFirstLetterApi,
+} from '../../services/fetchApiRadio';
 
 export default function SearchBar() {
-  function generateRadioButtons(name, label, onChange, dataTest) {
+  const [searchValue, setSearchValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  let searchResults = '';
+  const length = 1;
+  async function getApiSearch() {
+    switch (searchValue) {
+    case 'ingredient-search':
+      searchResults = await fetchByIngredientApi(inputValue);
+      console.log(searchResults);
+      break;
+    case 'name-search':
+      searchResults = await fetchByNameApi(inputValue);
+      console.log(searchResults);
+      break;
+    case 'first-letter-search':
+      if (inputValue.length > length) {
+        alert('Sua busca deve conter somente 1 (um) caracter');
+        return;
+      }
+      searchResults = await fetchByFirstLetterApi(inputValue);
+      console.log(searchResults);
+      break;
+
+    default:
+      return alert('nao encontrado');
+    }
+  }
+  function generateRadioButtons(value, label, dataTest) {
     return (
-      <label htmlFor={ name }>
+      <label htmlFor="search-radio">
         {label}
         <input
-          id={ name }
-          name={ name }
-          onChange={ onChange }
+          value={ value }
+          name="search-radio"
+          onClick={ (ev) => setSearchValue(ev.target.value) }
           type="radio"
           data-testid={ dataTest }
         />
@@ -21,15 +54,20 @@ export default function SearchBar() {
       <input
         data-testid="search-input"
         type="text"
+        value={ inputValue }
+        onChange={ (ev) => setInputValue(ev.target.value) }
       />
 
-      {generateRadioButtons('name', 'Ingrediente', 'onChange', 'ingredient-search-radio')}
-      {generateRadioButtons('name', 'Nome', 'onChange', 'name-search-radio')}
-      {generateRadioButtons('name', 'Primeira letra',
-        'onChange', 'first-letter-search-radio')}
+      {generateRadioButtons('ingredient-search',
+        'Ingrediente', 'ingredient-search-radio')}
+      {generateRadioButtons('name-search',
+        'Nome', 'name-search-radio')}
+      {generateRadioButtons('first-letter-search',
+        'Primeira letra', 'first-letter-search-radio')}
       <button
         data-testid="exec-search-btn"
         type="button"
+        onClick={ getApiSearch }
       >
         Buscar
 
