dcd2270bec505e62b61a72f120a6ec5abc7838bd
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-06-30 14:28:48 -0300

    fix: req21 pt1

--- src/Routes.js
+++ src/Routes.js
@@ -8,11 +8,15 @@ import ExplorarBebidas from './Pages/ExplorarBebidas';
 import ExplorarComida from './Pages/ExplorarComida';
 import ExplorarIngrediente from './Pages/ExplorarIngrediente';
 import Perfil from './Pages/Perfil';
+import ExplorarOrigem from './Pages/ExplorarOrigem';
 import ReceitasFavoritas from './Pages/ReceitasFavoritas';
+import NaoEncontrado from './Pages/NaoEncontrado';
+import ReceitasFeitas from './Pages/ReceitasFeitas';
 
 class Routes extends React.Component {
   render() {
     return (
+      // Dica da Ana Ventura no slack da turma
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Login } />
@@ -21,13 +25,19 @@ class Routes extends React.Component {
           <Route exact path="/explorar" component={ Explorar } />
           <Route exact path="/explorar/comidas" component={ ExplorarComida } />
           <Route exact path="/explorar/bebidas" component={ ExplorarBebidas } />
+          <Route path="/explorar/comidas/area" component={ ExplorarOrigem } />
+          <Route path="/explorar/bebidas/area" component={ NaoEncontrado } />
           <Route
-            exact
             path="/explorar/comidas/ingredientes"
+            component={ ExplorarIngrediente }
+          />
+          <Route
+            path="/explorar/bebidas/ingredientes"
             component={ ExplorarIngrediente }
           />
           <Route path="/perfil" component={ Perfil } />
           <Route path="/receitas-favoritas" component={ ReceitasFavoritas } />
+          <Route path="/receitas-feitas" component={ ReceitasFeitas } />
         </Switch>
       </BrowserRouter>
     );
