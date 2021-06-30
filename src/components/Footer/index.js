f5311984ed3a4e555e9889efc96aacfc68834b9e
Mateus Alencar <mtauws@gmail.com>
2021-06-30 09:41:57 -0300

    chore: footer changes and page creation

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import foodIcon from '../images/mealIcon.svg';
-import drinkIcon from '../images/drinkIcon.svg';
-import searchIcon from '../images/searchIcon.svg';
+import foodIcon from '../../images/mealIcon.svg';
+import drinkIcon from '../../images/drinkIcon.svg';
+import searchIcon from '../../images/searchIcon.svg';
 
 export default function FooterMenu() {
   return (
-    <div data-testid="footer">
+    <div data-testid="footer" className="footer">
       <Link data-testid="drinks-bottom-btn" to="/bebidas">{ drinkIcon }</Link>
       <Link data-testid="explore-bottom-btn" to="/explorar">{ searchIcon }</Link>
       <Link data-testid="food-bottom-btn" to="/comidas">{ foodIcon }</Link>
---
+++ src/pages/MainDrinks/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MainDrinks = () => (
+  <h1>MainDrinks</h1>
+);
+
+export default MainDrinks;
