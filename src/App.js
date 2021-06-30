34c3e6485df5b7ad160e415d44a3aa754c96ae58
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-06-30 14:27:00 -0400

    feat: routes file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import UserProvider from './context/UserProvider';
 import RecipeProvider from './context/RecipeProvider';
-import Login from './pages/Login';
+import Routes from './services/Routes';
 
 function App() {
   return (
     <RecipeProvider>
       <UserProvider>
         <BrowserRouter>
-          <Switch>
-            <Route path="/" component={ Login } />
-          </Switch>
+          <Routes />
         </BrowserRouter>
       </UserProvider>
     </RecipeProvider>
