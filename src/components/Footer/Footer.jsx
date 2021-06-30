648496cf999fb4592b127e163b8530abb60ca0f7
Guilherme de Prais <guilherme.prais@gmail.com>
2021-06-29 21:04:38 -0300

    feat: create component footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import drinkImg from '../../images/drinkIcon.svg';
+import foodImg from '../../images/mealIcon.svg';
+import exploreImg from '../../images/exploreIcon.svg';
 
 export default function Footer() {
   return (
-    <div>
-      Footer!
-    </div>
-  )
+    <footer data-testid="footer">
+      <ul>
+        <li>
+          <Link to="/bebidas" data-testid="drinks-bottom-btn">
+            <img src={ drinkImg } alt="Ícone de bebida" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/explorar" data-testid="explore-bottom-btn">
+            <img src={ exploreImg } alt="Ícone de bússola" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/comidas" data-testid="food-bottom-btn">
+            <img src={ foodImg } alt="Ícone de comida" />
+          </Link>
+        </li>
+      </ul>
+    </footer>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Footer from '../components/Footer/Footer';
 
 function Home() {
   return (
     <div>
       Tela principal
+      <Footer />
     </div>
   );
 }
