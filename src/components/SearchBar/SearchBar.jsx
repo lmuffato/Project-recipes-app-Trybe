65d021ed666a1ec4b1326b6acfd5e191e20d479c
Ana Ventura <anaventura1811@gmail.com>
2021-06-30 02:55:40 -0300

    style: header & searchbar css

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,41 +1,44 @@
 import React from 'react';
 import Button from '../Generics/Button';
+import SearchBarContainer from './styles';
 
 function SearchBar() {
   return (
-    <form>
+    <SearchBarContainer>
       <input type="text" placeholder="Buscar receita" data-testid="search-input" />
-      <label htmlFor="ingredient">
-        <input
-          type="radio"
-          name=""
-          id="ingredient"
-          data-testid="ingredient-search-radio"
-        />
-        Ingredient
-      </label>
-      <label htmlFor="name">
-        <input
-          type="radio"
-          name=""
-          id="name"
-          data-testid="name-search-radio"
-        />
-        Name
-      </label>
-      <label htmlFor="first-letter">
-        <input
-          type="radio"
-          name=""
-          id="first-letter"
-          data-testid="first-letter-search-radio"
-        />
-        First letter
-      </label>
+      <div className="form-control">
+        <label htmlFor="ingredient">
+          <input
+            type="radio"
+            name=""
+            id="ingredient"
+            data-testid="ingredient-search-radio"
+          />
+          Ingredient
+        </label>
+        <label htmlFor="name">
+          <input
+            type="radio"
+            name=""
+            id="name"
+            data-testid="name-search-radio"
+          />
+          Name
+        </label>
+        <label htmlFor="first-letter">
+          <input
+            type="radio"
+            name=""
+            id="first-letter"
+            data-testid="first-letter-search-radio"
+          />
+          First letter
+        </label>
+      </div>
       <Button data-testid="exec-search-btn">
         Buscar
       </Button>
-    </form>
+    </SearchBarContainer>
   );
 }
 
