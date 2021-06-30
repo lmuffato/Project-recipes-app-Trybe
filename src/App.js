be135805b3dc8b15c73e2cdbb42ab330cac5b781
iago <iago.pferreiravr@gmail.com>
2021-06-30 15:22:20 -0300

    Feat: context

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './compenents/Footer';
+import MainRecepies from './compenents/MainRecepies';
+import Provider from './contexts/Provider';
 
 function App() {
   return (
-    <div className="meals">
-      <Footer />
-    </div>
+    <Provider>
+      <div className="meals">
+        <MainRecepies />
+        <Footer />
+      </div>
+    </Provider>
   );
 }
 
