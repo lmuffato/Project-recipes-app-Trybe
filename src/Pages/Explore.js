e4f0aa62137f350922b77e0a2aa02c58eab46ae9
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-30 22:52:42 -0400

    adicionado css na tela explorar

--- src/Pages/Explore.js
+++ src/Pages/Explore.js
@@ -2,24 +2,31 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import './Pages Css/Explore.css';
 
 function Explore() {
   return (
     <>
       <Header props={ { search: false, title: 'Explorar' } } />
-      <main>
-        <Button
-          variant="primary"
-          data-testid="explore-food"
-        >
-          Explorar Comidas
-        </Button>
-        <Button
-          variant="primary"
-          data-testid="explore-drinks"
-        >
-          Explorar Bebidas
-        </Button>
+      <main className="main-container">
+        <section className="main-explore">
+          <Button
+            variant="light"
+            size="lg"
+            className="buttons"
+            data-testid="explore-food"
+          >
+            Explorar Comidas
+          </Button>
+          <Button
+            variant="light"
+            size="lg"
+            className="buttons"
+            data-testid="explore-drinks"
+          >
+            Explorar Bebidas
+          </Button>
+        </section>
       </main>
       <Footer />
     </>
