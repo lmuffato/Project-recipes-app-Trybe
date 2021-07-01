38e745dcf5394406751028cb71b71dcdd8744db0
Walvesb65 <willian.alvesb65@gmail.com>
2021-07-01 19:07:10 -0300

    Feat-action-api-start

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 // pagina actions
 export const HANDLE_CHANGE_USER = 'HANDLE_CHANGE_USER';
+export const HANDLE_CLICK_RADIO_BUTTON = 'HANDLE_CLICK_RADIO_BUTTON';
 
 export const handleChangeUser = (name, value) => ({
   type: HANDLE_CHANGE_USER,
@@ -8,3 +9,20 @@ export const handleChangeUser = (name, value) => ({
     value,
   },
 });
+
+export const handleClickRadioButton = (payload) => ({
+  type: HANDLE_CLICK_RADIO_BUTTON,
+  payload,
+});
+
+const requestApiOfRecipes = async (value) => {
+  const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${value}`;
+  const api = await fetch(endpoint);
+  const jsonTreatment = api.json();
+  return jsonTreatment;
+};
+
+export const actionTunk = () => async (dispatch) => {
+  const getApi = await requestApiOfRecipes();
+  dispatch(handleClickRadioButton(getApi));
+};
