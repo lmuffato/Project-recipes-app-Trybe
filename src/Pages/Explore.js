9fb5d38d15c5c2e383a167be4263d5aaaa171387
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-30 23:00:51 -0400

    req 67, 68, 69 prontos

--- src/Pages/Explore.js
+++ src/Pages/Explore.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import './Pages Css/Explore.css';
 
 function Explore() {
+  const history = useHistory();
   return (
     <>
       <Header props={ { search: false, title: 'Explorar' } } />
@@ -15,6 +17,7 @@ function Explore() {
             size="lg"
             className="buttons"
             data-testid="explore-food"
+            onClick={ () => history.push('/explorar/comidas') }
           >
             Explorar Comidas
           </Button>
@@ -23,6 +26,7 @@ function Explore() {
             size="lg"
             className="buttons"
             data-testid="explore-drinks"
+            onClick={ () => history.push('/explorar/bebidas') }
           >
             Explorar Bebidas
           </Button>
