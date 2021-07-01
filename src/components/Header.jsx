e3d99d4fd1eeba20e70762f46596b9c0c64d362c
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-06-30 23:30:13 -0400

    req 67, 68, 69 ,70 e 71 - prontos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
-import './Header.css';
 import { fetchSearch } from '../redux/actions';
+import './Header.css';
 
 function Header({ props: { search, title } }) {
   const [searchBar, setSearchBar] = useState(false);
