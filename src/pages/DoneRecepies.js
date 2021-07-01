4959cefdad2b468fd02183b4a2ce2e6478bc23d2
hheloisa <hhackenhaar@gmail.com>
2021-06-30 21:24:33 -0300

    Feat: Req 55-56, estrutura basica imaginada

--- src/pages/DoneRecepies.js
+++ src/pages/DoneRecepies.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Share from '../images/shareIcon.svg';
 
 function DoneRecepies () {
-
+  // myDoneRecepies = array com as receitas feitas
   return(
     <div>
     <button
@@ -19,6 +20,18 @@ function DoneRecepies () {
     >
       Drinks
     </button>
+    {myDoneRecepies.map((recepie, index) => {
+      if (recepie.type === meal) {
+        <div>
+        <img data-testid={`${index}-horizontal-image`}>{ recepie.img }</img>
+        <p data-testid={`${index}-horizontal-top-text>`}>{recepie.category}</p>
+        <p data-testid={`${index}-horizontal-name`}>{recepie.name}</p>
+        <p data-testid={`${index}-horizontal-done-date`}>{recepie.date}</p>
+        <img data-testid={`${index}-horizontal-share-btn`} src={Share} alt="share"></img>
+        <p data-testid={`${index}-${tagName}-horizontal-tag`}>{recepie.tag}</p>
+      }
+      </div>
+  )}}
       </div>
   )
 }
