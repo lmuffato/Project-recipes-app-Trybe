7406b97d8838e2e65844f2e56ad80a681c97c2ca
AndreBarroso <andre_barroso17@hotmail.com>
2021-07-01 17:01:39 -0300

    feat: requirements: 19, 20, 21

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
-import Footer from '../components/Footer';
 
 function FavoriteRecipes() {
   return (
     <div>
       Pagina FavoriteRecipes
       <Header title="Receitas Favoritas" />
-      <Footer />
-
     </div>
   );
 }
