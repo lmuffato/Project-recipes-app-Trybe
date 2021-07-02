e94cc8150fe36fec16980fe0a7af6c96c154ca52
rafael <rafittu@outlook.com>
2021-07-02 15:20:50 -0300

    fix package and npm install

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './compenents/Footer';
 import UserProvider from './contexts/UserProvider';
+// import SearchBar from './compenents/SearchBar';
 
 function App() {
   return (
     <div className="meals">
       <UserProvider>
         <Switch>
+          {/* <Route path="/comidas/:detalhes" component={} /> */}
           <Route exact path="/" component={ LoginPage } />
         </Switch>
+        {/* <SearchBar /> */}
       </UserProvider>
       <Footer />
     </div>
