df2999d31a76888da37e9c8e49339bde9a051a03
Elisa <elisafranca.carvalho@gmail.com>
2021-07-02 17:42:53 -0300

    feat: requirements 70 and 71

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import Header from '../components/Header';
 
 export default function ExploreDrinks() {
@@ -7,6 +8,20 @@ export default function ExploreDrinks() {
       <Header>
         <h1 data-testid="page-title">Explorar Bebidas</h1>
       </Header>
+      <Button
+        variant="dark"
+        data-testid="explore-by-ingredient"
+        name="ingredient"
+      >
+        Por Ingredientes
+      </Button>
+      <Button
+        variant="dark"
+        data-testid="explore-surprise"
+        name="ingredient"
+      >
+        Me Surpreenda!
+      </Button>
     </div>
   );
 }
--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import Header from '../components/Header';
 
 export default function ExploreFood() {
@@ -7,6 +8,27 @@ export default function ExploreFood() {
       <Header>
         <h1 data-testid="page-title">Explorar Comidas</h1>
       </Header>
+      <Button
+        variant="dark"
+        data-testid="explore-by-ingredient"
+        name="ingredient"
+      >
+        Por Ingredientes
+      </Button>
+      <Button
+        variant="dark"
+        data-testid="explore-by-area"
+        name="area"
+      >
+        Por Local de Origem
+      </Button>
+      <Button
+        variant="dark"
+        data-testid="explore-surprise"
+        name="ingredient"
+      >
+        Me Surpreenda!
+      </Button>
     </div>
   );
 }
