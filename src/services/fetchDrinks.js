3a00f5e1b421c7463705d338eb2039e83e8e35e1
Viegas <mvrdgs@gmail.com>
2021-07-02 00:22:19 -0300

    fix(meals/drinks): fix filter endpoint

--- src/services/fetchDrinks.js
+++ src/services/fetchDrinks.js
@@ -1,5 +1,6 @@
 export default async function fetchDrinks(filter) {
-  const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${filter || ''}`;
+  const endpoint = filter ? `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filter}`
+    : 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
   const { drinks } = await (await fetch(endpoint)).json();
   return drinks || [];
 }
