4a33ce72ae0d18264610702b6a1482ba65807a1f
Edrei <edreimateus@gmail.com>
2021-07-03 10:59:45 -0300

    fix: bugs

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router';
+// import { useParams } from 'react-router';
 
 function Details() {
-  const params = useParams();
   return (
-    <h1>{console.log(params)}</h1>
+    <section>Details</section>
   );
 }
 
