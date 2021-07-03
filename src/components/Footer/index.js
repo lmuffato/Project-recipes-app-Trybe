6716a7179d18d4d29d37bfa830d48652abe35249
Mateus Alencar <mtauws@gmail.com>
2021-07-03 13:56:43 -0300

    footer arrow function

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,32 +5,30 @@ import exploreIcon from '../../images/exploreIcon.svg';
 import drinkIcon from '../../images/drinkIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
 
-function Footer() {
-  return (
-    <div className="footer" data-testid="footer">
-      <Link to="/bebidas">
-        <img
-          alt="drinks buttom"
-          src={ drinkIcon }
-          data-testid="drinks-bottom-btn"
-        />
-      </Link>
-      <Link to="/explorar">
-        <img
-          alt="search buttom"
-          src={ exploreIcon }
-          data-testid="explore-bottom-btn"
-        />
-      </Link>
-      <Link to="/comidas">
-        <img
-          alt="meal buttom"
-          src={ mealIcon }
-          data-testid="food-bottom-btn"
-        />
-      </Link>
-    </div>
-  );
-}
+const Footer = () => (
+  <div className="footer" data-testid="footer">
+    <Link to="/bebidas">
+      <img
+        alt="drinks buttom"
+        src={ drinkIcon }
+        data-testid="drinks-bottom-btn"
+      />
+    </Link>
+    <Link to="/explorar">
+      <img
+        alt="search buttom"
+        src={ exploreIcon }
+        data-testid="explore-bottom-btn"
+      />
+    </Link>
+    <Link to="/comidas">
+      <img
+        alt="meal buttom"
+        src={ mealIcon }
+        data-testid="food-bottom-btn"
+      />
+    </Link>
+  </div>
+);
 
 export default Footer;
