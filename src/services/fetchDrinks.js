3204095498c836ff90fd35f65315aaf46391ed53
Viegas <mvrdgs@gmail.com>
2021-07-03 18:51:50 -0300

    feat(search bar): if only one recipe is found it automaticly redirects to it's details page

--- src/services/fetchDrinks.js
+++ src/services/fetchDrinks.js
@@ -8,17 +8,17 @@ export default async function fetchDrinks(filter) {
 export async function fetchMealSearched(text, option) {
   if (option === 'ingredient') {
     const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${text}`;
-    const { meals } = await (await fetch(endpoint)).json();
-    return meals || [];
+    const { drinks } = await (await fetch(endpoint)).json();
+    return drinks || [];
   }
   if (option === 'name') {
     const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${text}`;
-    const { meals } = await (await fetch(endpoint)).json();
-    return meals || [];
+    const { drinks } = await (await fetch(endpoint)).json();
+    return drinks || [];
   }
   if (option === 'first-letter') {
     const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${text}`;
-    const { meals } = await (await fetch(endpoint)).json();
-    return meals || [];
+    const { drinks } = await (await fetch(endpoint)).json();
+    return drinks || [];
   }
 }
