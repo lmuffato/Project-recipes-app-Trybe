0ff1210cf430eccb9005b698e7500cf78cbbf65d
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-07-04 15:33:44 -0300

    Feat(sugestao): pastas organizadas e index.js criado

---
+++ src/Pages/Foods/FoodId.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FoodId() {
+  return (
+    <div>
+      Food Id
+    </div>
+  );
+}
+
+export default FoodId;
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
 
 function Profile() {
   const [userEmail, setUserEmail] = useState('');
