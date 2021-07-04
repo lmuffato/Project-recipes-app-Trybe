38b11c1c24b2cf56dae364955e9786b23626e859
Viegas <mvrdgs@gmail.com>
2021-07-04 01:12:48 -0300

    feat(footer): implement links and css

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -1,35 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import drinkIcon from '../../images/drinkIcon.svg';
 import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
+import './Footer.css';
 
 function Footer() {
   return (
-    <Container as="footer" data-testid="footer">
+    <Container as="footer" className="footer-menu" data-testid="footer">
       <Row>
         <Col>
-          <img
-            src={ drinkIcon }
-            alt="Drink icon"
-            data-testid="drinks-bottom-btn"
-          />
+          <Link to="/bebidas">
+            <img
+              src={ drinkIcon }
+              alt="Drink icon"
+              data-testid="drinks-bottom-btn"
+            />
+          </Link>
         </Col>
         <Col>
-          <img
-            src={ exploreIcon }
-            alt="Explore icon"
-            data-testid="explore-bottom-btn"
-          />
+          <Link to="/explorar">
+            <img
+              src={ exploreIcon }
+              alt="Explore icon"
+              data-testid="explore-bottom-btn"
+            />
+          </Link>
         </Col>
         <Col>
-          <img
-            src={ mealIcon }
-            alt="Meal icon"
-            data-testid="food-bottom-btn"
-          />
+          <Link to="/comidas">
+            <img
+              src={ mealIcon }
+              alt="Meal icon"
+              data-testid="food-bottom-btn"
+            />
+          </Link>
         </Col>
       </Row>
     </Container>
