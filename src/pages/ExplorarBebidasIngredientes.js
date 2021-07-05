94492a354ef912797af773975131d054cbe479c2
biazidioti <bzferreira.bio@gmail.com>
2021-07-05 18:57:40 -0300

    fix lint and save file.

--- src/pages/ExplorarBebidasIngredientes.js
+++ src/pages/ExplorarBebidasIngredientes.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 function ExplorarBebidasIngredientes() {
-<<<<<<< Updated upstream
-  return <div>Página de Explorar Bebidas por Ingredientes</div>;
-=======
   return (
     <section>
       <Header title="Explorar Ingredientes" show={ false } />
@@ -11,7 +8,6 @@ function ExplorarBebidasIngredientes() {
         <Footer />
       </footer>
     </section>);
->>>>>>> Stashed changes
 }
 
 export default ExplorarBebidasIngredientes;
