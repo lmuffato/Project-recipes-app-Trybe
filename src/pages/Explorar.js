4d49a821b7bb64caf87fd94c5c06e98f9ca7b479
andy silva <andssilva.23@gmail.com>
2021-07-05 07:57:04 -0400

    feat: :rocket: style Explore page

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -3,15 +3,29 @@ import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
+import styles from '../styles/Explore.module.scss';
+
 function Explorar() {
   return (
-    <div>
+    <div className={ styles.container }>
       <Header title="Explorar" />
-      <Link to="/explorar/comidas" data-testid="explore-food">
-        Explorar Comidas
+      <Link
+        to="/explorar/comidas"
+        data-testid="explore-food"
+        className={ styles.redirect }
+      >
+        <button type="button">
+          Explorar Comidas
+        </button>
       </Link>
-      <Link to="/explorar/bebidas" data-testid="explore-drinks">
-        Explorar Bebidas
+      <Link
+        to="/explorar/bebidas"
+        data-testid="explore-drinks"
+        className={ styles.redirect }
+      >
+        <button type="button">
+          Explorar Bebidas
+        </button>
       </Link>
       <Footer />
     </div>
