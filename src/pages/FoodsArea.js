027da71ddbbd0bf3fe34574311a61ca29eef47a2
hheloisa <hhackenhaar@gmail.com>
2021-07-05 19:12:30 -0300

    Feat: Inicia Req. 78

--- src/pages/FoodsArea.js
+++ src/pages/FoodsArea.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function FoodsArea() {
+  const [areas, setAreas] = useState([]);
+  useEffect(() => {
+    const getAreas = async () => {
+      const fetchArea = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+        .then((response) => response.json())
+        .then((respo) => respo);
+      const allAreas = fetchArea.meals.map((country) => (
+        country.strArea
+      ));
+      console.log(allAreas);
+      setAreas(allAreas);
+    }
+    getAreas();
+  }, [])
   return (
-    <div>FoodsArea</div>
+    <div>
+      <div>FoodsArea</div>
+      <select data-testid="explore-by-area-dropdown">
+    {areas.map((area, index) => (
+      <option key={index} data-testid={`${area}-option`}>{area}</option>
+    ))}
+      </select>
+    </div>
   );
 }
 
