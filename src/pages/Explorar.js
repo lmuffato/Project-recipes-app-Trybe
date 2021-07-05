d58644af044ce72019755c623caa72e012d2c956
andy silva <andssilva.23@gmail.com>
2021-07-05 07:20:03 -0400

    feat(reqs 67, 68, 69): :rocket: explore screen

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +7,12 @@ function Explorar() {
   return (
     <div>
       <Header title="Explorar" />
+      <Link to="/explorar/comidas" data-testid="explore-food">
+        Explorar Comidas
+      </Link>
+      <Link to="/explorar/bebidas" data-testid="explore-drinks">
+        Explorar Bebidas
+      </Link>
       <Footer />
     </div>
   );
