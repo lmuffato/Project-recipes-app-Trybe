5bbcc55a795a6711e587cb92198a99a5a02e1f74
biazidioti <bzferreira.bio@gmail.com>
2021-07-05 17:45:39 -0300

    Profile page!

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Perfil() {
-  return <div>Página de Perfil</div>;
+  const userEmailStorage = JSON.parse(localStorage.getItem('user'));
+  const userEmail = userEmailStorage.email;
+
+  const clearStorage = () => localStorage.clear();
+
+  return (
+    <>
+      <span data-testid="profile-email">{userEmail}</span>
+      <Link to="/receitas-feitas">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ clearStorage }
+        >
+          Sair
+        </button>
+      </Link>
+    </>
+  );
 }
 
 export default Perfil;
