2e7c2e63d5e9114a9d6ed7d5862ac2b0b0ad57ed
andy silva <andssilva.23@gmail.com>
2021-07-05 19:35:24 -0400

    feat(req 33): :rocket: implements the elements on the screen

--- src/pages/drinks/DrinkDetails.js
+++ src/pages/drinks/DrinkDetails.js
@@ -7,6 +7,17 @@ function DrinkDetails(props) {
     <div>
       Bebida de ID :
       { id }
+      <div className="teste-details">
+        <img src="" alt="" data-testid="recipe-photo" />
+        <h1 data-testid="recipe-title">título</h1>
+        <button type="button" data-testid="share-btn">compartilhar</button>
+        <button type="button" data-testid="favorite-btn">favoritar</button>
+        <p data-testid="recipe-category">categoria</p>
+        <li data-testid="0-ingredient-name-and-measure">um</li>
+        <p data-testid="instructions">texto</p>
+        <div data-testid="0-recomendation-card">recomendação</div>
+        <button type="button" data-testid="start-recipe-btn">Iniciar</button>
+      </div>
     </div>
   );
 }
--- src/pages/foods/FoodDetails.js
+++ src/pages/foods/FoodDetails.js
@@ -5,8 +5,20 @@ function FoodDetails(props) {
   const { match: { params: { id } } } = props;
   return (
     <div>
-      Comida de ID :
+      Comida
       { id }
+      <div className="teste-details">
+        <img src="" alt="" data-testid="recipe-photo" />
+        <h1 data-testid="recipe-title">título</h1>
+        <button type="button" data-testid="share-btn">compartilhar</button>
+        <button type="button" data-testid="favorite-btn">favoritar</button>
+        <p data-testid="recipe-category">categoria</p>
+        <li data-testid="0-ingredient-name-and-measure">um</li>
+        <p data-testid="instructions">texto</p>
+        <div data-testid="video">vídeo</div>
+        <div data-testid="0-recomendation-card">recomendação</div>
+        <button type="button" data-testid="start-recipe-btn">Iniciar</button>
+      </div>
     </div>
   );
 }
@@ -20,3 +32,10 @@ FoodDetails.propTypes = {
 };
 
 export default FoodDetails;
+
+/* const urlRecipie = verifyPath
+      ? 'https://www.themealdb.com/api/json/v1/1/lookup.php?i='
+      : 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+    const fecthMeal = await fetch(`${urlRecipie}${element}`);
+    const resultMeal = await fecthMeal.json();
+    setStateRedux({ actionType: 'FETCH_RECIPIE', resultMeal }); */
