cab4ad2c7ed7a93274d640ba7f05cbbda987e8a8
Apoc35 <alexv_silveira@outlook.com>
2021-07-04 22:05:15 -0300

    Requisito 19.

---
+++ src/components/InferiorMenu.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function InferiorMenu() {
+  return (
+    <div>
+      <footer data-testid="footer">
+        <Link src={ drinkIcon } data-testid="drinks-bottom-btn" to="/bebidas">
+          <img src={ drinkIcon } alt="Drinks" />
+        </Link>
+
+        <Link src={ exploreIcon } data-testid="explore-bottom-btn" to="/explorar">
+          <img src={ exploreIcon } alt="Explorar" />
+        </Link>
+
+        <Link src={ mealIcon } data-testid="food-bottom-btn" to="/comidas">
+          <img src={ mealIcon } alt="Comidas" />
+        </Link>
+
+      </footer>
+    </div>
+  );
+}
+
+export default InferiorMenu;
