8a994d85c0b3165ff9743f5a0e986a85f463a33b
hheloisa <hhackenhaar@gmail.com>
2021-07-05 14:49:10 -0300

    Comenta codigo incompleto

--- src/pages/DoneRecepies.js
+++ src/pages/DoneRecepies.js
@@ -20,19 +20,27 @@ function DoneRecepies () {
     >
       Drinks
     </button>
-    {myDoneRecepies.map((recepie, index) => {
+    {/* {myDoneRecepies.map((recepie, index) => (
       if (recepie.type === meal) {
         <div>
         <img data-testid={`${index}-horizontal-image`}>{ recepie.img }</img>
-        <p data-testid={`${index}-horizontal-top-text>`}>{recepie.category}</p>
         <p data-testid={`${index}-horizontal-name`}>{recepie.name}</p>
+        <p data-testid={`${index}-horizontal-top-text>`}>{recepie.category}</p>
+        <p>area?</p>
+        <p data-testid={`${index}-horizontal-done-date`}>{recepie.date}</p>
+        <p data-testid={`${index}-${tagName}-horizontal-tag`}>{recepie.tagName[0]}{recepie.tagName[1]}</p>
+        <img data-testid={`${index}-horizontal-share-btn`} src={Share} alt="share"></img>
+      </div>
+    } else if (recepie.type === drink) {
+      <div>
+        <img data-testid={`${index}-horizontal-image`}>{ recepie.img }</img>
+        <p>alcólica:{recepie.alcoolic}</p> 
         <p data-testid={`${index}-horizontal-done-date`}>{recepie.date}</p>
         <img data-testid={`${index}-horizontal-share-btn`} src={Share} alt="share"></img>
-        <p data-testid={`${index}-${tagName}-horizontal-tag`}>{recepie.tag}</p>
-      }
       </div>
-  )}}
-      </div>
+    }
+    ) */}
+    </div>
   )
 }
 
