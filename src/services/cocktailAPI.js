8b74f2cab2bff33ac573da082c9cb91f5415af07
guidugaich <guidugaich.dev@gmail.com>
2021-07-05 20:23:35 -0300

    adicionando funcoes extras nas chamadas de API

--- src/services/cocktailAPI.js
+++ src/services/cocktailAPI.js
@@ -1,5 +1,7 @@
 const COCKTAIL_API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const COCKTAIL_CATEGORIES_API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+const COCKTAIL_API_FILTER_URL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+const COCKTAIL_API_FIRST_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
 
 export async function fetchCocktails() {
   const fetchURL = await fetch(COCKTAIL_API_URL);
@@ -8,5 +10,20 @@ export async function fetchCocktails() {
 
 export async function fetchCocktailCategories() {
   const fetchURL = await fetch(COCKTAIL_CATEGORIES_API_URL);
+  return fetchURL.json();
+}
+
+export async function fetchDrinksByName(name) {
+  const fetchURL = await fetch(`${COCKTAIL_API_URL}${name}`);
+  return fetchURL.json();
+}
+
+export async function fetchDrinksByIngredient(ingredient) {
+  const fetchURL = await fetch(`${COCKTAIL_API_FILTER_URL}${ingredient}`);
+  return fetchURL.json();
+}
+
+export async function fetchDrinksByFirstLetter(first) {
+  const fetchURL = await fetch(`${COCKTAIL_API_FIRST_URL}${first}`);
   return fetchURL.json();
 }
