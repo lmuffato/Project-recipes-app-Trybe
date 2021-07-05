2e1a9b099a20333efabf584cb9841748d757e450
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-07-05 19:24:23 -0300

    Bebidas filtrando

--- src/components/Main/CategoryButtons.js
+++ src/components/Main/CategoryButtons.js
@@ -1,15 +1,25 @@
 import React, { useContext } from 'react';
+import { useLocation } from 'react-router';
 import PropTypes from 'prop-types';
 import FoodContext from '../../contexts/FoodContext';
+import DrinkContext from '../../contexts/DrinkContext';
 import { fetchFoodByCategoryName } from '../../services/mealAPI';
+import { fetchDrinkByCategoryName } from '../../services/cocktailAPI';
 
 const NUMBER_OF_CATEGORIES = 5;
 
 export default function CategoryButtons({ categories }) {
   const { setFoods } = useContext(FoodContext);
+  const { setDrinks } = useContext(DrinkContext);
+  const { pathname } = useLocation();
 
   function handleFilterByCategoryName(categoryName) {
-    fetchFoodByCategoryName(categoryName).then((data) => setFoods(data.meals));
+    if (pathname === '/comidas') {
+      fetchFoodByCategoryName(categoryName).then((data) => setFoods(data.meals));
+    }
+    if (pathname === '/bebidas') {
+      fetchDrinkByCategoryName(categoryName).then((data) => setDrinks(data.drinks));
+    }
   }
 
   return (
--- src/services/cocktailAPI.js
+++ src/services/cocktailAPI.js
@@ -1,5 +1,6 @@
 const COCKTAIL_API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const COCKTAIL_CATEGORIES_API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+const DRINK_BY_CATEGORY = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=';
 
 export async function fetchCocktails() {
   const fetchURL = await fetch(COCKTAIL_API_URL);
@@ -8,5 +9,10 @@ export async function fetchCocktails() {
 
 export async function fetchCocktailCategories() {
   const fetchURL = await fetch(COCKTAIL_CATEGORIES_API_URL);
+  return fetchURL.json();
+}
+
+export async function fetchDrinkByCategoryName(categoryName) {
+  const fetchURL = await fetch(`${DRINK_BY_CATEGORY}${categoryName}`);
   return fetchURL.json();
 }
