95caf09656bd6adc77f5fa7e5aac3f587f84b2b7
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-07-05 19:26:39 -0300

    Desafio 28 OK - Lint OK

--- src/components/Main/RecipeCard.js
+++ src/components/Main/RecipeCard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function RecipeCard({ recipe, index, type }) {
-
   return (
     <div data-testid={ `${index}-recipe-card` }>
       <li>
