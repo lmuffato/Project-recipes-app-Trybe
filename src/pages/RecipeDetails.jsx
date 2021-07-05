bca370d44834e984d8a09ba4c3ee3b8ae5e666b6
Ana Ventura <anaventura1811@gmail.com>
2021-07-05 12:55:13 -0300

    feat: implementando fetch de receita na tela de detalhes

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import RecipeInfo from '../components/RecipeInfo/RecipeInfo';
-import RecipeIngredients from '../components/RecipeIngredients/RecipeIngredients';
+// import RecipeIngredients from '../components/RecipeIngredients/RecipeIngredients';
 
 function RecipeDetails() {
   const location = useLocation();
   const { state } = location;
   const { recipe, type } = state;
+  const endpointMeals = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipe.idMeal}`;
+  const endpointDrinks = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${recipe.idDrink}`;
+  const [fetchRecipeURL, setFetchRecipeURL] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [singleRecipe, setRecipe] = useState({});
+
+  const handleFetchIngredients = useCallback(() => {
+    if (type === 'meals') {
+      setFetchRecipeURL(endpointMeals);
+    }
+    if (type === 'drinks') {
+      setFetchRecipeURL(endpointDrinks);
+    }
+  }, [endpointDrinks, endpointMeals, type]);
+
+  const handleFetch = useCallback(async (url) => {
+    try {
+      const request = await fetch(url);
+      const data = await request.json();
+      setRecipe(data[type][0]);
+      console.log(data[type][0]);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }, [type]);
+
+  useEffect(() => {
+    handleFetchIngredients();
+    handleFetch(fetchRecipeURL);
+  }, [fetchRecipeURL, handleFetch, handleFetchIngredients]);
+
+  if (isLoading) {
+    return 'Loading';
+  }
+
   // console.log(recipe);
-  const recipeName = recipe.strMeal || recipe.strDrink;
-  const recipeId = recipe.idMeal || recipe.idDrink;
-  const recipeThumb = recipe.strMealThumb || recipe.strDrinkThumb;
-  const recipeCategory = recipe.strCategory;
-  const isAlchooholic = recipe.strAlcoholic || '';
+  // incluir fetch à API de cada receita
+  const recipeName = singleRecipe.strMeal || singleRecipe.strDrink;
+  // const recipeId = recipe.idMeal || recipe.idDrink;
+  const recipeThumb = singleRecipe.strMealThumb || singleRecipe.strDrinkThumb;
+  const recipeCategory = singleRecipe.strCategory;
+  const isAlchooholic = singleRecipe.strAlcoholic || '';
 
   return (
     <div className="recipe-details-page">
@@ -24,11 +61,11 @@ function RecipeDetails() {
         { type === 'drinks' ? (<h3 data-testid="recipe-category">{isAlchooholic}</h3>) : (
           <h3 data-testid="recipe-category">{recipeCategory}</h3>)}
       </RecipeInfo>
-      <RecipeIngredients id={ recipeId } type={ type } />
+      {/* <RecipeIngredients id={ recipeId } type={ type } /> */}
       <div className="instructions">
         <h5>Instructions</h5>
         <p data-testid="instructions">
-          { recipe.strInstructions }
+          { singleRecipe.strInstructions }
         </p>
       </div>
 
