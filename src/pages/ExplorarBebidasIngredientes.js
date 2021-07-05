6832fd332d5a438465a533d3fe4e18d8d63ab12c
biazidioti <bzferreira.bio@gmail.com>
2021-07-05 18:55:33 -0300

    fix lint error.

--- src/pages/ExplorarBebidasIngredientes.js
+++ src/pages/ExplorarBebidasIngredientes.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function ExplorarBebidasIngredientes() {
+<<<<<<< Updated upstream
   return <div>Página de Explorar Bebidas por Ingredientes</div>;
+=======
+  return (
+    <section>
+      <Header title="Explorar Ingredientes" show={ false } />
+      <footer>
+        <Footer />
+      </footer>
+    </section>);
+>>>>>>> Stashed changes
 }
 
 export default ExplorarBebidasIngredientes;
