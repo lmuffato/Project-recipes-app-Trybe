3ba07f7e143b6b4f2941401a49776ae0e978adbb
biazidioti <bzferreira.bio@gmail.com>
2021-07-05 19:17:33 -0300

    Fix merge conflicts.

--- src/pages/ExplorarBebidasIngredientes.js
+++ src/pages/ExplorarBebidasIngredientes.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-
 function ExplorarBebidasIngredientes() {
   return (
     <section>
@@ -13,5 +10,6 @@ function ExplorarBebidasIngredientes() {
         <Footer />
       </footer>
     </section>);
+}
 
 export default ExplorarBebidasIngredientes;
