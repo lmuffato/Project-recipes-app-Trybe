6022b0678c534e47d8c24eb488bf8fd3827993af
João Nasc <oi@joaonasc.dev>
2021-07-06 19:01:57 -0300

    feat: add header

--- src/pages/MainRecipes/index.jsx
+++ src/pages/MainRecipes/index.jsx
@@ -2,10 +2,12 @@ import { Route } from 'react-router-dom';
 import React from 'react';
 import Food from './components/Food';
 import Drink from './components/Drink';
+import Header from '../../components/Header';
 
 function MainRecipes() {
   return (
     <>
+      <Header title="Comidas" />
       <Route path="/comidas" component={ Food } />
       <Route path="/bebidas" component={ Drink } />
     </>
