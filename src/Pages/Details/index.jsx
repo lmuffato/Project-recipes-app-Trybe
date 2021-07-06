f6e36938f858b08dded489d0f5437353787d64d1
lucas-paz <lucas.hpaz@outlook.com>
2021-07-06 16:02:41 -0300

    add component ingredients

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -1,40 +1,54 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, Col, Container, Form, Image, Row } from 'react-bootstrap';
+import { Button, Card, Image } from 'react-bootstrap';
 import { useLocation, useParams } from 'react-router';
 import { getRecipeByID } from '../../services/fetchRecipes';
+import Ingredient from '../../Components/Ingredients';
 
 function Details() {
   const { pathname } = useLocation();
   const { id } = useParams();
-  const [recipesDetails, setRecipesDetails] = useState();
+  const [recipesDetails, setRecipesDetails] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [recipeType, setRecipeType] = useState('Meal');
   const toggleApi = (pathname.includes('comidas')) ? 'meals' : 'drinks';
 
   useEffect(() => {
     getRecipeByID(pathname, id).then((response) => {
-      setRecipesDetails(response[toggleApi]);
+      setRecipesDetails(response[toggleApi][0]);
+      setIsLoading(false);
+      if (toggleApi === 'drinks') { setRecipeType('Drink'); }
     });
   }, [id, pathname, toggleApi]);
 
   return (
-    <Container>
-      <Row>
-        <Col>
-          {console.log(recipesDetails)}
-          <Image thumbnail data-testid="recipe-photo" />
-          <h1 data-testid="recipe-title">Titulo</h1>
+    <div>
+      {isLoading ? 'Carregando' : (
+        <div>
+          <Image
+            thumbnail
+            data-testid="recipe-photo"
+            src={ recipesDetails.strMealThumb }
+          />
+          <h1 data-testid="recipe-title">
+            {recipesDetails.strMeal}
+          </h1>
+          <h3 data-testid="recipe-category">
+            {recipeType === 'Meal' ? recipesDetails.strCategory : ''}
+          </h3>
           <button type="button" data-testid="share-btn">Share</button>
           <button type="button" data-testid="favorite-btn">Favorite</button>
-          <p data-testid="recipe-category">Texto</p>
-          <Form.Check data-testid={ `${0}-ingredient-name-and-measure` } />
-          <p data-testid="instructions">Instruções</p>
-          { (pathname.includes('comidas'))
+          <Ingredient type="list" recipe={ recipesDetails } />
+          <p data-testid="instructions">
+            {recipesDetails.strInstructions}
+          </p>
+          {(recipeType === 'Meal')
             ? <video data-testid="video"><track kind="captions" /></video>
             : ''}
           <Card data-testid={ `${0}-recomendation-card` }>Card</Card>
           <Button data-testid="start-recipe-btn">Iniciar Receita</Button>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      )}
+    </div>
   );
 }
 
