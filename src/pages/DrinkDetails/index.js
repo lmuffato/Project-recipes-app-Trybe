980a60dba24d449bae004cdcc59506e76e97ce02
Debora Passos <deborinhapassos@gmail.com>
2021-07-06 12:09:25 -0300

    feat: requisito 21

--- src/pages/DrinkDetails/index.js
+++ src/pages/DrinkDetails/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-export default function DrinkDetails() {
-  return (
-    <div />
-  );
-}
+// export default function DrinkDetails() {
+//   return (
+//     <div>
+//       <img data-testid="recipe-photo" src="#" alt="#" />
+//       <h1 data-testid="recipe-title">Recipe Title</h1>
+//       <button type="button" data-testid="share-btn">Share button</button>
+//       <button type="button" data-testid="favorite-btn">Favorite button</button>
+
+//       <textarea data-testid="recipe-category" />
+//       <textarea data-testid="index-ingredient-name-and-measure" />
+//       <textarea data-testid="instructions" />
+//       <div data-testid="index-recomendation-card" />
+//       <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
+//     </div>
+//   );
+// }
--- src/pages/ExplorePage/index.js
+++ src/pages/ExplorePage/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../../components/Header';
 import FooterMenu from '../../components/footerMenu';
 
 export default function ExplorePage() {
   return (
     <div>
+      <Header />
       <FooterMenu />
     </div>
   );
