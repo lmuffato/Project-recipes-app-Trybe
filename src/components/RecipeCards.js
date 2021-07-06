189cd2b8275860dab0ce91399a43ca521b77e8b7
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-07-06 17:16:55 -0400

    diminuida complexidade de RecipeCards

--- src/components/RecipeCards.js
+++ src/components/RecipeCards.js
@@ -42,6 +42,50 @@ const obj = {
   comidas: 'comidas',
 };
 
+export const renderCards = (data, type) => {
+  const showLimit = 12;
+  let thumb = '';
+  let name = '';
+  let id = '';
+  if (data && data.length > 0 && data !== 'error' && data.length !== 1) {
+    return (
+      data.map((recipe, index) => {
+        if (type === 'comidas') {
+          thumb = recipe.strMealThumb;
+          name = recipe.strMeal;
+          id = recipe.idMeal;
+        } else {
+          thumb = recipe.strDrinkThumb;
+          name = recipe.strDrink;
+          id = recipe.idDrink;
+        }
+        if (index < showLimit) {
+          return (
+            <Link to={ `/${type}/${id}` } key={ name }>
+              <div
+                className="card"
+                data-testid={ `${index}-recipe-card` }
+              >
+                <img
+                  alt="recipe"
+                  data-testid={ `${index}-card-img` }
+                  className="recipe-card-image"
+                  src={ thumb }
+                />
+                <div className="title-div">
+                  <h4 data-testid={ `${index}-card-name` } className="title">{name}</h4>
+                </div>
+              </div>
+            </Link>
+          );
+        }
+        return '';
+      })
+    );
+  }
+  return '';
+};
+
 export default function RecipeCards() {
   const typePt = window.location.href.split('/')[3];
   const type = obj[typePt];
@@ -49,54 +93,10 @@ export default function RecipeCards() {
   const data2 = useSelector((state) => (typePt === 'bebidas'
     ? state.searchReducer.initialDrinks : state.searchReducer.initialMeals));
   const data = (data1 !== '' ? data1 : data2);
-  const showLimit = 12;
-  let thumb = '';
-  let name = '';
-  let id = '';
-
-  const renderCards = () => {
-    if (data && data.length > 0 && data !== 'error' && data.length !== 1) {
-      return (
-        data.map((recipe, index) => {
-          if (type === 'comidas') {
-            thumb = recipe.strMealThumb;
-            name = recipe.strMeal;
-            id = recipe.idMeal;
-          } else {
-            thumb = recipe.strDrinkThumb;
-            name = recipe.strDrink;
-            id = recipe.idDrink;
-          }
-          if (index < showLimit) {
-            return (
-              <Link to={ `/${type}/${id}` } key={ name }>
-                <div
-                  className="card"
-                  data-testid={ `${index}-recipe-card` }
-                >
-                  <img
-                    alt="recipe"
-                    data-testid={ `${index}-card-img` }
-                    className="recipe-card-image"
-                    src={ thumb }
-                  />
-                  <div className="title-div">
-                    <h4 data-testid={ `${index}-card-name` } className="title">{name}</h4>
-                  </div>
-                </div>
-              </Link>
-            );
-          }
-          return '';
-        })
-      );
-    }
-    return '';
-  };
 
   return (
     <div className="list">
-      { renderCards() }
+      { renderCards(data, type) }
       { GoToDetails(data) }
     </div>
   );
