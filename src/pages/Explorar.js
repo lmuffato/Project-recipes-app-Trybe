df5ab4f55f1e7dc8a7a63a1fdff6fabae9a84b09
biazidioti <bzferreira.bio@gmail.com>
2021-07-06 15:07:01 -0300

    Explore Page - req-67-68-69

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -7,7 +8,22 @@ function Explorar() {
   return (
     <section>
       <Header title="Explorar" show={ false } />
-      Página de Explorar
+      <Link to="/explorar/comidas">
+        <button
+          data-testid="explore-food"
+          type="button"
+        >
+          Explorar Comidas
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button
+          data-testid="explore-drinks"
+          type="button"
+        >
+          Explorar Bebidas
+        </button>
+      </Link>
       <footer>
         <Footer />
       </footer>
