dde2eb389ebb700465bb5120730df0912938a3db
João Nasc <oi@joaonasc.dev>
2021-07-06 19:02:30 -0300

    fix: tests were breaking by local storage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { BiSearch } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
-import useLocalStorage from '../../hooks/useLocalStorage';
 
 import styles from './styles.module.scss';
+import useLocalStorage from '../../hooks/useLocalStorage';
 
 function Header({ title }) {
-  const user = useLocalStorage('user');
-  const gravatar = localStorage.getItem('gravatar');
-
+  const { user, gravatar } = useLocalStorage('user', 'gravatar');
   return (
     <header className={ styles.header }>
       <Link to="/perfil" className={ styles.profilePic }>
-        <img src={ gravatar } alt={ user.email } />
+        <img
+          src={ gravatar || 'https://www.gravatar.com/avatar/default' }
+          alt={ user ? user.email : 'user@mail.com' }
+        />
       </Link>
       { title }
       <BiSearch />
