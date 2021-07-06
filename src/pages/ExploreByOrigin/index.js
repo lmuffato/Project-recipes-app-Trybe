24399ff283ecc577bfa1b6ba2fc3a49f4e1db010
Mateus Alencar <mtauws@gmail.com>
2021-07-06 20:19:32 -0300

    fix: change Header in ExploreOrigin

--- src/pages/ExploreByOrigin/index.js
+++ src/pages/ExploreByOrigin/index.js
@@ -5,7 +5,7 @@ import Footer from '../../components/Footer';
 export default function ExploreByOrigin() {
   return (
     <div>
-      <Header title="Explorar por Origem" />
+      <Header title="Explorar Origem" />
       <Footer />
     </div>
   );
