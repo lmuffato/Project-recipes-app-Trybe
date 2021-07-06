507c3f962b824f91f6af8a1cdcc0ccf154f5140d
LucasMunizLara <lucasml@live.com>
2021-07-06 12:41:40 -0300

    DrinkDetails Implemented

--- src/pages/DrinkDetails.jsx
+++ src/pages/DrinkDetails.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import useRecipeDetails from '../hooks/useRecipeDetails';
+import CarouselCard from '../components/CarouselCard';
 
 export default function DrinkDetails() {
-  const { recipe, getIngredientsAndMeasures } = useRecipeDetails('cocktail');
+  const { recipe,
+    getIngredientsAndMeasures,
+    recommended,
+    filterRecommended,
+  } = useRecipeDetails('cocktail');
   // ALERTA GAMBIARRA
   const recipeDrink = recipe.drinks
     ? recipe.drinks[0]
@@ -16,40 +21,58 @@ export default function DrinkDetails() {
   } = recipeDrink;
 
   return (
-    <section>
-      <img data-testid="recipe-photo" src={ strDrinkThumb } alt="#" />
-      <h1 data-testid="recipe-title">{strDrink}</h1>
-      <p data-testid="recipe-category">{strAlcoholic}</p>
-      <button type="button" data-testid="share-btn">
-        Share button
-      </button>
-      <button type="button" data-testid="favorite-btn">
-        Favorite button
-      </button>
+    <main>
+      <section>
+        <img data-testid="recipe-photo" src={ strDrinkThumb } alt="#" />
+        <h1 data-testid="recipe-title">{strDrink}</h1>
+        <button type="button" data-testid="share-btn">
+          Share button
+        </button>
+        <button type="button" data-testid="favorite-btn">
+          Favorite button
+        </button>
+        <p data-testid="recipe-category">{strAlcoholic}</p>
+      </section>
 
       <section>
-        <h2>Ingredients</h2>
-        <ul>
-          {ingredients.map((ingredient, index) => (
-            <li
-              data-testid={ `${index}-ingredient-name-and-measure` }
-              key={ index }
-            >
-              {`${ingredient} - ${measures[index]}`}
-            </li>
+        <section>
+          <h2>Ingredients</h2>
+          <ul>
+            {ingredients.map((ingredient, index) => (
+              <li
+                data-testid={ `${index}-ingredient-name-and-measure` }
+                key={ index }
+              >
+                {`${ingredient} - ${measures[index]}`}
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <section>
+          <h2>Instructions</h2>
+          <p data-testid="instructions">{strInstructions}</p>
+        </section>
+      </section>
+      <section>
+
+        <h2>Recommended</h2>
+        <div className="carousel">
+          {filterRecommended(recommended.meals).map((rec, index) => (
+            <CarouselCard
+              key={ rec.idMeal }
+              cardImg={ rec.strMealThumb }
+              cardTitle={ rec.strMeal }
+              cardCategory={ rec.strCategory }
+              index={ index }
+            />
           ))}
-        </ul>
+        </div>
       </section>
 
-      <section>
-        <h2>Instructions</h2>
-        <p data-testid="instructions">{strInstructions}</p>
-      </section>
-
-      <div data-testid="0-recomendation-card" />
       <button type="button" data-testid="start-recipe-btn">
         Iniciar Receita
       </button>
-    </section>
+    </main>
   );
 }
