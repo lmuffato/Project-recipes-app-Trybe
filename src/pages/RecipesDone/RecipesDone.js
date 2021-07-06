deee14870084b212d714744923d080e1a8ef3edc
Maria Baeta <m.martinsdg@gmail.com>
2021-07-06 16:38:06 -0300

    Merge com a main

--- src/pages/RecipesDone/RecipesDone.js
+++ src/pages/RecipesDone/RecipesDone.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header';
-import ButtonFilter from './ButtonsFilter';
+import ButtonFilter from './ButtonFilter';
 import Recipes from './Recipes';
 import './index.css';
 
