c0797b071b1a4ad1896f00959ebfc5b2d45f4597
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-07-05 20:23:20 -0400

    linter

--- src/components/RecipeCards.js
+++ src/components/RecipeCards.js
@@ -30,51 +30,47 @@ export default function RecipeCards() {
     ? state.searchReducer.initialDrinks : state.searchReducer.initialMeals));
   const data = (data1 !== '' ? data1 : data2);
 
-  const renderCards = () => {
-    // if (data.length > 0 && showDetails === false) {
-    return (
-      data.map((recipe, index) => {
-        let thumb = '';
-        let name = '';
-        let id = 0;
-        let ptType = '';
-        if (type === 'bebidas') {
-          thumb = recipe.strDrinkThumb;
-          name = recipe.strDrink;
-          id = recipe.idDrink;
-          ptType = 'bebidas';
-        } else {
-          thumb = recipe.strMealThumb;
-          name = recipe.strMeal;
-          id = recipe.idMeal;
-          ptType = 'comidas';
-        }
-        const showLimit = 12;
-        if (index < showLimit) {
-          return (
-            <Link to={ `${ptType}/${id}` } key={ name }>
-              <div
-                className="card"
-                data-testid={ `${index}-recipe-card` }
-              >
-                <img
-                  alt="recipe"
-                  data-testid={ `${index}-card-img` }
-                  className="recipe-card-image"
-                  src={ thumb }
-                />
-                <div className="title-div">
-                  <h4 data-testid={ `${index}-card-name` } className="title">{name}</h4>
-                </div>
+  const renderCards = () => (
+    data.map((recipe, index) => {
+      let thumb = '';
+      let name = '';
+      let id = 0;
+      let ptType = '';
+      if (type === 'bebidas') {
+        thumb = recipe.strDrinkThumb;
+        name = recipe.strDrink;
+        id = recipe.idDrink;
+        ptType = 'bebidas';
+      } else {
+        thumb = recipe.strMealThumb;
+        name = recipe.strMeal;
+        id = recipe.idMeal;
+        ptType = 'comidas';
+      }
+      const showLimit = 12;
+      if (index < showLimit) {
+        return (
+          <Link to={ `${ptType}/${id}` } key={ name }>
+            <div
+              className="card"
+              data-testid={ `${index}-recipe-card` }
+            >
+              <img
+                alt="recipe"
+                data-testid={ `${index}-card-img` }
+                className="recipe-card-image"
+                src={ thumb }
+              />
+              <div className="title-div">
+                <h4 data-testid={ `${index}-card-name` } className="title">{name}</h4>
               </div>
-            </Link>
-          );
-        }
-        return '';
-      })
-    );
-    // }
-  };
+            </div>
+          </Link>
+        );
+      }
+      return '';
+    })
+  );
 
   return (
     <div className="list">
