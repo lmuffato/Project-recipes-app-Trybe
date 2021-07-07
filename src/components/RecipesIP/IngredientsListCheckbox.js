a50318f1bb438f560bb3cfd41ba896ee116c9672
guidugaich <guidugaich.dev@gmail.com>
2021-07-07 18:52:39 -0300

    adicionando logica dos checkboxes

--- src/components/RecipesIP/IngredientsListCheckbox.js
+++ src/components/RecipesIP/IngredientsListCheckbox.js
@@ -1,10 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 
-export default function IngredientsListCheckbox({ recipe }) {
+import '../../style/IngredientsListCheckbox.css';
+import InProgressContext from '../../contexts/InProgressContext';
+
+export default function IngredientsListCheckbox({ recipe, isFood }) {
+  const recipeId = isFood ? recipe.idMeal : recipe.idDrink;
+
   const [fullRecipe, setFullRecipe] = useState([]);
+  const [usedIngredients, setUsedIngredients] = useState([]);
+
+  const {
+    inProgressDrinks,
+    setInProgressDrinks,
+    inProgressFoods,
+    setInProgressFoods,
+    saveInProgressToLS } = useContext(InProgressContext);
 
   useEffect(() => {
+    // define state with recipe
     const allIngredients = Object.entries(recipe).filter(
       (entry) => entry[0].includes('Ingredient'),
     );
@@ -22,18 +36,51 @@ export default function IngredientsListCheckbox({ recipe }) {
     });
 
     setFullRecipe(ingredientsAndMeasures);
-  }, [recipe]);
+
+    // get in progress data from state to usedIngredients
+    if (Object.keys(inProgressFoods).includes(recipeId)) {
+      setUsedIngredients(inProgressFoods[recipeId]);
+    }
+
+    if (Object.keys(inProgressDrinks).includes(recipeId)) {
+      setUsedIngredients(inProgressDrinks[recipeId]);
+    }
+  }, [recipe, inProgressFoods, inProgressDrinks, recipeId]);
+
+  function handleCheckIngredient(ev) {
+    const ingredient = ev.target.value;
+    const isUsed = usedIngredients.includes(ingredient);
+    console.log(isUsed);
+    if (!isUsed) {
+      setUsedIngredients([...usedIngredients, ingredient]);
+    } else {
+      setUsedIngredients(usedIngredients.filter(((ing) => ing !== ingredient)));
+    }
+  }
 
   return (
-    <div>
+    <div className="ingredients-checkbox-container">
       <strong>Ingredients</strong>
       {fullRecipe.map((ing, index) => (
-        <p
+        <label
+          htmlFor="ingredient-checkbox"
           key={ index }
+          className="checkbox-container"
           data-testid={ `${index}-ingredient-step` }
         >
-          {`${ing[0]} - ${ing[1] ? ing[1] : 'a vonts'}`}
-        </p>
+          <input
+            type="checkbox"
+            name="ingredient-checkbox"
+            value={ ing[0] }
+            onChange={ (ev) => handleCheckIngredient(ev) }
+            checked={ usedIngredients.includes(ing[0]) }
+          />
+          <p
+            className={ usedIngredients.includes(ing[0]) ? 'crossed' : '' }
+          >
+            {`${ing[0]} - ${ing[1] ? ing[1] : 'a vonts'}`}
+          </p>
+        </label>
       ))}
 
     </div>
