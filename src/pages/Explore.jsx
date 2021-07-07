f753eb348751c589739bd79d4ad1e47367651c8b
Renzo Sevilha <sevilharenzo@gmail.com>
2021-07-07 18:36:26 -0300

    feat: Explore Page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Explore() {
+  const { push } = useHistory();
+
+  const redirectToExplore = (type) => push(`/explorar/${type}`);
+
   return (
     <div>
-      <Header title="Explorar">
-        <h1>Explore!</h1>
-      </Header>
+      <Header title="Explorar" />
+
+      <button
+        onClick={ () => redirectToExplore('comidas') }
+        type="button"
+        data-testid="explore-food"
+      >
+        Explorar Comidas
+      </button>
+      <button
+        onClick={ () => redirectToExplore('bebidas') }
+        type="button"
+        data-testid="explore-drinks"
+      >
+        Explorar Bebidas
+      </button>
+
       <Footer />
     </div>
   );
