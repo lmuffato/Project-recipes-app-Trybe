e7586068d500930eb8511584b3fb7177f8f397a2
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-07-07 19:42:02 -0300

    Desafio 30 Concluido e Testado

--- src/components/Main/CategoryButtons.js
+++ src/components/Main/CategoryButtons.js
@@ -12,80 +12,76 @@ export default function CategoryButtons({ categories }) {
   const { setFoods } = useContext(FoodContext);
   const { setDrinks } = useContext(DrinkContext);
   const { pathname } = useLocation();
-  let NUMBER_OF_CLICKED_CATEGORIES = 0;
   const buttons = document.querySelectorAll('.category-buttons');
+  const checked = false;
 
-  function countChecks() {
+  function resetAllCheckbox() {
     for (let index = 0; index < buttons.length; index += 1) {
-      if (buttons[index].checked) {
-        NUMBER_OF_CLICKED_CATEGORIES += 1;
-        console.log('existem ', NUMBER_OF_CLICKED_CATEGORIES);
-      }
-    }
-    return NUMBER_OF_CLICKED_CATEGORIES;
-  }
-
-  function handleFilterByCategoryName(ev, categoryName) {
-    countChecks();
-
-    switch (NUMBER_OF_CLICKED_CATEGORIES) {
-    case 0:
-      if (pathname === '/comidas') {
-        fetchFoods().then((data) => setFoods(data.meals));
-      }
-      if (pathname === '/bebidas') {
-        fetchCocktails().then((data) => setDrinks(data.drinks));
-      }
-      break;
-    case 1:
-      if (pathname === '/comidas') {
-        fetchFoodByCategoryName(categoryName).then((data) => setFoods(data.meals));
-        console.log('(NUMBER_OF_CLICKED_CATEGORIES === 1 / comidas ');
-      }
-      if (pathname === '/bebidas') {
-        fetchDrinkByCategoryName(categoryName).then((data) => setDrinks(data.drinks));
-      }
-      break;
-    case 2:
-      if (pathname === '/comidas') {
-        fetchFoodByCategoryName(categoryName).then((data) => setFoods(data.meals));
-        for (let index = 0; index < buttons.length; index += 1) {
-          buttons[index].checked = false;
-          ev.target.checked = true;
-        }
-      }
-      if (pathname === '/bebidas') {
-        fetchDrinkByCategoryName(categoryName).then((data) => setDrinks(data.drinks));
-        for (let index = 0; index < buttons.length; index += 1) {
-          buttons[index].checked = false;
-          ev.target.checked = true;
-        }
-      }
-      break;
-    default: global.alert('Ocorreu um erro');
+      buttons[index].checked = false;
     }
   }
 
+  function handleFilterByCategoryNameFOODS(ev, categoryName) {
+    if (ev.target.checked) {
+      fetchFoodByCategoryName(categoryName).then((data) => setFoods(data.meals));
+      resetAllCheckbox();
+      ev.target.checked = true;
+    } else if (ev.target.checked === false) {
+      fetchFoods().then((data) => setFoods(data.meals));
+      resetAllCheckbox();
+    } else if (ev.target.checked === !checked) {
+      fetchFoods().then((data) => setFoods(data.meals));
+      resetAllCheckbox();
+      ev.target.checked = !checked;
+    }
+  }
+
+  function handleFilterByCategoryNameDRINKS(ev, categoryName) {
+    if (ev.target.checked) {
+      fetchDrinkByCategoryName(categoryName).then((data) => setDrinks(data.drinks));
+      resetAllCheckbox();
+      ev.target.checked = !ev.target.checked;
+    } else if (ev.target.checked === false) {
+      fetchCocktails().then((data) => setDrinks(data.drinks));
+      resetAllCheckbox();
+    } else if (ev.target.checked === !checked) {
+      fetchCocktails().then((data) => setDrinks(data.drinks));
+      resetAllCheckbox();
+      ev.target.checked = !checked;
+    }
+  }
   return (
     <div className="category-button">
       {categories.map((categoryName, index) => (
         index < NUMBER_OF_CATEGORIES ? (
-          <label htmlFor="category-button">
-            {categoryName.strCategory}
+          <label htmlFor="category-button" key={ index }>
             <input
               className="category-buttons"
-              name="category-button"
+              name="category-buttons"
               value={ categoryName.strCategory }
               type="checkbox"
               key={ index }
               data-testid={ `${categoryName.strCategory}-category-filter` }
               onClick={ (ev) => {
-                handleFilterByCategoryName(ev, categoryName.strCategory);
+                if (pathname === '/comidas') {
+                  handleFilterByCategoryNameFOODS(ev, categoryName.strCategory);
+                } else if (pathname === '/bebidas') {
+                  handleFilterByCategoryNameDRINKS(ev, categoryName.strCategory);
+                }
               } }
             />
+            {categoryName.strCategory}
           </label>
-        ) : ''
+        ) : null
       ))}
+      <label htmlFor="category-buttons">
+        <input
+          type="checkbox"
+          name="category-buttons"
+          onClick={ (ev) => console.log(ev.target) }
+        />
+        All
+      </label>
     </div>
   );
 }
