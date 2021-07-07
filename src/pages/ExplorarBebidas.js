2f183edab841d8445e0610150c596710af09a2ed
biazidioti <bzferreira.bio@gmail.com>
2021-07-07 12:00:12 -0300

    req 70-71-72-73

--- src/pages/ExplorarBebidas.js
+++ src/pages/ExplorarBebidas.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -7,6 +8,22 @@ function ExplorarBebidas() {
   return (
     <section>
       <Header title="Explorar Bebidas" show={ false } />
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas/">
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
       <footer>
         <Footer />
       </footer>
