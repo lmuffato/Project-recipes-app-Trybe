2a1a6ce3cf9f761628b6108685ca584b408c362b
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-07-06 21:11:52 -0400

    fix: req 10

--- src/Pages/DoneRecepies.js
+++ src/Pages/DoneRecepies.js
@@ -68,7 +68,7 @@ export default function DoneRecepies() {
     <>
       <Header props={ { search: false, title: 'Receitas Feitas' } } />
       {filterButtons()}
-      {renderCards(doneRecipies)}
+      {doneRecipies ? renderCards(doneRecipies) : null}
     </>
   );
 }
