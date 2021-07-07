65c758637c2a3748ef612282adec9165c1c66810
Wendrick <wendrick.b.c1@gmail.com>
2021-07-07 17:17:45 -0300

    67 ate 73

--- src/pages/explore/DrinkExplore.js
+++ src/pages/explore/DrinkExplore.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import Header from '../../components/Header';
 import BottomMenu from '../../components/bottomMenu';
+import { Link } from 'react-router-dom';
 
 export default function DrinksExplore() {
   return (
     <div>
       <Header title="Explorar Bebidas" show={ false } />
+
+      <div className="buttonsExplore">
+        <Link to="/explorar/bebidas/ingredientes">
+          <button
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+
+        <Link to="">
+          <button
+            data-testid="explore-surprise"
+          >
+            Me Surpreenda!
+          </button>
+        </Link>
+      </div>
+
       <BottomMenu />
     </div>
   );
--- src/pages/explore/FoodsExplore.js
+++ src/pages/explore/FoodsExplore.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import Header from '../../components/Header';
 import BottomMenu from '../../components/bottomMenu';
+import { Link } from 'react-router-dom';
 
 export default function FoodsExplore() {
   return (
     <div>
       <Header title="Explorar Comidas" show={ false } />
+
+      <div className="buttonsExplore">
+        <Link to="/explorar/comidas/ingredientes">
+          <button
+            data-testid="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+
+        <Link to="/explorar/comidas/area">
+          <button
+            data-testid="explore-by-area"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+
+        <Link to="">
+          <button
+            data-testid="explore-surprise"
+          >
+            Me Surpreenda!
+          </button>
+        </Link>
+      </div>
+
       <BottomMenu />
     </div>
   );
