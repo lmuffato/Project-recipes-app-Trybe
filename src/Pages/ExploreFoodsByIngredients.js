82b1645d7298c6fb2c403fbf2486b21a7844de9b
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-07-07 13:48:32 -0400

    req 37 pronto

--- src/Pages/ExploreFoodsByIngredients.js
+++ src/Pages/ExploreFoodsByIngredients.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import fetchFoodIngredients from '../helpers/fetchFoodIngredients';
 
-function ExploreFoodsByIngredients() {
+// const renderIngredients = () => {
+//   return (
+//     <h1>Funfou</h1>
+//   );
+// };
+
+export default function ExploreFoodsByIngredients() {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    const getIngredients = async () => {
+      const { meals } = await fetchFoodIngredients();
+      setIngredients(meals);
+    };
+    getIngredients();
+  }, []);
+  console.log(ingredients);
   return (
     <>
       <Header props={ { search: false, title: 'Explorar Ingredientes' } } />
+      {/* {renderIngredients()} */}
       <Footer />
     </>
   );
 }
-
-export default ExploreFoodsByIngredients;
