1bf28b6bc3e4e44f00b01356cb4f2e346cbe86a4
João Nasc <oi@joaonasc.dev>
2021-07-07 16:56:35 -0300

    feat: refactor MainRecipes

--- src/pages/MainRecipes/index.jsx
+++ src/pages/MainRecipes/index.jsx
@@ -1,15 +1,37 @@
-import { Route } from 'react-router-dom';
-import React from 'react';
-import Food from './components/Food';
-import Drink from './components/Drink';
+import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
+import getRecipes from '../../services/recipesData';
 
 function MainRecipes() {
+  const { location: { pathname } } = useHistory();
+  const [recipes, setRecipes] = useState([]);
+  const [titlePage, setTitlePage] = useState('');
+
+  useEffect(() => {
+    async function loadRecipes() {
+      const arrayLimit = 12;
+      const results = await getRecipes(pathname);
+      setRecipes(results.list.slice(0, arrayLimit));
+      setTitlePage(results.titlePage);
+    }
+
+    loadRecipes();
+  }, [pathname]);
+
   return (
     <>
-      <Header title="Comidas" />
-      <Route path="/comidas" component={ Food } />
-      <Route path="/bebidas" component={ Drink } />
+      <Header title={ titlePage } />
+      {recipes.map((recipe, index) => (
+        <div key={ recipe.id } data-testid={ `${index}-recipe-card` }>
+          <h1 data-testid={ `${index}-card-name` }>{recipe.name}</h1>
+          <img
+            src={ recipe.imagePath }
+            alt={ recipe.name }
+            data-testid={ `${index}-card-img` }
+          />
+        </div>
+      ))}
     </>
   );
 }
