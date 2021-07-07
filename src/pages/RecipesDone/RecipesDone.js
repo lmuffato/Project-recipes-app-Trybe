4fa7076a31b4700c554849cff97dc26fa9900b30
Maria Baeta <m.martinsdg@gmail.com>
2021-07-06 21:58:33 -0300

    Fix - Retrive information from localStorag

--- src/pages/RecipesDone/RecipesDone.js
+++ src/pages/RecipesDone/RecipesDone.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header';
 import ButtonFilter from './ButtonsFilter';
+// import Footer from '../../components/Footer';
 import Recipes from './Recipes';
 import './index.css';
 
