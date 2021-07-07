969678360765f7bc7019b45af07ab7fb5d715c45
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-07-07 10:48:16 -0300

    styles: explore page

--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './styles.module.scss';
 
 function Explore() {
   return (
-    <>
-      <Link to="/explorar/comidas">
-        <button data-testid="explore-food" type="button">Explorar Comidas</button>
-      </Link>
-      <Link to="/explorar/bebidas">
-        <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
-      </Link>
-    </>
+    <div className={ styles.explorePage }>
+      <div className={ styles.exploreContent }>
+        <Link to="/explorar/comidas" className="first">
+          <button
+            id={ styles.first }
+            data-testid="explore-food"
+            type="button"
+            className="primary-btn"
+          >
+            Explorar Comidas
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button
+            type="button"
+            data-testid="explore-drinks"
+            className="primary-btn"
+          >
+            Explorar Bebidas
+          </button>
+        </Link>
+      </div>
+
+    </div>
   );
 }
 
