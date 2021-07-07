f1bb2abdc4ab3761b3bdd178a306757b832760df
hheloisa <hhackenhaar@gmail.com>
2021-07-07 20:18:07 -0300

    Implementa funçoes localStorage

--- src/pages/DoneRecepies.js
+++ src/pages/DoneRecepies.js
@@ -1,45 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Share from '../images/shareIcon.svg';
 
 function DoneRecepies () {
-  // myDoneRecepies = array com as receitas feitas
-  return(
+    const [doneRecepies, setDoneRecepies] = useState([]);
+    const [showDoneRecepies, setShowRecepies] = useState(doneRecepies);
+  
+  // funções para pegar as receitas do local storage
+  const getDoneRecepies = () => {
+    const doneRecepiesString = localStorage.getItem('doneRecipes');
+    const doneRecepies = JSON.parse(doneRecepiesString);
+    return doneRecepies;
+  }
+
+  useEffect(() => {
+    // função que checa se há receitas no local storage
+    const checkDoneRecepies = () => {
+      const recepiesStorage = getDoneRecepies();
+      if (recepiesStorage === null) {
+        alert('Você ainda não concluiu nenhuma receita!')
+      } else {
+        setDoneRecepies(recepiesStorage);
+      }
+    }
+
+    checkDoneRecepies();
+  }, []);
+
+  function handleContent(all, food, drink) {
+    if(all) {
+      setShowRecepies(doneRecepies);
+    } else if(food) {
+      const mealsRecepi = recepiesStorage.filter((recepie) => recepi.type === 'meals');
+      setShowRecepies(mealsRecepi);
+    } else if(drink) {
+      const drinksRecepi = recepiesStorage.filter((recepie) => recepi.type === 'drinks');
+      setShowRecepies(drinksRecepi);
+    }
+  }
+
+  return (
     <div>
-    <button
-    data-testid="filter-by-all-btn"
-    >
-      All
-    </button>
-    <button
-      data-testid="filter-by-food-btn"
-    >
-      Food
-    </button>
-    <button
-      data-testid="filter-by-drink-btn"
-    >
-      Drinks
-    </button>
-    {/* {myDoneRecepies.map((recepie, index) => (
+      <div>
+        <button
+          data-testid="filter-by-all-btn"
+          onClick={handleContent(all)}
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          onClick={handleContent(food)}
+        >
+          Food
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          onClick={handleContent(drink)}
+        >
+          Drinks
+        </button>
+      </div>
+    {showDoneRecepies.map((recepie, index) => {
       if (recepie.type === meal) {
+        // <Componente cardFood image, name, category, area, doneDate, tags />
         <div>
-        <img data-testid={`${index}-horizontal-image`}>{ recepie.img }</img>
+        <img data-testid={`${index}-horizontal-image`}>{ recepie.image }</img>
         <p data-testid={`${index}-horizontal-name`}>{recepie.name}</p>
         <p data-testid={`${index}-horizontal-top-text>`}>{recepie.category}</p>
-        <p>area?</p>
-        <p data-testid={`${index}-horizontal-done-date`}>{recepie.date}</p>
-        <p data-testid={`${index}-${tagName}-horizontal-tag`}>{recepie.tagName[0]}{recepie.tagName[1]}</p>
+        <p>{recepie.area}</p>
+        <p data-testid={`${index}-horizontal-done-date`}>{recepie.doneDate}</p>
+        <p data-testid={`${index}-${tags}-horizontal-tag`}>{`recepie.tags[0]}, {recepie.tags[1]`}</p>
         <img data-testid={`${index}-horizontal-share-btn`} src={Share} alt="share"></img>
       </div>
     } else if (recepie.type === drink) {
+      // // <Componente cardDrink image, name, doneDate, alcoholicOrNot />
       <div>
-        <img data-testid={`${index}-horizontal-image`}>{ recepie.img }</img>
-        <p>alcólica:{recepie.alcoolic}</p> 
-        <p data-testid={`${index}-horizontal-done-date`}>{recepie.date}</p>
+        <img data-testid={`${index}-horizontal-image`}>{ recepie.image }</img>
+        <p>alcólica:{recepie.alcoholicOrNot}</p> 
+        <p data-testid={`${index}-horizontal-done-date`}>{recepie.doneDate}</p>
         <img data-testid={`${index}-horizontal-share-btn`} src={Share} alt="share"></img>
       </div>
     }
-    ) */}
+    })
+  }
     </div>
   )
 }
