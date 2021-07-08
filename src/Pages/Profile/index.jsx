b87f289c0c7bc73f5c7237f7652de56f2cabd9da
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-07-08 17:01:31 -0300

    feat:req82-87

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -1,15 +1,52 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from '../../Components/Footer';
-import Header from '../../Components/Header';
+import './Profile.css';
 
-function Profile() {
+const Profile = () => {
+  const history = useHistory();
+  const email = JSON.parse(localStorage.getItem('user'))
+    ? JSON.parse(localStorage.getItem('user')).email
+    : '';
+  function handleLogout() {
+    localStorage.clear();
+    history.push('/');
+  }
   return (
-    <div>
-      <Header>Perfil</Header>
-      <h1>Profile</h1>
+    <div className="container">
+      <h1>Perfil</h1>
+      <h5 data-testid="profile-email">{email}</h5>
+      <Link to="/receitas-feitas">
+        <button
+          className="myButton"
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          className="myButton"
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          className="myButton"
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleLogout }
+        >
+          Sair
+        </button>
+      </Link>
       <Footer />
     </div>
   );
-}
+};
 
 export default Profile;
