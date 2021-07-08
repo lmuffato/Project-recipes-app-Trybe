95199a8261a8b6ef9e4b4bfe2d3f8439ad0bb290
guidugaich <guidugaich.dev@gmail.com>
2021-07-07 22:09:36 -0300

    corrigindo funcionalidade de favoritar

--- src/components/DetailsHeader.js
+++ src/components/DetailsHeader.js
@@ -1,4 +1,5 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
@@ -12,7 +13,7 @@ const copy = require('clipboard-copy');
 export default function DetailsHeader({ recipe, isFood }) {
   const recipeId = isFood ? recipe.idMeal : recipe.idDrink;
   const recipeType = isFood ? 'comida' : 'bebida';
-  const recipeArea = recipe.strArea;
+  const recipeArea = recipe.strArea ? recipe.strArea : '';
   const recipeCategory = recipe.strCategory;
   const recipeAlcoholicOrNot = isFood ? '' : recipe.strAlcoholic;
   const recipeName = isFood ? recipe.strMeal : recipe.strDrink;
@@ -24,8 +25,29 @@ export default function DetailsHeader({ recipe, isFood }) {
     favorites.map((fav) => fav.id).includes(recipeId),
   );
 
+  useEffect(() => {
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (favoriteRecipes) {
+      const isFavoriteLS = favoriteRecipes.map((fav) => fav.id).includes(recipeId);
+      if (isFavoriteLS) setIsFavorite(true);
+    }
+  }, [recipeId]);
+
+  const { pathname } = useLocation();
+
   function handleShareClick() {
-    copy(window.location.href);
+    const pageType = pathname.split('/')[1];
+    const id = pathname.split('/')[2];
+    copy(`${window.location.origin}/${pageType}/${id}`);
+
+    const copyMsg = document.createElement('p');
+    copyMsg.innerText = 'Link copiado!';
+    copyMsg.className = 'toast';
+    document.getElementsByTagName('body')[0].appendChild(copyMsg);
+    const WAIT_TIME = 4000;
+    setTimeout(() => {
+      copyMsg.remove();
+    }, WAIT_TIME);
   }
 
   function handleFavoriteClick() {
@@ -71,13 +93,16 @@ export default function DetailsHeader({ recipe, isFood }) {
           </button>
           <button
             type="button"
-            data-testid="favorite-btn"
             onClick={ () => {
               setIsFavorite(!isFavorite);
               handleFavoriteClick();
             } }
           >
-            <img src={ isFavorite ? blackHeartIcon : whiteHeartIcon } alt="favoritar" />
+            <img
+              data-testid="favorite-btn"
+              src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
+              alt="favoritar"
+            />
           </button>
         </div>
       </div>
