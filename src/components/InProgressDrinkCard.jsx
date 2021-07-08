192a4504e4df9522a31924e131f00ee816d2f9bc
Felipe Muller <felipeavmuller@gmail.com>
2021-07-08 16:30:41 -0300

    feate: req 47, 48 49

---
+++ src/components/InProgressDrinkCard.jsx
@@ -0,0 +1,42 @@
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import UserContext from '../context/UserContext';
+import shareIcon from '../images/shareIcon.svg';
+import FavoriteButton from './FavoriteButton';
+import IngredientsCheckBox from './IngredientsCheckBox';
+
+const copy = require('clipboard-copy');
+
+function InProgressDrinkCard() {
+  const { currentDrink } = useContext(UserContext);
+  const [copyLink, setCopyLink] = useState(false);
+  const history = useHistory();
+  const shareClick = () => {
+    const URL = history.location.pathname;
+    copy(`http://localhost:3000${URL}`);
+    setCopyLink(true);
+  };
+  return (
+    <div>
+      <img
+        data-testid="recipe-photo"
+        src={ currentDrink.strDrinkThumb }
+        alt="Current Meal"
+      />
+      <h3 data-testid="recipe-title">{currentDrink.strDrink}</h3>
+      <button data-testid="share-btn" type="button" onClick={ shareClick }>
+        <img src={ shareIcon } alt="compartilhar" />
+      </button>
+      {copyLink ? <span>Link copiado!</span> : null}
+      <FavoriteButton type="bebida" />
+      <h4 data-testid="recipe-category">{ currentDrink.strAlcoholic }</h4>
+      <h4>Ingredients</h4>
+      <IngredientsCheckBox currentMeal={ currentDrink } />
+      <h4>Instructions</h4>
+      <span data-testid="instructions">{ currentDrink.strInstructions }</span>
+      <button type="button" data-testid="finish-recipe-btn">Finalizar Receita</button>
+    </div>
+  );
+}
+
+export default InProgressDrinkCard;
--- src/pages/InProgressDrink.jsx
+++ src/pages/InProgressDrink.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import PropTypes from 'prop-types';
+import UserContext from '../context/UserContext';
+import InProgressDrinkCard from '../components/InProgressDrinkCard';
 
-function InProgressDrink() {
+function InProgressDrink({ match }) {
+  const { id } = match.params;
+  const { setGlobalId } = useContext(UserContext);
+
+  useEffect(() => {
+    setGlobalId(id);
+  }, []);
+
   return (
     <div>
-      Receita In PRogress DRink
+      <InProgressDrinkCard />
     </div>
   );
 }
+
+InProgressDrink.propTypes = {
+  match: PropTypes.object,
+  id: PropTypes.string,
+}.isRequired;
 
 export default InProgressDrink;
