e768891b94139fcaa99562b796d61ed49d0cbfe5
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-07-08 16:39:01 -0300

    Desafios ate o 31 Concluidos - 32 pronto mas nao passa no teste

--- src/components/Main/RecipeCard.js
+++ src/components/Main/RecipeCard.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import { useLocation } from 'react-router';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export default function RecipeCard({ recipe, index, type }) {
+  const { pathname } = useLocation();
+  console.log(recipe);
   return (
     <div data-testid={ `${index}-recipe-card` }>
-      <li>
-        <img
-          data-testid={ `${index}-card-img` }
-          src={ type === '/comidas' ? recipe.strMealThumb : recipe.strDrinkThumb }
-          alt="thumbnail recipe"
-          width="100"
-        />
-        <p data-testid={ `${index}-card-name` }>
-          { type === '/comidas' ? recipe.strMeal : recipe.strDrink }
-        </p>
-      </li>
+      { (pathname === '/comidas')
+        ? (
+          <Link to={ `/comidas/${recipe.idMeal}` }>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ type === '/comidas' ? recipe.strMealThumb : recipe.strDrinkThumb }
+              alt="thumbnail recipe"
+              width="100"
+            />
+            <p data-testid={ `${index}-card-name` }>
+              { type === '/comidas' ? recipe.strMeal : recipe.strDrink }
+            </p>
+          </Link>)
+        : (
+          <Link to={ `/bebidas/${recipe.idDrink}` }>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ type === '/comidas' ? recipe.strMealThumb : recipe.strDrinkThumb }
+              alt="thumbnail recipe"
+              width="100"
+            />
+            <p data-testid={ `${index}-card-name` }>
+              { type === '/comidas' ? recipe.strMeal : recipe.strDrink }
+            </p>
+          </Link>)}
     </div>
   );
 }
