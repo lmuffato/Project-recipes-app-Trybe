f711795280a778ad76b313d34d183944dbf6693e
Mateus Alencar <mtauws@gmail.com>
2021-07-08 15:18:09 -0300

    test: login

--- src/tests/login.spec.js
+++ src/tests/login.spec.js
@@ -1,37 +1,55 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
+import Login from '../pages/Login';
 
-import App from '../App';
+describe('Login Screen', () => {
+  it('Check whether the login page has been rendered', () => {
+    const { getByText } = renderWithRouter(<Login />);
+    const loginTitle = getByText('Login');
 
-describe('2 - Create all elements that must respect the attributes described', () => {
-  it('It has the data-testids email-input, password-input and login-submit-btn', () => {
-    const { getByTestId } = renderWithRouter(<App />);
-
-    getByTestId('email-input');
-    getByTestId('password-input');
-    getByTestId('login-submit-btn');
+    expect(loginTitle).toBeInTheDocument();
   });
-});
 
-describe('3 - Design the screen so that the person should be able to write email', () => {
-  it('It is possible to write the email', () => {
-    const { getByTestId } = renderWithRouter(<App />);
+  it('It has two inputs', () => {
+    const { container } = renderWithRouter(<Login />);
+    const inputs = container.querySelectorAll('input');
 
-    const inputEmail = getByTestId('email-input');
-    expect(inputEmail).toHaveValue('');
-    fireEvent.change(inputEmail, { target: { value: 'mateus@betrybe.com' } });
-    expect(inputEmail).toHaveValue('mateus@betrybe.com');
+    expect(inputs.length).toBe(2);
+    expect(inputs[0].type).toBe('email');
+    expect(inputs[1].type).toBe('password');
   });
-});
 
-describe('4 - Screen must be able to write password', () => {
-  it('It is possible to write the password', () => {
-    const { getByTestId } = renderWithRouter(<App />);
+  it('It has login button', () => {
+    const { getByTestId, container } = renderWithRouter(<Login />);
+    const button = getByTestId('login-submit-btn');
+    const inputs = container.querySelectorAll('input');
 
-    const inputPassword = getByTestId('password-input');
-    expect(inputPassword).toHaveValue('');
-    fireEvent.change(inputPassword, { target: { value: '12345678' } });
-    expect(inputPassword).toHaveValue('12345678');
+    expect(button).toBeInTheDocument();
+    expect(button.textContent).toBe('Entrar');
+    expect(button).toBeDisabled();
+
+    userEvent.type(inputs[0], 'test');
+    userEvent.type(inputs[1], '123456');
+    expect(button).toBeDisabled();
+
+    userEvent.type(inputs[0], 'test@email.com');
+    userEvent.type(inputs[1], '12');
+    expect(button).toBeDisabled();
+
+    userEvent.type(inputs[0], 'test@test.com');
+    userEvent.type(inputs[1], '1234567');
+    expect(button).toBeEnabled();
+
+    fireEvent.click(button);
+
+    const localUser = localStorage.getItem('user');
+    const localMeal = localStorage.getItem('mealsToken');
+    const localCocktail = localStorage.getItem('cocktailsToken');
+
+    expect(localUser).toContain('test@test.com');
+    expect(localMeal).toContain(1);
+    expect(localCocktail).toContain(1);
   });
 });
