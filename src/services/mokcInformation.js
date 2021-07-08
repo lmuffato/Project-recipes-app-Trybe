b6c3da840757826a3367aa0a3ff4fe7f6eb5d29b
Maria Baeta <m.martinsdg@gmail.com>
2021-07-07 21:48:08 -0300

    Correção lint

--- src/services/mokcInformation.js
+++ src/services/mokcInformation.js
@@ -41,7 +41,7 @@ export const MockFavorite = [
     type: 'bebida',
     area: '',
     category: 'Cocktail',
-    alcoholicOrNot:  'Alcoholic',
+    alcoholicOrNot: 'Alcoholic',
     name: 'Aquamarine',
     image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
   },
