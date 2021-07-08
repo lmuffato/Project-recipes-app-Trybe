499574853ec100783db114aa30ae1496c11d6689
Marcelo Maurício <mmauricio.design@gmail.com>
2021-07-08 11:51:31 -0300

    feat: create recipe cards for Receitas Favoritas - req 60

---
+++ src/pages/ReceitasFavoritaCard.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import shareIcon from '../images/shareIcon.svg';
+// import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import '../styles/ReceitasFavoritas.css';
+
+const copy = require('clipboard-copy');
+
+function ReceitasFavoritasCard({ props: { recipe, index } }) {
+  // const [favoriteRecipe, setFavoriteRecipe] = useState(true);
+  const history = useHistory();
+
+  const { category, name, image } = recipe;
+
+  const shareClick = (e) => {
+    e.preventDefault();
+    const { location: { pathname } } = history;
+
+    copy(`http://localhost:3000${pathname}`);
+    // setClipboardStatus('copied');
+  };
+
+  const favoriteClick = (e) => {
+    e.preventDefault();
+
+    // const favoriteRecipe = {
+    //   id,
+    //   type: 'bebida',
+    //   area: '',
+    //   category: acctualyDrink.drinks[0].strCategory,
+    //   alcoholicOrNot: acctualyDrink.drinks[0].strAlcoholic,
+    //   name: acctualyDrink.drinks[0].strDrink,
+    //   image: acctualyDrink.drinks[0].strDrinkThumb,
+    // };
+
+    // if (JSON.parse(localStorage.getItem('favoriteRecipes') !== null)) {
+    //   const oldRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
+    //   console.log(oldRecipes);
+
+    //   const newRecipes = [...oldRecipes, favoriteRecipe];
+
+    //   console.log('New Recipe', newRecipes);
+    //   localStorage.setItem('favoriteRecipes', JSON.stringify(newRecipes));
+    // } else {
+    //   localStorage.setItem('favoriteRecipes', JSON.stringify([favoriteRecipe]));
+    // }
+
+    // return !favoriteDrink ? setFavoriteDrink(true) : setFavoriteDrink(false);
+  };
+
+  const createButtons = () => {
+    console.log('recipe');
+    return (
+      <div>
+        <button
+          type="button"
+          data-testid={ `${index}-horizontal-share-btn` }
+          onClick={ shareClick }
+        >
+          <img alt="Share link" src={ shareIcon } />
+        </button>
+        <button type="button" onClick={ favoriteClick }>
+          <img
+            alt="Favorite button"
+            data-testid={ `${index}-horizontal-favorite-btn` }
+            src={ blackHeartIcon }
+          />
+        </button>
+      </div>
+    );
+  };
+  console.log(recipe, index);
+
+  return (
+    <>
+      <div>
+        <button type="button" data-testid="filter-by-all-btn">All</button>
+        <button type="button" data-testid="filter-by-food-btn">Food</button>
+        <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      </div>
+      <main>
+        <img
+          alt="Receita Favoritada"
+          src={ image }
+          className="img-card-favorite"
+          data-testid={ `${index}-horizontal-image` }
+        />
+        <div>
+          <span data-testid={ `${index}-horizontal-top-text` }>{ category }</span>
+          <span data-testid={ `${index}-horizontal-name` }>{ name }</span>
+          { createButtons() }
+        </div>
+      </main>
+    </>
+  );
+}
+
+ReceitasFavoritasCard.propTypes = {
+  props: PropTypes.object,
+}.isRequired;
+
+export default ReceitasFavoritasCard;
--- src/pages/ReceitasFavoritas.js
+++ src/pages/ReceitasFavoritas.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
+import ReceitasFavoritasCard from './ReceitasFavoritaCard';
 
 function ReceitasFavoritas() {
-  return (
+  const [favoriteRecipes, setFavoriteRecipes] = useState();
+
+  useEffect(() => {
+    const verifyFavorite = () => {
+      if (JSON.parse(localStorage.getItem('favoriteRecipes') !== null)) {
+        const recipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
+        if (recipes) setFavoriteRecipes(recipes);
+      }
+    };
+    verifyFavorite();
+  }, []);
+
+  return favoriteRecipes ? (
     <div>
       <Header title="Receitas Favoritas" />
+      { favoriteRecipes
+        .map((recipe, index) => {
+          const props = {
+            recipe,
+            index,
+          };
+
+          return <ReceitasFavoritasCard key={ index } props={ props } />;
+        }) }
     </div>
-  );
+  ) : <span>Nenhuma receita favoritada</span>;
 }
 
 export default ReceitasFavoritas;
