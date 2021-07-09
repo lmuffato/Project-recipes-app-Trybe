fcd18a7caf72de7be0c4df2f8b8ee7a682f6a6d9
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-07-09 13:38:14 -0300

    fixing linter issues

--- src/test/renderWithRouter.js
+++ src/test/renderWithRouter.js
@@ -6,8 +6,7 @@ import { render } from '@testing-library/react';
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
-    ...render(<Router history={ history }>{component}</Router>,
-    ),
+    ...render(<Router history={ history }>{component}</Router>),
     history,
   });
 };
