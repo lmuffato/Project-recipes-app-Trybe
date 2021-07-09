bc60bd71bfd3ebb7416ca5195a0d9f3c5caaa1bf
Luiz <luizfvaccarif@gmail.com>
2021-07-09 13:53:20 -0300

    fix: initial state of receitas feitas

--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -1,41 +1,18 @@
-import React, { /* useEffect, */ useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DoneRecipeCard from '../components/DoneRecipes/DoneRecipeCard';
 
 import Header from '../components/Header';
 
 function ReceitasFeitas() {
-  const [doneRecipes/* setDoneRecipes */] = useState([
-    {
-      id: '52771',
-      type: 'comida',
-      area: 'Italian',
-      category: 'Vegetarian',
-      alcoholicOrNot: '',
-      name: 'Spicy Arrabiata Penne',
-      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-      doneDate: '23/06/2020',
-      tags: ['Pasta', 'Curry'],
-    },
-    {
-      id: '178319',
-      type: 'bebida',
-      area: '',
-      category: 'Cocktail',
-      alcoholicOrNot: 'Alcoholic',
-      name: 'Aquamarine',
-      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-      doneDate: '23/06/2020',
-      tags: [],
-    },
-  ]);
+  const [doneRecipes, setDoneRecipes] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('todos');
 
-  /*   useEffect(() => {
+  useEffect(() => {
     setDoneRecipes(
-      JSON.parse(localStorage.getItem('favoriteRecipes')),
+      JSON.parse(localStorage.getItem('doneRecipes')),
     );
   }, []);
- */
+
   function changeSelectedCategory({ target: { name } }) {
     setSelectedCategory(name);
   }
