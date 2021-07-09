3bff462d8f27acdb560d56b0a1b4d198a08517e6
Marcelo Maurício <mmauricio.design@gmail.com>
2021-07-09 16:56:29 -0300

    feat: create filter by Area and not found page - 79 && 80

--- src/pages/ExplorarComidasArea.js
+++ src/pages/ExplorarComidasArea.js
@@ -6,21 +6,27 @@ import Header from '../components/Header';
 
 import ContextComidas from '../provider/ContextComida';
 
-import getAreas from '../services/apisArea';
+import { getAreas, getFoodsByArea } from '../services/apisArea';
 import { mealsAPI } from '../services/apisMealsAndCocktails';
 
 function ExplorarComidasArea() {
   const [areas, setAreas] = useState('');
   const [acctualyFood, setAcctualyFood] = useState([]);
-  // const [filterArea, setFilterArea] = useState('');
+  const [filterArea, setFilterArea] = useState('All');
 
   const { data, setData } = useContext(ContextComidas);
 
   const fetchapi = async () => {
     const comidas = await mealsAPI();
     setData(comidas);
-    setAcctualyFood(comidas);
+    // setAcctualyFood(comidas);
   };
+
+  // const fetchByArea = async (area) => {
+  //   const filterFoods = await getFoodsByArea(area);
+  //   console.log('Tentando', filterFoods.meals);
+  //   setAcctualyFood(filterFoods);
+  // };
 
   useEffect(() => {
     setData([]);
@@ -34,58 +40,99 @@ function ExplorarComidasArea() {
     fetchAreas();
   }, []);
 
+  useEffect(() => {
+    if (filterArea !== 'All') {
+      const fetchByArea = async (area) => {
+        const filterFoods = await getFoodsByArea(area);
+        console.log('Tentando', filterFoods.meals);
+        setAcctualyFood(filterFoods.meals);
+      };
+
+      fetchByArea(filterArea);
+    } else {
+      setAcctualyFood(data);
+    }
+  }, [filterArea, data]);
+
   const handleChange = (event) => {
     const { target: { value } } = event;
-    console.log('Mudança', value);
-    // setFilterArea(value);
-    setAcctualyFood(data.filter((meal) => meal.strArea === value));
-    console.log('Entrou aqui!');
+
+    setFilterArea(value);
   };
 
   const areasCard = () => {
-    // const filterByArea = data.filter((meal) => meal.strArea === filterArea);
-    // console.log('Filtro by Area', filterByArea);
-    // console.log(target.value);
-    console.log('Teste de', acctualyFood);
-
-    return acctualyFood.map((item, index) => {
-      const magicNumber = 12;
-      if (index < magicNumber) {
-        return (
-          <Link to={ `/comidas/${item.idMeal}` } key={ index }>
-            <Card
-              key={ item.strArea }
-              style={ { width: '8rem' } }
-              data-testid={ `${index}-recipe-card` }
-              className="shadow m-1 rounded"
-              // onClick={ handleClick }
-            >
-              <Card.Img
-                variant="top"
-                data-testid={ `${index}-card-img` }
-                alt={ item.strMeal }
-                src={ item.strMealThumb }
-              />
-              <Card.Body>
-                <Card.Title data-testid={ `${index}-card-name` }>
-                  { item.strMeal }
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </Link>
-        );
-      }
-      return null;
-    });
+    if (filterArea !== undefined) {
+      console.log('Render do All');
+      return acctualyFood.map((item, index) => {
+        const magicNumber = 12;
+        if (index < magicNumber) {
+          return (
+            <Link to={ `/comidas/${item.idMeal}` } key={ index }>
+              <Card
+                key={ item.strArea }
+                style={ { width: '8rem' } }
+                data-testid={ `${index}-recipe-card` }
+                className="shadow m-1 rounded"
+                // onClick={ handleClick }
+              >
+                <Card.Img
+                  variant="top"
+                  data-testid={ `${index}-card-img` }
+                  alt={ item.strMeal }
+                  src={ item.strMealThumb }
+                />
+                <Card.Body>
+                  <Card.Title data-testid={ `${index}-card-name` }>
+                    { item.strMeal }
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Link>
+          );
+        }
+        return null;
+      });
+    }
+    console.log('Diferente de All');
+    // return acctualyFood.map((item, index) => (
+    //   <Link to={ `/comidas/${item.idMeal}` } key={ index }>
+    //     <Card
+    //       key={ item.strArea }
+    //       style={ { width: '8rem' } }
+    //       data-testid={ `${index}-recipe-card` }
+    //       className="shadow m-1 rounded"
+    //       // onClick={ handleClick }
+    //     >
+    //       <Card.Img
+    //         variant="top"
+    //         data-testid={ `${index}-card-img` }
+    //         alt={ item.strMeal }
+    //         src={ item.strMealThumb }
+    //       />
+    //       <Card.Body>
+    //         <Card.Title data-testid={ `${index}-card-name` }>
+    //           { item.strMeal }
+    //         </Card.Title>
+    //       </Card.Body>
+    //     </Card>
+    //   </Link>
+    // ));
   };
 
   // console.log('Teste data', data);
-  // console.log('Teste de', acctualyFood);
+  console.log('Teste de', acctualyFood);
 
   return (
     <div>
       <Header title="Explorar Origem" />
       <select data-testid="explore-by-area-dropdown" onChange={ handleChange }>
+        <option
+          key="All"
+          value="All"
+          data-testid="All-option"
+        >
+          All
+        </option>
         { areas !== '' && areas !== undefined && areas.meals !== undefined
           ? areas.meals
             .map(
--- src/services/apisArea.js
+++ src/services/apisArea.js
@@ -1,6 +1,16 @@
-export default async function getAreas() {
+export async function getAreas() {
   try {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
+    const res = response.json();
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getFoodsByArea(area) {
+  try {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`);
     const res = response.json();
     return res;
   } catch (error) {
