656f28d4e371e9394802dd16f1f365f337bcf08d
Stanrley Barbosa - Surface <3011stan@gmail.com>
2021-07-09 17:53:52 -0300

    Fix bug logout button

--- src/pages/ProfileScreen/ProfileScreen.jsx
+++ src/pages/ProfileScreen/ProfileScreen.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Context from '../../context/Context';
 import styleCard, { styleBtn } from './index.style';
-import clearLS from '../../services/localStorage/clearLS';
+// import clearLS from '../../services/localStorage/clearLS';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 // import doneRecipes from '../../services/mokcInformation';
@@ -34,7 +34,8 @@ const ProfileScreen = () => {
     if (target.id === 'logout') {
       console.log('Logout clcikec!');
       setLogout(true); // setando variavel "logout" para verdadeiro, usuário deslogado!
-      clearLS();
+      // clearLS();
+      localStorage.setItem('user', JSON.stringify({}));
     }
   };
 
