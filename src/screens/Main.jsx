969719412a799a4fc1f9f332be11e9207e90cd16
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-09 18:19:41 -0300

    integraçao da pagina principal com a de pesquisa

--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -1,26 +1,41 @@
 import React, { useContext, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import ContextRecipes from '../context/ContextRecipes';
 import FooterBar from '../components/FooterBar';
+import SearchBar from '../components/SearchBar';
 
 import Card from '../components/Card';
 import CategoriesButtons from '../components/CategoriesButtons';
 import '../styleSheets/Main.css';
 
 function Main() {
-  const { getRecipes, getCategories, filteredRecipe } = useContext(ContextRecipes);
+  const { getRecipes, getCategories, filteredRecipe,
+    searchBtn } = useContext(ContextRecipes);
   const { pathname } = useLocation();
+  const history = useHistory();
   const type = pathname === '/comidas' ? 'Meal' : 'Drink';
   const cardsQuantity = 12;
+
+  useEffect(() => {
+    if (searchBtn && filteredRecipe && filteredRecipe.length === 1) {
+      history.push(`${pathname}/${filteredRecipe[0].idMeal}`);
+    }
+    if (searchBtn && !filteredRecipe) {
+      const ms = 'Sinto muito, não encontramos nenhuma receita para esses filtros.';
+      return window.alert(ms);
+    }
+  }, [searchBtn]);
+
   useEffect(() => {
     getRecipes('All', type);
     getCategories(type);
   }, [pathname]);
   return (
     <main className="main-container">
+      <SearchBar />
       <section className="content-container">
         <section className="recipe-cards-container">
-          { filteredRecipe.reduce((acc, recipe, index) => {
+          { filteredRecipe && filteredRecipe.reduce((acc, recipe, index) => {
             if (index < cardsQuantity) {
               acc.push(
                 <Card
