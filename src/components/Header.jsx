4423994845d7398a207ef9a625e7c17edffb53ef
maricegalla <maricegalla@gmail.com>
2021-07-09 17:05:47 -0300

    Guilherme salva

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import './Style.css';
 
+// Stan <3
+
 function Header() {
   const pageTitle = () => {
     const path = window.location.pathname;
