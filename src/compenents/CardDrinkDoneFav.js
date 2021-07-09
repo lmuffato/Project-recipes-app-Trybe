78bf825cc737e9465a245199eac7942043c2b16d
hheloisa <hhackenhaar@gmail.com>
2021-07-08 21:47:53 -0300

    Feat: requisito 57

--- src/compenents/CardDrinkDoneFav.js
+++ src/compenents/CardDrinkDoneFav.js
@@ -1,25 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Share from '../images/shareIcon.svg';
 
-function CardDrinkDoneFav(recepie, index) {
-  const { id, name, image, doneDate, alcoholicOrNot } = recepie.recepie;
+const copy = require('clipboard-copy');
+
+function CardDrinkDoneFav({ recepie }, index) {
+  const { id, name, image, doneDate, alcoholicOrNot } = recepie;
+
+  function copyLink() {
+    const linkToCopy = `/bebidas/${id}`;
+    copy(linkToCopy);
+    global.alert('Link copiado!');
+  }
+
   return (
     <div>
       <Link to={ `/bebidas/${id}` }>
         <img
           data-testid={ `${index}-horizontal-image` }
           alt="the recepie drink"
-          width='50px'
+          width="50px"
           src={ image }
         />
         <p data-testid={ `${index}-horizontal-name` }>{ name }</p>
         <p>{alcoholicOrNot}</p>
         <p data-testid={ `${index}-horizontal-done-date` }>{ doneDate }</p>
-        <img data-testid={ `${index}-horizontal-share-btn` } src={ Share } alt="share" />
+        <button
+          type="button"
+          onClick={ () => copyLink() }
+        >
+          <img
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ Share }
+            alt="share"
+          />
+        </button>
       </Link>
     </div>
   );
 }
+
+CardDrinkDoneFav.propTypes = {
+  recepie: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    image: PropTypes.string,
+    doneDate: PropTypes.string,
+    alcoholicOrNot: PropTypes.string,
+  }).isRequired,
+};
 
 export default CardDrinkDoneFav;
--- src/compenents/CardMealDoneFav.js
+++ src/compenents/CardMealDoneFav.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Share from '../images/shareIcon.svg';
 
-function CardMealDoneFav(recepie, index) {
+const copy = require('clipboard-copy');
+
+function CardMealDoneFav({ recepie }, index) {
   console.log(recepie);
-  const { id, name, image, category, area, doneDate, tags } = recepie.recepie;
+  const { id, name, image, category, area, doneDate, tags } = recepie;
+
+  function copyLink() {
+    const linkToCopy = `/comidas/${id}`;
+    copy(linkToCopy);
+    global.alert('Link copiado!');
+  }
+
   return (
     <div>
       <Link to={ `/comidas/${id}` }>
         <img
           data-testid={ `${index}-horizontal-image` }
           src={ image }
-          width='50px'
+          width="50px"
           alt="the food recepie"
         />
         <p data-testid={ `${index}-horizontal-name` }>{ name }</p>
@@ -23,10 +33,31 @@ function CardMealDoneFav(recepie, index) {
         >
           { `${tags[0]}, ${tags[1]}` }
         </p>
-        <img data-testid={ `${index}-horizontal-share-btn` } src={ Share } alt="share" />
+        <button
+          type="button"
+          onClick={ () => copyLink() }
+        >
+          <img
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ Share }
+            alt="share"
+          />
+        </button>
       </Link>
     </div>
   );
 }
+
+CardMealDoneFav.propTypes = {
+  recepie: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    image: PropTypes.string,
+    category: PropTypes.string,
+    area: PropTypes.string,
+    doneDate: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+};
 
 export default CardMealDoneFav;
