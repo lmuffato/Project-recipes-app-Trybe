7182d4bbd19d96049b20534eec7af7e28ad0313b
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-07-10 09:16:53 -0400

    req 58 - pronto

--- src/Pages/DoneRecepies.js
+++ src/Pages/DoneRecepies.js
@@ -6,16 +6,29 @@ import shareIcon from '../images/shareIcon.svg';
 export default function DoneRecepies() {
   const doneRecipies = JSON.parse(localStorage.getItem('doneRecipes'));
   const [copy, setCopy] = useState('');
+  const [filter, setFilter] = useState('');
 
   const filterButtons = () => (
     <section>
-      <button type="button" data-testid="filter-by-all-btn">
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ () => setFilter('') }
+      >
         All
       </button>
-      <button type="button" data-testid="filter-by-food-btn">
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ () => setFilter('food') }
+      >
         Food
       </button>
-      <button type="button" data-testid="filter-by-drink-btn">
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ () => setFilter('drink') }
+      >
         Drinks
       </button>
     </section>
@@ -34,55 +47,62 @@ export default function DoneRecepies() {
     setCopy('Link copiado!');
   };
 
-  const renderCards = (data) => (
-    data.map((recipe, index) => {
-      const { type, id, doneDate, image, category, name, area, alcoholicOrNot } = recipe;
-      return (
-        <div
-          key={ index }
-          className="card"
-        >
-          <Link to={ `/${type}s/${id}` } key={ name }>
-            <div>
-              <h5 data-testid={ `${index}-horizontal-top-text` }>
-                {
-                  area !== '' ? `${area} - ${category}`
-                    : `${alcoholicOrNot} - ${category}`
-                }
-              </h5>
+  const renderCards = (data) => {
+    let aux = data;
+    if (filter === 'food') aux = data.filter((e) => e.type === 'comida');
+    if (filter === 'drink') aux = data.filter((e) => e.type === 'bebida');
+    return (
+      aux.map((recipe, index) => {
+        const {
+          type, id, doneDate, image, category, name, area, alcoholicOrNot,
+        } = recipe;
+        return (
+          <div
+            key={ index }
+            className="card"
+          >
+            <Link to={ `/${type}s/${id}` } key={ name }>
+              <div>
+                <h4
+                  data-testid={ `${index}-horizontal-name` }
+                  className="title"
+                >
+                  {name}
+                </h4>
+                <h5 data-testid={ `${index}-horizontal-top-text` }>
+                  {
+                    area !== '' ? `${area} - ${category}`
+                      : `${alcoholicOrNot} - ${category}`
+                  }
+                </h5>
+                <img
+                  alt="recipe"
+                  data-testid={ `${index}-horizontal-image` }
+                  className="recipe-card-image"
+                  src={ image }
+                />
+              </div>
+            </Link>
+            <button
+              type="button"
+              onClick={ () => copyLink(type, id) }
+            >
               <img
-                alt="recipe"
-                data-testid={ `${index}-horizontal-image` }
-                className="recipe-card-image"
-                src={ image }
+                src={ shareIcon }
+                alt="share"
+                data-testid={ `${index}-horizontal-share-btn` }
               />
+            </button>
+            {copy}
+            <h5 data-testid={ `${index}-horizontal-done-date` }>{doneDate}</h5>
+            <div>
+              {recipe.tags ? mapTags(recipe.tags, index) : null}
             </div>
-          </Link>
-          <button
-            type="button"
-            onClick={ () => copyLink(type, id) }
-          >
-            <img
-              src={ shareIcon }
-              alt="share"
-              data-testid={ `${index}-horizontal-share-btn` }
-            />
-          </button>
-          {copy}
-          <h4
-            data-testid={ `${index}-horizontal-name` }
-            className="title"
-          >
-            {name}
-          </h4>
-          <h5 data-testid={ `${index}-horizontal-done-date` }>{doneDate}</h5>
-          <div>
-            {recipe.tags ? mapTags(recipe.tags, index) : null}
           </div>
-        </div>
-      );
-    })
-  );
+        );
+      })
+    );
+  };
 
   return (
     <>
