4711587405feed6ee069a628c9f6d13b1ae89148
Perycles reis <perycles_reis@hotmail.com>
2021-07-10 11:58:58 -0300

    refactoring v1.7

--- src/components/EmProgressPage/Ingredientes.jsx
+++ src/components/EmProgressPage/Ingredientes.jsx
@@ -1,35 +1,51 @@
 import { object } from 'prop-types';
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function Ingredientes({ params: { ingredientsList, data } }) {
   const [ingArray, setIngArray] = useState([]);
-  const [isChecked, setIsChecked] = useState([]);
+  // const [isChecked, setIsChecked] = useState([]);
+  const LOCATION = useLocation();
 
-  const achando = () => {
+  const getLocalStorage = () => {
     const progressRecipes = JSON.parse(localStorage
       .getItem('inProgressRecipes'));
-    // console.log(progressRecipes);
+
     if (progressRecipes !== null) {
       const { cocktails, meals } = progressRecipes;
-      const elements = document.querySelector('.py-2').nextSibling.childNodes;
+      const elementsTag = document.querySelector('.py-2').nextSibling.childNodes;
+      let valuesMeals;
+      let valuesCocktails;
 
-      const test = Object.values(meals);
-
-      elements.forEach((item, index) => {
-        // console.log(item);
-        test[0].forEach((element) => {
-          // console.log(element);
-          if (index === element) {
-            elements[index].firstChild.className = 'isCheckedCss';
-            elements[index].firstChild.firstChild.checked = true;
-          }
-        });
+      elementsTag.forEach((elementTag, index) => {
+        switch (LOCATION.pathname) {
+        case `/comidas/${data.idMeal}/in-progress`:
+          valuesMeals = Object.values(meals);
+          valuesMeals[0].forEach((valueMeal) => {
+            if (index === valueMeal) {
+              elementsTag[index].firstChild.className = 'isCheckedCss';
+              elementsTag[index].firstChild.firstChild.checked = true;
+            }
+          });
+          break;
+        case `/bebidas/${data.Drink}/in-progress`:
+          valuesCocktails = Object.values(cocktails);
+          valuesCocktails[0].forEach((valueCocktail) => {
+            if (index === valueCocktail) {
+              elementsTag[index].firstChild.className = 'isCheckedCss';
+              elementsTag[index].firstChild.firstChild.checked = true;
+            }
+          });
+          break;
+        default:
+          break;
+        }
       });
     }
   };
 
   useEffect(() => {
-    achando();
+    getLocalStorage();
   }, []);
 
   const localSTSettings = (target) => {
@@ -68,7 +84,7 @@ function Ingredientes({ params: { ingredientsList, data } }) {
   };
 
   const isCheckedBool = ({ target }) => {
-    const { checked, name, id } = target;
+    const { checked } = target;
 
     if (checked) {
       document.querySelectorAll('.mr-2')[6].checked = true;
@@ -99,7 +115,7 @@ function Ingredientes({ params: { ingredientsList, data } }) {
               name={ ingredient[1] }
               id={ index }
               onClick={ isCheckedBool }
-              checked={ isChecked }
+              // checked={ isChecked }
             />
             {`Ingrediente ${index}: ${ingredient[1]}`}
           </label>
