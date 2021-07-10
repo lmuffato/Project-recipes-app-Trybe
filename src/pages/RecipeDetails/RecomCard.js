b6be40b5e5c05d66b79074d0927da900e31f2367
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-07-10 10:17:34 -0300

    feat: requirement 37

--- src/pages/RecipeDetails/RecomCard.js
+++ src/pages/RecipeDetails/RecomCard.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function RecomCard({ recipe }) {
+export default function RecomCard({ recipe, index }) {
   return (
     <div>
       <img
         src={ recipe.strMealThumb || recipe.strDrinkThumb }
         data-testid="recipe-photo"
-        width="80"
+        width="100"
         alt="recipe details"
       />
-      <h2 data-testid="recipe-title">
+      <h3 data-testid={ `${index}-recomendation-title` }>
         { recipe.strMeal || recipe.strDrink }
-      </h2>
+      </h3>
     </div>
   );
 }
 
 RecomCard.propTypes = {
   recipe: PropTypes.objectOf(PropTypes.string).isRequired,
+  index: PropTypes.string.isRequired,
 };
--- src/pages/RecipeDetails/Recommendations.js
+++ src/pages/RecipeDetails/Recommendations.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import RecomCard from './RecomCard';
 import { fetchRecipesApi } from '../../services/fetchApiMain';
+import './recipesDetails.css';
 
 export default function Recommendations({ path }) {
   const [recommended, setRecommended] = useState([]);
-
   const NUM_RECIPES_SHOWN = 6;
 
   useEffect(() => {
@@ -16,20 +16,71 @@ export default function Recommendations({ path }) {
       });
   }, []);
 
-  return (
-    <div>
-      <h3>Recommendeds</h3>
-      { recommended.length
-        ? recommended.map(
+  function handleCarousel(direction) {
+    const next = direction === 'next';
+    let nextActiveId = null;
+    let activeId = null;
+
+    const recipesShown = document.getElementsByClassName('carousel active');
+
+    if (recipesShown[0].id === '0' && recipesShown[1].id === '5') {
+      activeId = Number(recipesShown[next ? 1 : 0].id);
+      recipesShown[next ? 1 : 0].className = 'carousel';
+      nextActiveId = next ? 1 : 4;
+    } else {
+      activeId = Number(recipesShown[next ? 0 : 1].id);
+      recipesShown[next ? 0 : 1].className = 'carousel';
+
+      if (next ? (activeId < recommended.length - 2) : (activeId > 1)) {
+        nextActiveId = next ? (activeId + 2) : (activeId - 2);
+      } else {
+        nextActiveId = next ? 0 : 5;
+      }
+    }
+    document.getElementById(nextActiveId).className = 'carousel active';
+  }
+
+  function renderCarousel() {
+    console.log(recommended[0]);
+    return (
+      <div className="carousel-container">
+        <button
+          type="button"
+          onClick={ () => handleCarousel('prev') }
+        >
+          {'<'}
+        </button>
+        {recommended.map(
           (recomRecipe, index) => (
             <div
-              data-testid="0-recomendation-card"
+              data-testid={ `${index}-recomendation-card` }
               key={ index }
+              className={ `carousel ${index === 0 || index === 1 ? 'active' : ''}` }
+              id={ index }
             >
-              <RecomCard recipe={ recomRecipe } />
+              <RecomCard
+                recipe={ recomRecipe }
+                index={ index }
+              />
             </div>
           ),
-        ) : ''}
+        ) }
+        <button
+          type="button"
+          onClick={ () => handleCarousel('next') }
+        >
+          {'>'}
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h3>Recommended</h3>
+      { recommended.length
+        ? renderCarousel()
+        : <span>Loading...</span>}
     </div>
   );
 }
