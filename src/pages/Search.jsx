7ba6a4aa5ff7775593ccc74e4ba2637a9fba667c
Felipe Muller <felipeavmuller@gmail.com>
2021-07-10 14:44:21 -0300

    feat: requirements 67 68 69

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -7,6 +8,14 @@ function Search() {
     <div>
       Pagina Search
       <Header title="Explorar" />
+      <div>
+        <Link to="/explorar/comidas">
+          <button type="button" data-testid="explore-food">Explorar Comidas</button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
