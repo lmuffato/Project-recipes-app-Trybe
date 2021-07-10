9bb12112aeba1f69b84470a5220ed0ab8c22795e
andy silva <andssilva.23@gmail.com>
2021-07-10 17:29:31 -0400

    feat(req 87): :rocket: logout button

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Perfil() {
-  const getEmail = JSON.parse(localStorage.getItem('user')).email;
+  const getEmail = JSON.parse(localStorage.getItem('user'));
+
+  const done = () => {
+    window.location.href = '/receitas-feitas';
+  };
+  const favorite = () => {
+    window.location.href = '/receitas-favoritas';
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
 
   return (
     <div>
       <Header title="Perfil" />
-      <h2 data-testid="profile-email">{getEmail}</h2>
-      <button type="button">
-        <Link to="/receitas-feitas" data-testid="profile-done-btn">
-          Receitas Feitas
-        </Link>
+      <h2 data-testid="profile-email">{getEmail ? getEmail.email : ''}</h2>
+      <button type="button" data-testid="profile-done-btn" onClick={ () => done() }>
+        Receitas Feitas
       </button>
-      <button type="button">
-        <Link to="/receitas-favoritas" data-testid="profile-favorite-btn">
-          Receitas Favoritas
-        </Link>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => favorite() }
+      >
+        Receitas Favoritas
       </button>
-      <button type="button" data-testid="profile-logout-btn">
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => logout() }
+      >
         Sair
       </button>
       <Footer />
