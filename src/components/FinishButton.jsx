53160b4fdf71f80512db00320c2aeb64f7e80180
Felipe Flores <felipefloresvendas@gmail.com>
2021-07-10 12:36:54 -0300

    feat requirement 53

--- src/components/FinishButton.jsx
+++ src/components/FinishButton.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import RecipeFinish from '../effects/RecipeFinish';
 
@@ -7,18 +8,23 @@ export default function FinishButton({ ingredients }) {
   const [ingredient, setIngredients] = useState({
     currIngredientsLocal: [],
     finish: false,
-    disable: false,
+    redirect: false,
   });
+
+  const shouldRedirect = () => {
+    setIngredients({ ...ingredient, redirect: true });
+  };
+
   RecipeFinish(ingredients, ingredient, setIngredients);
-  const { finish } = ingredient;
-  console.log(finish);
+  const { finish, redirect } = ingredient;
+  if (redirect) return <Redirect to="/receitas-feitas" />;
   return (
     <Button
       className="fixed-bottom"
       variant="dark"
       data-testid="finish-recipe-btn"
       default
-      onClick={ () => console.log('clicou') }
+      onClick={ () => shouldRedirect() }
       disabled={ !finish }
     >
       Finish Recipe
