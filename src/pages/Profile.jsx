ce4472f346bfe9fad7cf0548de52954c0851ce98
Elisa <elisafranca.carvalho@gmail.com>
2021-07-11 16:12:58 -0300

    feat: implements requirement 87

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import InferiorMenu from '../components/InferiorMenu';
 import { getItemFromLocalStorage } from '../services/localStorage';
 
 export default function Profile() {
   const [user, setUser] = useState('');
+  const history = useHistory();
 
   useEffect(() => {
     const getStorage = () => {
@@ -15,6 +16,11 @@ export default function Profile() {
     };
     getStorage();
   }, []);
+
+  const handleClick = () => {
+    localStorage.clear();
+    history.push('/');
+  };
 
   return (
     <div>
@@ -33,7 +39,13 @@ export default function Profile() {
           Receitas Favoritas
         </Button>
       </Link>
-      <Button variant="dark" data-testid="profile-logout-btn">Sair</Button>
+      <Button
+        variant="dark"
+        data-testid="profile-logout-btn"
+        onClick={ handleClick }
+      >
+        Sair
+      </Button>
       <InferiorMenu />
     </div>
   );
