daae6874e7500021968a02d218f4917ece087611
Elisa <elisafranca.carvalho@gmail.com>
2021-07-11 15:55:26 -0300

    feat: implements requirements 82, 83 and 84

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import Header from '../components/Header';
 import InferiorMenu from '../components/InferiorMenu';
+import { getItemFromLocalStorage } from '../services/localStorage';
 
 export default function Profile() {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    const getStorage = () => {
+      const userFromStorage = getItemFromLocalStorage('user');
+      if (userFromStorage) setUser(userFromStorage.email);
+    };
+    getStorage();
+  }, []);
+
   return (
     <div>
       <Header>
         <h1 data-testid="page-title">Perfil</h1>
       </Header>
+      <p data-testid="profile-email">{ user }</p>
+      <Button variant="dark" data-testid="profile-done-btn">Receitas Feitas</Button>
+      <Button
+        variant="dark" 
+        data-testid="profile-favorite-btn"
+      >
+        Receitas Favoritas
+      </Button>
+      <Button variant="dark" data-testid="profile-logout-btn">Sair</Button>
       <InferiorMenu />
     </div>
   );
