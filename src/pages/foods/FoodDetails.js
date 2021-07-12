8e2ff9eab9d14c7f9e30b5503a9613081d1944ab
andy silva <andssilva.23@gmail.com>
2021-07-12 15:57:45 -0400

    refactor: :art: recipe details screen

--- src/pages/foods/FoodDetails.js
+++ src/pages/foods/FoodDetails.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useStateEasyRedux } from 'easy-redux-trybe';
-import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import copy from 'clipboard-copy';
 import positions from '../../services/data';
@@ -9,6 +8,7 @@ import createIngredients from '../../services/functions';
 import shareIcon from '../../images/shareIcon.svg';
 import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../../images/blackHeartIcon.svg';
+import { setLocalStorage, getLocalStorage } from '../../helper';
 
 import styles from '../../styles/DetailsPages.module.scss';
 
@@ -16,9 +16,11 @@ function FoodDetails(props) {
   const { match: { params: { id } } } = props;
 
   const history = useHistory();
-  const [, setStateRedux] = useStateEasyRedux(FoodDetails, {});
-  const [, setCopyUrl] = useStateEasyRedux({ name: 'copyFood' }, {});
-  const [, setFavoriteRecipe] = useStateEasyRedux({ name: 'favoriteRecipe' }, {});
+  const [stateRedux, setStateRedux] = useStateEasyRedux(FoodDetails, {});
+  const [copyUrl, setCopyUrl] = useStateEasyRedux({ name: 'copyFood' }, {});
+  const [favoriteRecipe, setFavoriteRecipe] = useStateEasyRedux(
+    { name: 'favoriteRecipe' }, {},
+  );
 
   useEffect(() => {
     const fetchRecipe = async () => {
@@ -38,33 +40,21 @@ function FoodDetails(props) {
       setStateRedux({ actionType: 'FETCH_FOOD', responseFood, resultRecommendations });
       setCopyUrl({ actionType: 'COPY_URL', copyRecipe: false });
 
-      const getRecipe = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
-      const resultGet = getRecipe.some((element) => element.id === id);
+      const getRecipe = getLocalStorage('favoriteRecipes');
+      const resultGet = getRecipe && getRecipe.some((element) => element.id === id);
       setFavoriteRecipe({ actionType: 'FAVORITE_RECIPE', favorite: resultGet });
     };
     fetchRecipe();
     // eslint-disable-next-line
   }, []);
 
-  const foods = useSelector((state) => (
-    state.FoodDetails ? state.FoodDetails.responseFood : undefined
-  ));
-
-  const drinksRecommendations = useSelector((state) => (
-    state.FoodDetails ? state.FoodDetails.resultRecommendations : undefined
-  ));
-
-  const copyTrue = useSelector((state) => (
-    state.copyFood ? state.copyFood.copyRecipe : undefined
-  ));
-
-  const favorite = useSelector((state) => (
-    state.favoriteRecipe ? state.favoriteRecipe.favorite : undefined
-  ));
+  const { responseFood, resultRecommendations } = stateRedux;
+  const { copyRecipe } = copyUrl;
+  const { favorite } = favoriteRecipe;
 
   const choiceRec = (element) => history.push(`/bebidas/${element}`);
 
-  const copyUrl = () => {
+  const copyUrlLink = () => {
     copy(window.location.href.toString());
     setCopyUrl({ copyRecipe: true });
     const time = 3000;
@@ -75,7 +65,7 @@ function FoodDetails(props) {
 
   const clickFavorite = ({ idMeal, strArea, strCategory, strMeal, strMealThumb }) => {
     if (!favorite) {
-      const favoriteRecipe = [{
+      const favoriteRecipeObj = [{
         id: idMeal,
         type: 'comida',
         area: strArea,
@@ -84,20 +74,18 @@ function FoodDetails(props) {
         name: strMeal,
         image: strMealThumb,
       }];
-      localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipe));
+      setLocalStorage('favoriteRecipes', favoriteRecipeObj);
     } else {
-      const removeFavorite = JSON.parse(localStorage.getItem('favoriteRecipes'))
+      const removeFavorite = getLocalStorage('favoriteRecipes')
         .filter((el) => el.id !== idMeal);
-      localStorage.setItem('favoriteRecipes', JSON.stringify(removeFavorite));
+      setLocalStorage('favoriteRecipes', removeFavorite);
     }
     setFavoriteRecipe({ favorite: !favorite });
   };
 
-  console.log(foods);
-
   return (
     <div>
-      {foods && foods.map(
+      {responseFood && responseFood.map(
         (el) => (
           <div className={ styles.areaRecipe } key={ el.idMeal }>
             <img
@@ -107,13 +95,13 @@ function FoodDetails(props) {
               className={ styles.imgThumb }
             />
             <div className={ styles.containerContent }>
-              {copyTrue && <span className={ styles.copyUrl }>Link copiado!</span>}
+              {copyRecipe && <span className={ styles.copyUrl }>Link copiado!</span>}
               <div className={ styles.headerContent }>
                 <h1 data-testid="recipe-title">{ el.strMeal }</h1>
                 <button
                   type="button"
                   data-testid="share-btn"
-                  onClick={ () => copyUrl() }
+                  onClick={ () => copyUrlLink() }
                 >
                   <img src={ shareIcon } alt="Compartilhar" />
                 </button>
@@ -146,7 +134,7 @@ function FoodDetails(props) {
               />
               <h3>Recommendations</h3>
               <div className={ styles.carousel }>
-                {drinksRecommendations && drinksRecommendations.map((element, index) => (
+                {resultRecommendations && resultRecommendations.map((element, index) => (
                   <div
                     key={ element.idDrink }
                     data-testid={ `${index}-recomendation-card` }
