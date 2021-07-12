111071f354bca58e6e7d93ec01b84d79e004e2d9
João Nasc <oi@joaonasc.dev>
2021-07-12 16:27:19 -0300

    feat: create and style search component

--- src/components/Header/components/Search/index.jsx
+++ src/components/Header/components/Search/index.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Search() {
+import styles from './styles.module.scss';
+
+function Search({ visibility }) {
   return (
-    <div>
-      Busca
-    </div>
+    <form className={ `${styles.search} ${visibility && styles.visible}` }>
+      <input type="text" data-testid="search-input" placeholder="Buscar receita" />
+      <div className={ styles.options }>
+        <label htmlFor="ingredient" data-testid="ingredient-search-radio">
+          <input type="radio" name="search-type" id="ingredient" />
+          Ingrediente
+        </label>
+        <label htmlFor="name" data-testid="name-search-radio">
+          <input type="radio" name="search-type" id="name" />
+          Nome
+        </label>
+        <label htmlFor="first-letter" data-testid="first-letter-search-radio">
+          <input type="radio" name="search-type" id="first-letter" />
+          Primeira letra
+        </label>
+      </div>
+      <button className="primary-btn" type="submit">Buscar</button>
+    </form>
   );
 }
+
+Search.propTypes = {
+  visibility: PropTypes.string.isRequired,
+};
 
 export default Search;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { BiSearch } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
+import Search from './components/Search';
 
 import styles from './styles.module.scss';
 import useLocalStorage from '../../hooks/useLocalStorage';
 
 function Header({ title }) {
+  const [searchMode, setSearchMode] = useState(false);
+
   const { user, gravatar } = useLocalStorage('user', 'gravatar');
   return (
     <header className={ styles.header }>
-      <Link to="/perfil" className={ styles.profilePic }>
-        <img
-          src={ gravatar || 'https://www.gravatar.com/avatar/default' }
-          alt={ user ? user.email : 'user@mail.com' }
-        />
-      </Link>
-      { title }
-      <BiSearch />
+      <div>
+        <Link to="/perfil" className={ styles.profilePic }>
+          <img
+            src={ gravatar || 'https://www.gravatar.com/avatar/default' }
+            alt={ user ? user.email : 'user@mail.com' }
+          />
+        </Link>
+        { title }
+        <BiSearch onClick={ () => setSearchMode(!searchMode) } />
+      </div>
+      <Search visibility={ searchMode } />
     </header>
   );
 }
