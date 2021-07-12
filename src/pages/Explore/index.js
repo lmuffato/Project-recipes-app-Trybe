e7b1a43d52bdc5081f957863f9541b8f7af1d315
Mateus Alencar <mtauws@gmail.com>
2021-07-12 18:32:53 -0300

    revert: explore

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -6,7 +6,7 @@ import Header from '../../components/Header';
 import './style.css';
 
 const Explore = () => (
-  <div className="screen-explore">
+  <div>
     <Header title="Explorar" />
 
     <div className="buttons-explore">
