453f690e586bd30bb4fe6e74d2b5aeb4d5421f07
Mateus Alencar <mtauws@gmail.com>
2021-07-12 17:22:47 -0300

    refix: login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -10,16 +10,28 @@ const Login = () => {
   const [validPassword, setValidPassword] = useState(false);
 
   const emailIsValid = ({ target }) => {
+    const inputEmail = document.querySelector('.email');
     const { value } = target;
     const emailValidation = /^[\S.]+@[a-z]+\.\w{2,3}$/g.test(value);
     setEmail(value);
     setValidEmail(emailValidation);
+    if (validEmail && inputEmail.value.length >= 1) {
+      inputEmail.style.border = '2px solid #04AA6B';
+    } else {
+      inputEmail.style.border = '2px solid #D70E21';
+    }
   };
 
   const passwordIsValid = ({ target }) => {
+    const inputPassword = document.querySelector('.password');
     const { value } = target;
     const passwordValidation = value.length >= Number('7');
     setValidPassword(passwordValidation);
+    if (passwordValidation) {
+      inputPassword.style.border = '2px solid #04AA6B';
+    } else {
+      inputPassword.style.border = '2px solid #D70E21';
+    }
   };
 
   const handleSubmit = () => {
@@ -30,34 +42,42 @@ const Login = () => {
   };
 
   return (
-    <div className="container">
-      <div className="line">
-        <h2>Login</h2>
-        <form className="form">
-          <input
-            type="email"
-            className="input"
-            placeholder="Email"
-            data-testid="email-input"
-            onChange={ emailIsValid }
-          />
-          <input
-            type="password"
-            className="input"
-            placeholder="Senha"
-            data-testid="password-input"
-            onChange={ passwordIsValid }
-          />
-          <button
-            type="submit"
-            className="login-submit-btn"
-            data-testid="login-submit-btn"
-            disabled={ !(validEmail && validPassword) }
-            onClick={ handleSubmit }
-          >
-            Entrar
-          </button>
-        </form>
+    <div className="screen">
+      <div className="container">
+        <div className="line">
+          <h2 className="title-login">Login</h2>
+          <form className="form">
+            <input
+              type="email"
+              className="input email"
+              placeholder="Email"
+              data-testid="email-input"
+              onChange={ emailIsValid }
+            />
+            <input
+              type="password"
+              className="input password"
+              placeholder="Senha"
+              data-testid="password-input"
+              onChange={ passwordIsValid }
+            />
+            <button
+              type="submit"
+              className="login-submit-btn"
+              data-testid="login-submit-btn"
+              disabled={ !(validEmail && validPassword) }
+              onClick={ handleSubmit }
+            >
+              Entrar
+            </button>
+            <span className="have-account">
+              Don&apos;t have a account?
+            </span>
+            <span className="create-account">
+              Create an account
+            </span>
+          </form>
+        </div>
       </div>
     </div>
   );
