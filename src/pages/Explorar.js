fdf76d3a33c073d6bfa45c5b6910952747f5627e
Bruno Mendes <mendessbruno@gmail.com>
2021-07-12 20:04:25 -0300

    realizado a estilizacao dos botoes das paginas de explorar

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Explore.css';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -8,26 +9,28 @@ function Explorar() {
   return (
     <>
       <Header title="Explorar" displayButton={ false } />
-      <Link to="/explorar/comidas">
-        <button
-          data-testid="explore-food"
-          type="button"
-        >
-          Explorar Comidas
+      <div className="path-buttons-container">
+        <Link to="/explorar/comidas">
+          <button
+            className="path-btn"
+            data-testid="explore-food"
+            type="button"
+          >
+            Explorar Comidas
 
-        </button>
-      </Link>
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button
+            className="path-btn"
+            data-testid="explore-drinks"
+            type="button"
+          >
+            Explorar Bebidas
 
-      <Link to="/explorar/bebidas">
-        <button
-          data-testid="explore-drinks"
-          type="button"
-        >
-          Explorar Bebidas
-
-        </button>
-      </Link>
-
+          </button>
+        </Link>
+      </div>
       <Footer />
     </>
   );
