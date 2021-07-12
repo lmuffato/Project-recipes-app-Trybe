8046b7b28657f8fa51693da8ddb7b3f59fc735a3
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-07-12 15:56:38 -0300

    fix: render area link

--- src/pages/Explore/components/ExploreButtons/index.jsx
+++ src/pages/Explore/components/ExploreButtons/index.jsx
@@ -1,11 +1,28 @@
 import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import styles from '../../styles.module.scss';
 
 import getRecipes from '../../../../services/recipesData';
 
 function ExploreButtons({ title }) {
   const [randomId, setRandomId] = useState('');
+
+  function renderAreaLink() {
+    if (title === 'comidas') {
+      return (
+        <Link to="/explorar/comidas/area">
+          <button
+            type="button"
+            className="primary-btn"
+            data-testid="explore-by-area"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+      );
+    }
+  }
 
   async function getRandomId() {
     const { getRandom } = await getRecipes(`/${title}`);
@@ -28,15 +45,7 @@ function ExploreButtons({ title }) {
             Por Ingredientes
           </button>
         </Link>
-        {title === 'comidas' && <Link to="/explorar/comidas/area">
-          <button
-            type="button"
-            className="primary-btn"
-            data-testid="explore-by-area"
-          >
-            Por Local de Origem
-          </button>
-        </Link>}
+        {renderAreaLink()}
         <Link to={ `/${title}/${randomId}` }>
           <button
             id={ styles.last }
@@ -74,5 +83,13 @@ function ExploreButtons({ title }) {
     </>
   );
 }
+
+ExploreButtons.propTypes = {
+  title: PropTypes.string,
+};
+
+ExploreButtons.defaultProps = {
+  title: '',
+};
 
 export default ExploreButtons;
