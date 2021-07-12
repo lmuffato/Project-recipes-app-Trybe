f385f714439f6062b20da73318cf275141b9bf10
Marcelo Maurício <mmauricio.design@gmail.com>
2021-07-12 17:40:19 -0300

    feat: css login page

--- src/styles/login.js
+++ src/styles/login.js
@@ -1,5 +1,6 @@
 const divMain = 'd-flex align-items-center center';
-const divContentForm = 'shadow p-3 bg-white rounded col-md-4 offset-md-4';
+// const divContentForm = 'shadow p-3 bg-white rounded col-md-4 offset-md-4';
+const divContentForm = 'p-3 rounded col-md-4 offset-md-4';
 const textLogin = 'text-center mb-4';
 const textUpInput = 'text-muted';
 const btn = 'col-md-4 offset-md-4';
