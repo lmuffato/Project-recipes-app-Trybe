002fffc0c4a04cc23b5cf622d297443b56444d82
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-07-12 23:15:57 -0300

    created 404 page and made the css for the profile page

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../styles/Perfil.css';
 
 function Perfil() {
   const emailFinder = () => {
@@ -14,31 +15,35 @@ function Perfil() {
   };
 
   return (
-    <>
+    <div className="profile-page">
       <Header title="Perfil" displayButton={ false } />
-      { emailFinder() }
-      <Link to="/receitas-feitas">
-        <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
-      </Link>
-      <Link to="/receitas-favoritas">
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-        >
-          Receitas Favoritas
-        </button>
-      </Link>
-      <Link to="/">
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ () => localStorage.clear() }
-        >
-          Sair
-        </button>
-      </Link>
+      <div className="email-profile-page">
+        { emailFinder() }
+      </div>
+      <div className="btn-profile-page">
+        <Link to="/receitas-feitas">
+          <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+        </Link>
+        <Link to="/receitas-favoritas">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Receitas Favoritas
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => localStorage.clear() }
+          >
+            Sair
+          </button>
+        </Link>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
