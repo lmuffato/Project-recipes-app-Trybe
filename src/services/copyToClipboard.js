856ac368974c4f740dde91345b12b7d63ce8ed21
iago <iago.pferreiravr@gmail.com>
2021-07-13 17:44:07 -0300

    Fix: lint

--- src/services/copyToClipboard.js
+++
@@ -1,9 +0,0 @@
-const copyToClipboard = ({ target }, setIsCopy) => {
-  // setIsCopy(true);
-  console.log(target);
-  const { alt } = target;
-  const path = `http://localhost:3000/${alt}`;
-  navigator.clipboard.writeText(path);
-};
-
-export default copyToClipboard;
