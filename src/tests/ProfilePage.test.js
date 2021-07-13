f76efb4acdd89f348c58a3861fb8289a23e6aca9
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-07-13 15:13:26 -0300

    refactor: ProfilePage test | does not work properly yet

--- src/tests/ProfilePage.test.js
+++ src/tests/ProfilePage.test.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 
 import renderWithRouter from './renderWithRouter';
 import ProfilePage from '../pages/ProfilePage';
+import Login from '../pages/Login';
 
 const renderProfileWithRouter = () => renderWithRouter(<ProfilePage />);
 
 describe('ProfilePage', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<Login />);
+    const email = screen.getByTestId('email-input');
+    const password = screen.getByTestId('password-input');
+    const button = screen.getByTestId('login-submit-btn');
+    userEvent.type(email, 'user@email.com');
+    userEvent.type(password, '1234567');
+    userEvent.click(button);
+    history.push('/perfil');
+  });
+
   test('Verifica se exibe o email da pessoa usuária', () => {
     const { getByTestId } = renderProfileWithRouter();
     const profileEmail = getByTestId('profile-email');
@@ -18,7 +31,9 @@ describe('ProfilePage', () => {
     const recipesDoneButton = getByTestId('profile-done-btn');
     expect(recipesDoneButton).toBeInTheDocument();
     userEvent.click(recipesDoneButton);
-    expect(history.location.pathname).toBe('/receitas-feitas');
+    // expect(history.location.pathname).toBe('/receitas-feitas');
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/receitas-feitas');
   });
 
   test('Verifica se exibe o botão para receitas favoritas', () => {
@@ -35,5 +50,8 @@ describe('ProfilePage', () => {
     expect(logoutButton).toBeInTheDocument();
     userEvent.click(logoutButton);
     expect(history.location.pathname).toBe('/');
+    expect(localStorage.getItem('mealsToken')).toBeNull();
+    expect(localStorage.getItem('cocktailsToken')).toBeNull();
+    expect(localStorage.getItem('user')).toBeNull();
   });
 });
