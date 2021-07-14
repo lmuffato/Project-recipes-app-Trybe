0002a2716c8ce6c048bea585ab28108f94969e7d
Renzo Sevilha <sevilharenzo@gmail.com>
2021-07-14 16:31:13 -0300

    feat: Lottie Loading

--- src/hooks/useLoading.js
+++ src/hooks/useLoading.js
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
+import Lottie from 'react-lottie';
+import animationData from '../assets/loading-lottie.json';
 
 export default function useLoading() {
   const [loading, setLoading] = useState(false);
+  const [isStopped] = useState(false);
+  const [isPaused] = useState(false);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
 
   const renderLoading = (Component) => {
-    if (loading) return <h1>Loading...</h1>;
+    if (loading) {
+      return (
+        <div style={ { margin: '5.8rem 6.7rem' } }>
+          <Lottie
+            options={ defaultOptions }
+            height={ 150 }
+            width={ 150 }
+            isStopped={ isStopped }
+            isPaused={ isPaused }
+          />
+        </div>
+      );
+    }
     return Component;
   };
 
