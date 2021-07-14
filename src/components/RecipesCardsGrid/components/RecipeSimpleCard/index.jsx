32f7ac2cd98edf5d5c026c3853d418042347ae64
João Nasc <oi@joaonasc.dev>
2021-07-14 09:17:17 -0300

    feat: move link of card to grid component

--- src/components/RecipesCardsGrid/components/RecipeSimpleCard/index.jsx
+++ src/components/RecipesCardsGrid/components/RecipeSimpleCard/index.jsx
@@ -1,42 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, useHistory } from 'react-router-dom';
 
 import plus18Icon from '../../../../images/plus18.svg';
 
 import styles from './styles.module.scss';
 
 function RecipeSimpleCard({ recipe, index, alcoholic, recommendationCard }) {
-  const { location: { pathname } } = useHistory();
   return (
-    <Link to={ `${pathname}/${recipe.id}` }>
-      <div
-        data-testid={
-          recommendationCard ? `${index}-recomendation-card` : `${index}-recipe-card`
-        }
-        className={ styles.card }
-      >
-        { alcoholic && (
-          <span className={ styles.alcoholicTag }>
-            <img src={ plus18Icon } alt="18+" />
-            Alcoholic
-          </span>
-        ) }
-        <img
-          src={ recipe.imagePath }
-          alt={ recipe.name }
-          data-testid={ `${index}-card-img` }
-        />
-        <span
-          data-testid={
-            recommendationCard ? `${index}-recomendation-title` : `${index}-card-name`
-          }
-          className={ styles.title }
-        >
-          {recipe.name}
+    <div
+      data-testid={
+        recommendationCard ? `${index}-recomendation-card` : `${index}-recipe-card`
+      }
+      className={ styles.card }
+    >
+      { alcoholic && (
+        <span className={ styles.alcoholicTag }>
+          <img src={ plus18Icon } alt="18+" />
+          Alcoholic
         </span>
-      </div>
-    </Link>
+      ) }
+      <img
+        src={ recipe.imagePath }
+        alt={ recipe.name }
+        data-testid={ `${index}-card-img` }
+      />
+      <span
+        data-testid={
+          recommendationCard ? `${index}-recomendation-title` : `${index}-card-name`
+        }
+        className={ styles.title }
+      >
+        {recipe.name}
+      </span>
+    </div>
   );
 }
 
--- src/components/RecipesCardsGrid/index.jsx
+++ src/components/RecipesCardsGrid/index.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import { RecipesContext } from '../../context/Recipes';
 import RecipeSimpleCard from './components/RecipeSimpleCard';
@@ -7,14 +8,16 @@ import styles from './styles.module.scss';
 
 function RecipesCardsGrid() {
   const { recipes } = useContext(RecipesContext);
+  const history = useHistory();
   return (
     <section className={ styles.grid }>
       {recipes.map((recipe, index) => (
-        <RecipeSimpleCard
-          key={ recipe.id }
-          recipe={ recipe }
-          index={ index }
-        />
+        <Link key={ recipe.id } to={ `${history.location.pathname}/${recipe.id}` }>
+          <RecipeSimpleCard
+            recipe={ recipe }
+            index={ index }
+          />
+        </Link>
       ))}
     </section>
   );
