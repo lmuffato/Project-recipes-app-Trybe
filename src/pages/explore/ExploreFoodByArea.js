30a21aff87759e41adb56cb4710a11d17f91c4b5
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-07-13 23:59:25 -0300

    req 78 part 1 done

--- src/pages/explore/ExploreFoodByArea.js
+++ src/pages/explore/ExploreFoodByArea.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import BottomMenu from '../../components/bottomMenu';
+import getByArea from '../../services/getFoodByArea';
 
 export default function ExploreFoodByArea() {
+  const [area, setArea] = useState([])
+  const [isLoading, setLoading] = useState(false)
+  
+  useEffect(() => {
+    const getArea = async () => {
+      setLoading(true);
+      const data = await getByArea();
+      setArea(data);
+      console.log(data);
+      setLoading(false);
+    }
+    getArea();
+  }, []);
+
+  const renderFilter = () => {
+    
+  }
+
   return (
     <div>
       <Header title="Explorar Origem" show />
---
+++ src/services/getFoodByArea.js
@@ -0,0 +1,19 @@
+const setAreas = (arr) => {
+  const toReturn = arr.map((e) => {
+    const { strArea } = e;
+    return (strArea);
+  });
+  return toReturn;
+}
+
+const getByArea = async () => {
+  const endPoint = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
+  const response = await fetch(endPoint);
+  const data = await response.json();
+  const { meals } = data;
+  const toReturn = await setAreas(meals);
+
+  return toReturn;
+}
+
+export default getByArea;
