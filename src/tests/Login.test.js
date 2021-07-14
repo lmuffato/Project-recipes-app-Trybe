ce4d49c73d3024e6c421a4005af9d49a8ed6f4ec
rcalvs <rafaelcalvette@gmail.com>
2021-07-14 15:40:45 -0300

    Testes iniciais Login

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,8 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Login from '../pages/Login';
 
-test.skip('Farewell, front-end', () => {
+test('Botão de Login', () => {
   const { getByText } = render(<Login />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+  const Button = getByText(/Entrar/i);
+  expect(Button).toBeInTheDocument();
+});
+
+test('Input de Email', () => {
+  const { getByText } = render(<Login />);
+  const Email = getByText(/Email/i);
+  expect(Email).toBeInTheDocument();
+});
+
+test('Input de Senha', () => {
+  const { getByText } = render(<Login />);
+  const Password = getByText(/Password/i);
+  expect(Password).toBeInTheDocument();
 });
