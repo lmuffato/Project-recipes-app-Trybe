6a1d52dd8659a3f8ae86f5c204048a056a905dbf
lucasdev-96 <lucasgodoidev96@gmail.com>
2021-07-13 22:39:50 -0300

    Updates

---
+++ precache-manifest.36e3b957976bc30de4654748a21652b7.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3eac42b48a71c5fb72c82a74009ae12",
+    "url": "/%7Brepo-name%7D/index.html"
+  },
+  {
+    "revision": "9b689dfc6831601089e3",
+    "url": "/%7Brepo-name%7D/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "89766862451488d3cbff",
+    "url": "/%7Brepo-name%7D/static/css/main.98c8cbb8.chunk.css"
+  },
+  {
+    "revision": "9b689dfc6831601089e3",
+    "url": "/%7Brepo-name%7D/static/js/2.025b8438.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/%7Brepo-name%7D/static/js/2.025b8438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89766862451488d3cbff",
+    "url": "/%7Brepo-name%7D/static/js/main.d66ed1eb.chunk.js"
+  },
+  {
+    "revision": "24ba4411f4e370d393a4",
+    "url": "/%7Brepo-name%7D/static/js/runtime-main.f4167bab.js"
+  },
+  {
+    "revision": "c2094f653a7f4625877bb62e3e0e07a9",
+    "url": "/%7Brepo-name%7D/static/media/Recipes.c2094f65.gif"
+  },
+  {
+    "revision": "a04e7963f65d8b505454e334fd3d2e65",
+    "url": "/%7Brepo-name%7D/static/media/bebidas.a04e7963.jpg"
+  },
+  {
+    "revision": "3087e4f819bce236eda568e40e2ece78",
+    "url": "/%7Brepo-name%7D/static/media/blackHeartIcon.3087e4f8.svg"
+  },
+  {
+    "revision": "79051bd2569f210a157958b1f3fdeddc",
+    "url": "/%7Brepo-name%7D/static/media/checkIcon.79051bd2.svg"
+  },
+  {
+    "revision": "ede1863c5241f1163668bbd4d6893873",
+    "url": "/%7Brepo-name%7D/static/media/checkedRecipes.ede1863c.svg"
+  },
+  {
+    "revision": "5671d1c21bdfbec7fb8a0390daaf5717",
+    "url": "/%7Brepo-name%7D/static/media/comidas.5671d1c2.jpg"
+  },
+  {
+    "revision": "5f73de66ffbce8ef30b38702204bdf54",
+    "url": "/%7Brepo-name%7D/static/media/drinkIcon.5f73de66.svg"
+  },
+  {
+    "revision": "30fcbf81c8622c1c94d43921848e2c82",
+    "url": "/%7Brepo-name%7D/static/media/drinks.30fcbf81.jpg"
+  },
+  {
+    "revision": "e8fb4c976bdbe295f75ac93c3a1860fe",
+    "url": "/%7Brepo-name%7D/static/media/exploreIcon.e8fb4c97.svg"
+  },
+  {
+    "revision": "17207861879eb9a923ca53769f28cd22",
+    "url": "/%7Brepo-name%7D/static/media/favoriteRecipes.17207861.svg"
+  },
+  {
+    "revision": "968b730d346d68f9189c7f2758a5d554",
+    "url": "/%7Brepo-name%7D/static/media/ingredientes.968b730d.jpg"
+  },
+  {
+    "revision": "54d3af5588fa742acaae21850cfc479a",
+    "url": "/%7Brepo-name%7D/static/media/localizacao.54d3af55.jpg"
+  },
+  {
+    "revision": "374debffe5050b45e492ab4daa094ffc",
+    "url": "/%7Brepo-name%7D/static/media/mealIcon.374debff.svg"
+  },
+  {
+    "revision": "c5d7a2c05772846cd4149597fe10ee97",
+    "url": "/%7Brepo-name%7D/static/media/playIcon.c5d7a2c0.svg"
+  },
+  {
+    "revision": "8712a673584ce3ba8d57bfcba4860342",
+    "url": "/%7Brepo-name%7D/static/media/profileIcon.8712a673.svg"
+  },
+  {
+    "revision": "4a85aa1984d65004d129338964e6de88",
+    "url": "/%7Brepo-name%7D/static/media/searchIcon-2.4a85aa19.svg"
+  },
+  {
+    "revision": "07d3fc27de505ef17d6e9a631205f8fd",
+    "url": "/%7Brepo-name%7D/static/media/searchIcon.07d3fc27.svg"
+  },
+  {
+    "revision": "0d16489b1441ce1b913be229d2fe5ca3",
+    "url": "/%7Brepo-name%7D/static/media/shareIcon.0d16489b.svg"
+  },
+  {
+    "revision": "bd5b3a0285d82e184bc9bb1a136dd346",
+    "url": "/%7Brepo-name%7D/static/media/suprisedrink.bd5b3a02.jpg"
+  },
+  {
+    "revision": "3ddb90f0857aa82bb03b3d4698f0d0d1",
+    "url": "/%7Brepo-name%7D/static/media/surpreenda.3ddb90f0.jpg"
+  },
+  {
+    "revision": "e1b51d442b950404037e95b20191f1dc",
+    "url": "/%7Brepo-name%7D/static/media/userImage.e1b51d44.png"
+  },
+  {
+    "revision": "753f696e93c6e26bc48e5e1f2833b9b7",
+    "url": "/%7Brepo-name%7D/static/media/whiteHeartIcon.753f696e.svg"
+  }
+]);
