3d6a6eede9daef982e23605449c2a653c90b80da
renancosta21 <renancosta21@gmail.com>
2021-07-14 15:15:04 -0300

    fix: test Profile page

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import ProfileBody from '../pages/components/ProfilePage/ProfileBody';
+import Profile from '../pages/Profile';
 import renderWithRouterAndRedux from './renderWithRouterAndRedux';
 
 describe('tests if the elements of the Profile page are being rendered', () => {
   test('email element and the buttons Done Recipes, Favorite Recipes and Logout', () => {
-    const { getByTestId } = renderWithRouterAndRedux(<ProfileBody />);
+    const { getByTestId } = renderWithRouterAndRedux(<Profile />);
 
     const emailElement = getByTestId('profile-email');
     expect(emailElement).toBeInTheDocument();
@@ -23,21 +23,21 @@ describe('tests if the elements of the Profile page are being rendered', () => {
 
 describe('tests the routes after clicking the buttons', () => {
   test('done recipes button', () => {
-    const { getByTestId, history } = renderWithRouterAndRedux(<ProfileBody />);
+    const { getByTestId, history } = renderWithRouterAndRedux(<Profile />);
     const doneRecipesBtn = getByTestId('profile-done-btn');
     userEvent.click(doneRecipesBtn);
     const path = history.location.pathname;
     expect(path).toBe('/receitas-feitas');
   });
   test('favorite recipes button', () => {
-    const { getByTestId, history } = renderWithRouterAndRedux(<ProfileBody />);
+    const { getByTestId, history } = renderWithRouterAndRedux(<Profile />);
     const favoriteRecipesBtn = getByTestId('profile-favorite-btn');
     userEvent.click(favoriteRecipesBtn);
     const path = history.location.pathname;
     expect(path).toBe('/receitas-favoritas');
   });
   test('logout button', () => {
-    const { getByTestId, history } = renderWithRouterAndRedux(<ProfileBody />);
+    const { getByTestId, history } = renderWithRouterAndRedux(<Profile />);
     const logoutBtn = getByTestId('profile-logout-btn');
     userEvent.click(logoutBtn);
     const path = history.location.pathname;
