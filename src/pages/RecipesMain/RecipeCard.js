97a839d62a69884bfeed3c2a3f629765fa0e5124
Alexandre Oliveira <alexandremolive@gmail.com>
2021-07-14 16:45:32 -0300

    created page exploreByIngredients

--- src/pages/RecipesMain/RecipeCard.js
+++ src/pages/RecipesMain/RecipeCard.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function RecipeCard({ recipe, index }) {
+export default function RecipeCard({ recipe, index, isMain }) {
+  // const [setGetImage] = useState('');
+
+  // useEffect(() => {
+  //   setGetImage(path.includes('/comidas') ? 'themealdb' : 'thecocktaildb');
+  // }, [path]);
+
   return (
-    <div className="recipe-card" data-testid={ `${index}-recipe-card` }>
+
+    <div
+      className="recipe-card"
+      data-testid={ isMain ? `${index}-recipe-card` : `${index}-ingredient-card` }
+    >
+
       <img
-        src={ recipe.strMealThumb || recipe.strDrinkThumb }
-        data-testid={ `${index}-card-img` }
+        src={ isMain
+          ? recipe.strMealThumb || recipe.strDrinkThumb
+          : '' }
+        data-testid={ isMain ? `${index}-card-img` : `${index}-card-img` }
         alt="recipe"
         width="150px"
       />
-      <p data-testid={ `${index}-card-name` }>{ recipe.strMeal || recipe.strDrink }</p>
+      <p
+        data-testid={ isMain ? `${index}-card-name` : `${index}-card-name` }
+      >
+        { isMain
+          ? recipe.strMeal || recipe.strDrink
+          : recipe.strIngredient || recipe.strIngredient1 }
+
+      </p>
     </div>
   );
 }
