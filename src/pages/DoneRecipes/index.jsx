38ad10116e29ee4db5797eeb4f1bfecffd7ef913
Ederson rodrigo <edersonrodrigo31@gmail.com>
2021-07-15 12:14:18 -0300

    feat: req 58

--- src/pages/DoneRecipes/index.jsx
+++ src/pages/DoneRecipes/index.jsx
@@ -1,30 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styles from './styles.module.scss';
+
+import Header from '../../components/Header';
 
 import Card from '../../components/HorizontalRecipeCard';
 
 function DoneRecipes() {
-  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  const [doneRecipes, setDoneRecipes] = useState([]);
 
-  //   const as = [{
-  //     id: id - da - receita,
-  //     type: comida - ou - bebida,
-  //     area: area - da - receita - ou - texto - vazio,
-  //     category: categoria - da - receita - ou - texto - vazio,
-  //     alcoholicOrNot: alcoholic - ou - non - alcoholic - ou - texto - vazio,
-  //     name: nome - da - receita,
-  //     image: imagem - da - receita,
-  //     doneDate: quando - a - receita - foi - concluida,
-  //     tags: array - de - tags - da - receita - ou - array - vazio,
-  //   }];
+  function filterDoneRecipes(filter) {
+    const doneRecipesStorage = JSON.parse(localStorage.getItem('doneRecipes'));
+
+    switch (filter) {
+    case 'All': {
+      setDoneRecipes(doneRecipesStorage);
+      break;
+    }
+    case 'Food': {
+      setDoneRecipes(doneRecipesStorage.filter(({ type }) => type === 'comida'));
+      break;
+    }
+    case 'Drink': {
+      setDoneRecipes(doneRecipesStorage.filter(({ type }) => type === 'bebida'));
+      break;
+    }
+    default:
+      break;
+    }
+  }
+
+  useEffect(() => {
+    const doneRecipesStorage = JSON.parse(localStorage.getItem('doneRecipes'));
+    setDoneRecipes(doneRecipesStorage);
+  }, []);
 
   return (
     <>
-      <button type="button" data-testid="filter-by-all-btn">All</button>
-      <button type="button" data-testid="filter-by-food-btn">Food</button>
-      <button type="button" data-testid="filter-by-drink-btn">Drink</button>
-      {doneRecipes.map((recipe, index) => (
-        <Card key={ recipe.id } recipe={ recipe } index={ index } />
-      ))}
+      <Header title="Receitas Feitas" hideSearch />
+      <div className={ styles.page }>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ ({ target }) => filterDoneRecipes(target.innerText) }
+        >
+          All
+
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          onClick={ ({ target }) => filterDoneRecipes(target.innerText) }
+        >
+          Food
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ ({ target }) => filterDoneRecipes(target.innerText) }
+        >
+          Drink
+        </button>
+        {doneRecipes.map((recipe, index) => (
+          <Card key={ recipe.id } recipe={ recipe } index={ index } />
+        ))}
+      </div>
     </>
   );
 }
