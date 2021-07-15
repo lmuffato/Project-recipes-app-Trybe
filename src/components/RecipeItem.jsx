a161b7d12346300d793ceab83c38da83f44716f5
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-07-15 20:50:13 -0300

    Feat: botao de compartilhar implementado - requisito 63

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Button, Item } from 'semantic-ui-react';
+import { Button, Item, Popup } from 'semantic-ui-react';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 
+const copy = require('clipboard-copy');
+
 function RecipeItem(Props) {
   const { data, index } = Props;
+  const contextRef = React.useRef();
+  const [open, setOpen] = React.useState(false);
+  const handleShare = () => {
+    const SEC = 3000;
+    copy(`http://localhost:3000/${data.type}s/${data.id}`);
+    setOpen((prevOpen) => !prevOpen);
+    setTimeout(() => {
+      setOpen((prevOpen) => !prevOpen);
+    }, SEC);
+  };
   const topText = data.type === 'comida'
     ? `${data.area} - ${data.category}`
     : `${data.alcoholicOrNot}`;
@@ -21,14 +33,22 @@ function RecipeItem(Props) {
         </Item.Meta>
         <Item.Header data-testid={ `${index}-horizontal-name` }>{data.name}</Item.Header>
         <Item.Extra>
+          <Popup
+            context={ contextRef }
+            content="Link copiado!"
+            position="bottom center"
+            open={ open }
+          />
           <Button
             style={ { background: 'none' } }
             data-testid={ `${index}-horizontal-share-btn` }
             src={ shareIcon }
+            onClick={ handleShare }
           >
             <img
               src={ shareIcon }
               alt="share-icon"
+              ref={ contextRef }
             />
           </Button>
           <Button
