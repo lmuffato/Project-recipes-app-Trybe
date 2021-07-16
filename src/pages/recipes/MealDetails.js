3651f3f005054d94f203be75d10554f88688dec0
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-07-16 11:15:12 -0300

    meal detail v2 part 1

--- src/pages/recipes/MealDetails.js
+++ src/pages/recipes/MealDetails.js
@@ -1,6 +1,49 @@
 import React from 'react';
 
 function MealDetails() {
+
+  const renderFavNShare = () => {
+    return (
+      <div>
+        <button
+          type="button"
+          data-testid="share-btn"
+        >
+          share
+        </button>
+        <button
+          type="button"
+          data-testid="favorite-btn"
+        >
+          Fav
+        </button>
+      </div>
+    );
+  }
+
+  const renderInstructions
+
+  const renderDetail = () => {
+    return (
+      <div>
+        <img
+          src=""
+          data-testid="recipe-photo"
+        />
+        <h1
+          data-testid="recipe-title"
+        >
+          Recipe Name
+        </h1>
+        <p
+          data-testid="recipe-category"
+        >
+          Recipe category
+        </p>
+      </div>
+    );
+  };
+
   return (
     <div>
       <h1>hello world</h1>
