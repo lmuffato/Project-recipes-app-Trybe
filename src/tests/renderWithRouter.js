2fe770f4d50ac4e2ec4332f611a7cab00db224d8
iago <iago.pferreiravr@gmail.com>
2021-07-17 12:43:15 -0300

    Re-up all the changes of branch main-group-14-header-test

--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
+import UserProvider from '../contexts/UserProvider';
+import SearchbarProvider from '../contexts/SearchbarProvider';
+import RecipesProvider from '../contexts/RecipesProvider';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
-    ...render(<Router history={ history }>{component}</Router>), history,
+    ...render(
+      <UserProvider>
+        <SearchbarProvider>
+          <RecipesProvider>
+            <Router history={ history }>{component}</Router>
+          </RecipesProvider>
+        </SearchbarProvider>
+      </UserProvider>,
+    ),
+    history,
   });
 };
 
