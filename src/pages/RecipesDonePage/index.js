a8630abab2c88cb257c2c1b6841ac924f5137759
Gabriel Ferreira <glucas709@gmail.com>
2021-07-17 20:42:59 -0300

    fix: requiriment 21

--- src/pages/RecipesDonePage/index.js
+++ src/pages/RecipesDonePage/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from '../../components/Header';
 import RecipesDoneItems from '../../components/RecipesDoneItems';
-import Footer from '../../components/Footer';
 
 export default function RecipesDonePage() {
   return (
     <div>
       <Header title="Receitas Feitas" />
       <RecipesDoneItems />
-      <Footer />
     </div>
   );
 }
