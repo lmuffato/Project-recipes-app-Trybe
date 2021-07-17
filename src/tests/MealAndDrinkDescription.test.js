f86e9af264a8992a9536d258364f062672bd096d
iago <iago.pferreiravr@gmail.com>
2021-07-17 12:51:52 -0300

    Create MealAndDrinkDescripiton.test.js

---
+++ src/tests/MealAndDrinkDescription.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import handleLogin from './handleLogin';
+import App from '../App';
+
+describe('Tests the component MealDescripiton', () => {
+  it('verify if it has all the necessary elements', () => {});
+
+});
