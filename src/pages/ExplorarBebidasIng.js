b1a02ff57f85901a9e12c71a11d70cd857a1bdcf
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-07-18 12:44:41 -0300

    paginas de explorar comidas por ingredientes

--- src/pages/ExplorarBebidasIng.js
+++ src/pages/ExplorarBebidasIng.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+import { fetchDrinkIngredients } from '../services/cocktailAPI';
+
+import IngredientCard from '../components/ExplorarIngredientes/IngredientCard';
+
 export default function ExplorarBebidasIng() {
+  const [ingredients, setIngredients] = useState([]);
+  const amountOfIngredientsToDisplay = 12;
+
+  useEffect(() => {
+    fetchDrinkIngredients().then((data) => {
+      const displayIngredients = data.drinks.slice(0, amountOfIngredientsToDisplay);
+      setIngredients(displayIngredients);
+    });
+  }, []);
+
   return (
     <div>
       <Header
         title="Explorar Ingredientes"
         enableSearchIcon={ false }
       />
+      {ingredients && ingredients.map((ing, index) => (
+        <IngredientCard
+          key={ index }
+          ingredient={ ing }
+          type="bebidas"
+          index={ index }
+        />
+      ))}
       <Footer />
     </div>
   );
--- src/pages/ExplorarComidasIng.js
+++ src/pages/ExplorarComidasIng.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+import { fetchFoodIngredients } from '../services/mealAPI';
+
+import IngredientCard from '../components/ExplorarIngredientes/IngredientCard';
+
 export default function ExplorarComidasIng() {
+  const [ingredients, setIngredients] = useState([]);
+  const amountOfIngredientsToDisplay = 12;
+
+  useEffect(() => {
+    fetchFoodIngredients().then((data) => {
+      const displayIngredients = data.meals.slice(0, amountOfIngredientsToDisplay);
+      setIngredients(displayIngredients);
+    });
+  }, []);
+
   return (
     <div>
       <Header
         title="Explorar Ingredientes"
         enableSearchIcon={ false }
       />
+      {ingredients && ingredients.map((ing, index) => (
+        <IngredientCard
+          key={ index }
+          ingredient={ ing }
+          type="comidas"
+          index={ index }
+        />
+      ))}
       <Footer />
     </div>
   );
