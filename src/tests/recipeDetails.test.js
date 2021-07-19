e7ee17827d1c26d9e864dd02d82e812d56a98493
rcalvs <rafaelcalvette@gmail.com>
2021-07-19 13:38:24 -0300

    Setup inicial de test

---
+++ src/tests/recipeDetails.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Test Recipes Details page', () => {
+  const myPath = '/comidas/52978';
+  it('Check Page Components', () => {
+    const { getByTestId, history } = renderWithRouter(<App />);
+    history.push(myPath);
+
+    const recipeTitle = getByTestId('recipe-title');
+    expect(recipeTitle).toBeInTheDocument();
+
+    const recipeCategory = getByTestId('recipe-category');
+    expect(recipeCategory).toBeInTheDocument();
+
+    const recipeFirstIngredient = getByTestId('0-ingredient-name-and-measure');
+    expect(recipeFirstIngredient).toBeInTheDocument();
+
+    const recipeInstructions = getByTestId('instructions');
+    expect(recipeInstructions).toBeInTheDocument();
+
+    const recipeVideo = getByTestId('video');
+    expect(recipeVideo).toBeInTheDocument();
+
+    const recipeStartBtn = getByTestId('start-recipe-btn');
+    expect(recipeStartBtn).toBeInTheDocument();
+  });
+});
