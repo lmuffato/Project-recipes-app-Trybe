3e9f1801cd570b0055fd8860744b2bef418f5c13
Gabriel Ferreira <glucas709@gmail.com>
2021-07-19 10:17:54 -0300

    fix lint

--- src/components/ComidasInProgress/index.jsx
+++ src/components/ComidasInProgress/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useLocation, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import getIngredients from '../../services/getIngredients';
+// import getIngredients from '../../services/getIngredients';
 import ShareButton from '../ShareButton';
 import FavoriteButton from '../FavoriteButton';
 
