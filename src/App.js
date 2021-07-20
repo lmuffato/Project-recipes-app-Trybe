d71097adbdc0729ec7edc2b757ea3b087dd9c920
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-07-20 13:35:43 -0400

    refatoring 2

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { FoodProvider } from './Context/FoodProvider';
-import Login from './Pages/Login';
-import FoodPage from './Pages/FoodPage';
-import BeveragePage from './Pages/BeveragePage';
-import ProfilePage from './Pages/ProfilePage';
-import DetailsPage from './Pages/DetailsPage';
-import ExplorePage from './Pages/ExplorePage';
-import ProgressPage from './Pages/ProgressPage';
 import './App.css';
-import DoneRecipes from './Pages/DoneRecipes';
-import ExploreDrinks from './Pages/ExploreDrinks';
-import ExploreFood from './Pages/ExploreFood';
-import Area from './Pages/Area';
-import FavoriteRecipes from './Pages/FavoriteRecipes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
+import Food from './pages/Food';
+import Drinks from './pages/Drinks';
+import FoodRecipe from './pages/FoodRecipe';
+import DrinkRecipe from './pages/DrinkRecipe';
+import FoodRecipeProgress from './pages/FoodRecipeProgress';
+import DrinkRecipeProgress from './pages/DrinkRecipeProgress';
+import Explore from './pages/Explore';
+import ExploreFood from './pages/ExploreFood';
+import ExploreDrinks from './pages/ExploreDrinks';
+import ExploreFoodIngredients from './pages/ExploreFoodIngredients';
+import ExploreDrinksIngredients from './pages/ExploreDrinksIngredients';
+import ExploreFoodArea from './pages/ExploreFoodArea';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import NotFound from './components/NotFound';
 
-function App() {
+export default function App() {
   return (
-    <FoodProvider>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/comidas" component={ FoodPage } />
-        <Route exact path="/comidas/:id" component={ DetailsPage } />
-        <Route exact path="/comidas/:id/in-progress" component={ ProgressPage } />
-        <Route exact path="/bebidas" component={ BeveragePage } />
-        <Route exact path="/bebidas/:id" component={ DetailsPage } />
-        <Route exact path="/bebidas/:id/in-progress" component={ ProgressPage } />
-        <Route exact path="/perfil" component={ ProfilePage } />
-        <Route exact path="/explorar" component={ ExplorePage } />
-        <Route exact path="/explorar/comidas" component={ ExploreFood } />
-        <Route exact path="/explorar/bebidas" component={ ExploreDrinks } />
-        <Route exact path="/receitas-feitas" component={ DoneRecipes } />
-        <Route exact path="/receitas-favoritas" component={ FavoriteRecipes } />
-        <Route exact path="/explorar/comidas/area" component={ Area } />
-      </Switch>
-    </FoodProvider>
+    <Switch>
+      <Route
+        path="/explorar/bebidas/ingredientes"
+        component={ ExploreDrinksIngredients }
+      />
+      <Route path="/perfil" component={ Profile } />
+      <Route path="/receitas-feitas" component={ DoneRecipes } />
+      <Route path="/receitas-favoritas" component={ FavoriteRecipes } />
+      <Route path="/explorar/comidas/ingredientes" component={ ExploreFoodIngredients } />
+      <Route path="/explorar/comidas/area" component={ ExploreFoodArea } />
+      <Route path="/explorar/comidas" component={ ExploreFood } />
+      <Route exact path="/explorar/bebidas" component={ ExploreDrinks } />
+      <Route exact path="/explorar" component={ Explore } />
+      <Route path="/bebidas/:id/in-progress" component={ DrinkRecipeProgress } />
+      <Route path="/bebidas/:id" component={ DrinkRecipe } />
+      <Route path="/bebidas" component={ Drinks } />
+      <Route path="/comidas/:id/in-progress" component={ FoodRecipeProgress } />
+      <Route path="/comidas/:id" component={ FoodRecipe } />
+      <Route path="/comidas" component={ Food } />
+      <Route exact path="/" component={ Login } />
+      <Route component={ NotFound } />
+    </Switch>
   );
 }
-
-export default App;
--- src/Pages/Area.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Area = () => (
-  <div>
-    Pagina de Area
-  </div>
-);
-
-export default Area;
