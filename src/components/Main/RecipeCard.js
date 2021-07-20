b2d55e59afa570d1245670621cf0723a78564894
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-07-20 13:08:52 -0300

    Max of Characters length in Recipe Cards Fixed

--- src/components/Main/RecipeCard.js
+++ src/components/Main/RecipeCard.js
@@ -4,6 +4,19 @@ import PropTypes from 'prop-types';
 import '../../style/RecipeCard.css';
 
 export default function RecipeCard({ recipe, index, type: URL_PATH }) {
+
+  function zipName(recipeName) {
+    const MAX_OF_CHARACTERS = 25;
+
+    let compactedName = recipeName.slice(0, MAX_OF_CHARACTERS);
+
+    if (recipeName.length > MAX_OF_CHARACTERS) {
+      compactedName = recipeName.slice(0, MAX_OF_CHARACTERS).concat('...');
+    }
+
+    return compactedName;
+  }
+
   return (
     <div className="recipe-card">
       <Link
@@ -24,7 +37,7 @@ export default function RecipeCard({ recipe, index, type: URL_PATH }) {
           />
           <p data-testid={ `${index}-card-name` }>
             { URL_PATH === '/comidas' || URL_PATH === '/comidas/'
-              ? recipe.strMeal
+              ? zipName(recipe.strMeal)
               : recipe.strDrink }
           </p>
         </div>
