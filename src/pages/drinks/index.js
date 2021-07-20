367dfa7aa41ae555b7fb620f9134004daed938d2
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-07-20 13:58:42 -0400

    refatoring 5

--- src/pages/drinks/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Drinks } from './Drinks';
-export { default as DrinksDetails } from './DrinksDetails';
-export { default as ExploreDrinks } from './ExploreDrinks';
-export { default as DrinksByIngredients } from './DrinksByIngredients';
-export { default as DrinksInProgress } from './DrinksInProgress';
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Login } from './Login';
-export { default as Explore } from './Explore';
-export { default as Profile } from './Profile';
-export { default as FavoriteRecipes } from './FavoriteRecipes';
-export { default as DoneRecipes } from './DoneRecipes';
