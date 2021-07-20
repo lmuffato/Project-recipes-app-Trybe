0cd504e190fb127e360bd8045aeb52d4ea199563
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-07-20 12:39:45 -0300

    URL Fix

--- src/components/Main/RecipeCard.js
+++ src/components/Main/RecipeCard.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../../style/RecipeCard.css';
 
-export default function RecipeCard({ recipe, index, type }) {
+export default function RecipeCard({ recipe, index, type: URL_PATH }) {
   return (
     <div className="recipe-card">
       <Link
         to={
-          type === '/comidas'
+          URL_PATH === '/comidas' || URL_PATH === '/comidas/'
             ? `/comidas/${recipe.idMeal}`
             : `/bebidas/${recipe.idDrink}`
         }
@@ -16,12 +16,16 @@ export default function RecipeCard({ recipe, index, type }) {
         <div data-testid={ `${index}-recipe-card` }>
           <img
             data-testid={ `${index}-card-img` }
-            src={ type === '/comidas' ? recipe.strMealThumb : recipe.strDrinkThumb }
+            src={ URL_PATH === '/comidas' || URL_PATH === '/comidas/'
+              ? recipe.strMealThumb
+              : recipe.strDrinkThumb }
             alt="thumbnail recipe"
             width="100"
           />
           <p data-testid={ `${index}-card-name` }>
-            { type === '/comidas' ? recipe.strMeal : recipe.strDrink }
+            { URL_PATH === '/comidas' || URL_PATH === '/comidas/'
+              ? recipe.strMeal
+              : recipe.strDrink }
           </p>
         </div>
       </Link>
