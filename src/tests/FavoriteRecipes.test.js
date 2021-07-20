9fa6683a16ca73726c2a43b30f9532bcefc509fb
Gustavo Sousa <gustavohl98@gmail.com>
2021-07-20 20:25:13 -0300

    test: melhora testes da pag de favoritos

--- src/tests/FavoriteRecipes.test.js
+++ src/tests/FavoriteRecipes.test.js
@@ -1,7 +1,6 @@
 import { waitForElement, fireEvent } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
-import ExploreContainer from '../styles/exploreIngredients';
 import renderWithRouterHooksAndProvider from './renderWithRouterHooksAndProvider';
 
 describe('Testa funcionalidade de receitas favoritas', () => {
@@ -22,4 +21,41 @@ describe('Testa funcionalidade de receitas favoritas', () => {
     const noFavoritesMsg = getByText('Você não possui receitas favoritas!');
     expect(noFavoritesMsg).toBeInTheDocument();
   });
+
+  const FAVORITE_BTN = 'favorite-btn';
+  const FIRST_IMG = '0-horizontal-image';
+
+  it('Receita é favoritada corretamente',
+    async () => {
+      const {
+        getByText,
+        getByTestId,
+        history,
+      } = renderWithRouterHooksAndProvider(
+        <App />,
+        '/bebidas/15997',
+      );
+
+      await waitForElement(() => getByTestId('recipe-photo'));
+
+      expect(getByTestId(FAVORITE_BTN)).toBeInTheDocument();
+      fireEvent.click(getByTestId(FAVORITE_BTN));
+      history.push('/receitas-favoritas');
+
+      await waitForElement(() => getByTestId(FIRST_IMG));
+      expect(getByTestId(FIRST_IMG)).toBeInTheDocument();
+      expect(getByText('GG')).toBeInTheDocument();
+
+      history.push('/bebidas/15997');
+      await waitForElement(() => getByTestId('recipe-photo'));
+
+      expect(getByTestId('share-btn')).toBeInTheDocument();
+      expect(getByTestId(FAVORITE_BTN).src).toMatch(
+        'blackHeartIcon.svg',
+      );
+      fireEvent.click(getByTestId(FAVORITE_BTN));
+      expect(getByTestId(FAVORITE_BTN).src).toMatch(
+        'whiteHeartIcon.svg',
+      );
+    });
 });
