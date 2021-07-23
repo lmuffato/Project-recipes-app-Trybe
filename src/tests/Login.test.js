e9674c9c069273ba6f9ef56c8b1d430c55650dc8
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-07-23 15:18:46 -0300

    mocking fetch

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createMemoryHistory } from 'history';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from './renderWithRouter';
+import { act } from '@testing-library/react';
+import renderWithRouterAndProvider from './renderWithRouterAndProvider';
 import Login from '../pages/Login';
 import App from '../App';
-import Provider from '../contexts/Provider';
+import mockFetch from '../../cypress/mocks/fetch';
 
 const validEmail = 'email@email.com';
 const invalidEmail1 = 'email@emailcom';
@@ -15,11 +16,19 @@ const emailInputString = 'email-input';
 const passwordInputString = 'password-input';
 const loginInputString = 'login-submit-btn';
 
+beforeAll(() => jest.spyOn(window, 'fetch'));
+
+beforeEach(() => window.fetch.mockImplementation(mockFetch));
+
 describe('Verifica se na tela de login', () => {
   const historyMock = createMemoryHistory();
 
-  it('os inputs possuem os test ids corretos', () => {
-    const { getByTestId } = renderWithRouter(<Login history={ historyMock } />);
+  it('os inputs possuem os test ids corretos', async () => {
+    const { getByTestId } = renderWithRouterAndProvider(
+      <Login history={ historyMock } />,
+    );
+    await act(() => mockFetch());
+
     const emailTestId = getByTestId(emailInputString);
     const passwordTestId = getByTestId(passwordInputString);
     const loginBtnTestId = getByTestId(loginInputString);
@@ -29,31 +38,36 @@ describe('Verifica se na tela de login', () => {
     expect(loginBtnTestId).toBeInTheDocument();
   });
 
-  it('é possível digitar o email no input de email', () => {
-    const { getByTestId } = renderWithRouter(
+  it('é possível digitar o email no input de email', async () => {
+    const { getByTestId } = renderWithRouterAndProvider(
       <Login history={ historyMock } />,
     );
     const emailInput = getByTestId(emailInputString);
+    await act(() => mockFetch());
 
     userEvent.type(emailInput, validEmail);
+
     expect(emailInput).toHaveAttribute('value', validEmail);
   });
 
-  it('é possível digitar a senha no input de password', () => {
-    const { getByTestId } = renderWithRouter(
+  it('é possível digitar a senha no input de password', async () => {
+    const { getByTestId } = renderWithRouterAndProvider(
       <Login history={ historyMock } />,
     );
+    await act(() => mockFetch());
     const passwordInput = getByTestId(passwordInputString);
 
     userEvent.type(passwordInput, validPassword);
+
     expect(passwordInput).toHaveAttribute('value', validPassword);
   });
 
   it('só é possível enviar o formulário quando os conteúdos dos campos de email e'
-  + ' password são válidos e o botão enviar fica desabilidato caso não', () => {
-    const { getByTestId } = renderWithRouter(
+  + ' password são válidos e o botão enviar fica desabilidato caso não', async () => {
+    const { getByTestId } = renderWithRouterAndProvider(
       <Login history={ historyMock } />,
     );
+    await act(() => mockFetch());
     const emailInput = getByTestId(emailInputString);
     const passwordInput = getByTestId(passwordInputString);
     const loginBtn = getByTestId(loginInputString);
@@ -80,10 +94,11 @@ describe('Verifica se na tela de login', () => {
   });
 
   it('após a submissão são armazenados na local storage dois tokens:'
-  + ' mealsToken e cocktailsToken ambos com valor 1', () => {
-    const { getByTestId } = renderWithRouter(
+  + ' mealsToken e cocktailsToken ambos com valor 1', async () => {
+    const { getByTestId } = renderWithRouterAndProvider(
       <Login history={ historyMock } />,
     );
+    await act(() => mockFetch());
     localStorage.removeItem('mealsToken');
     localStorage.removeItem('cocktailsToken');
 
@@ -102,10 +117,11 @@ describe('Verifica se na tela de login', () => {
   });
 
   it('após a submissão o email do usuário é armazenado na localStorage  na chave'
-  + ' user no formato { email: email-da-pessoa }', () => {
-    const { getByTestId } = renderWithRouter(
+  + ' user no formato { email: email-da-pessoa }', async () => {
+    const { getByTestId } = renderWithRouterAndProvider(
       <Login history={ historyMock } />,
     );
+    await act(() => mockFetch());
     localStorage.removeItem('user');
 
     const emailInput = getByTestId(emailInputString);
@@ -122,20 +138,18 @@ describe('Verifica se na tela de login', () => {
 
   it('após a submissão o usuário é redirecionado para a página de receita de'
   + ' comidas', async () => {
-    const { getByTestId, history } = renderWithRouter(
-      <Provider>
-        <App />
-      </Provider>
-      ,
-    );
-
+    const { getByTestId, history } = await renderWithRouterAndProvider(<App />);
+    await act(() => mockFetch());
     const emailInput = getByTestId(emailInputString);
     const passwordInput = getByTestId(passwordInputString);
     const loginBtn = getByTestId(loginInputString);
 
-    userEvent.type(passwordInput, validPassword);
-    userEvent.type(emailInput, validEmail);
-    userEvent.click(loginBtn);
+    act(() => {
+      userEvent.type(passwordInput, validPassword);
+      userEvent.type(emailInput, validEmail);
+      userEvent.click(loginBtn);
+    });
+    await act(() => mockFetch());
 
     expect(history.location.pathname).toBe('/comidas');
   });
