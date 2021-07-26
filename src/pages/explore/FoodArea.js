37d936a7a0acf675299f29c9b8223d5a6819fbe6
andy silva <andssilva.23@gmail.com>
2021-07-26 19:28:22 -0400

    feat: :rocket: agora vai!

--- src/pages/explore/FoodArea.js
+++ src/pages/explore/FoodArea.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useStateEasyRedux, useClassState } from 'easy-redux-trybe';
